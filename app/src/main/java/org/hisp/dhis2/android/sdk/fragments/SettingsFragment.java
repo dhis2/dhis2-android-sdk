1b44f2acc21261cf6633a14c8aef1c88d119731c
Araz Abishov <araz.abishov.gsoc@gmail.com>
2015-03-30 03:29:34 +0500

    Refactored SettingsFragment:  - Better UI  - Better code structure in Fragment

--- app/src/main/java/org/hisp/dhis2/android/sdk/fragments/SettingsFragment.java
+++ app/src/main/java/org/hisp/dhis2/android/sdk/fragments/SettingsFragment.java
@@ -31,12 +31,10 @@ package org.hisp.dhis2.android.sdk.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
@@ -45,70 +43,53 @@ import org.hisp.dhis2.android.sdk.controllers.Dhis2;
 import org.hisp.dhis2.android.sdk.events.BaseEvent;
 import org.hisp.dhis2.android.sdk.events.MessageEvent;
 import org.hisp.dhis2.android.sdk.persistence.Dhis2Application;
-import org.hisp.dhis2.android.sdk.persistence.models.OrganisationUnit;
-import org.hisp.dhis2.android.sdk.persistence.models.Program;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Basic settings Fragment giving users options to change update frequency to the server,
  * and logging out.
+ *
  * @author Simen Skogly Russnes on 02.03.15.
  */
-public class SettingsFragment extends Fragment {
-
+public class SettingsFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     private Spinner updateFrequencySpinner;
     private Button logoutButton;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // we need to disable options menu in this fragment
+        setHasOptionsMenu(false);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
-        View rootView = inflater.inflate(R.layout.fragment_settings,
-                container, false);
-        setupUi(rootView);
-        return rootView;
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
-    public void setupUi(View rootView) {
-        updateFrequencySpinner = (Spinner) rootView.findViewById(R.id.settings_updatefrequencyspinner);
-        logoutButton = (Button) rootView.findViewById(R.id.settings_logoutbutton);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        updateFrequencySpinner = (Spinner) view.findViewById(R.id.settings_update_frequency_spinner);
         updateFrequencySpinner.setSelection(Dhis2.getUpdateFrequency(getActivity()));
+        updateFrequencySpinner.setOnItemSelectedListener(this);
 
-        updateFrequencySpinner.post(new Runnable() {
-                                        public void run() {
-                                            updateFrequencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        logoutButton = (Button) view.findViewById(R.id.settings_logout_button);
+        logoutButton.setOnClickListener(this);
+    }
 
-                                            {
-                                                @Override
-                                                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                                                    Dhis2.setUpdateFrequency(getActivity(), position);
-                                                }
-
-                                                @Override
-                                                public void onNothingSelected(AdapterView<?> parent) {
-
-                                                }
-                                            });
-                                        }
-                                    });
-
-            logoutButton.setOnClickListener(new View.OnClickListener()
-
-            {
-                @Override
-                public void onClick (View v){
-                logout();
-            }
-            }
-
-            );
-        }
-
-    public void logout() {
+    @Override
+    public void onClick(View v) {
         MessageEvent event = new MessageEvent(BaseEvent.EventType.logout);
         Dhis2Application.bus.post(event);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Dhis2.setUpdateFrequency(getActivity(), position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // stub implementation
     }
 }

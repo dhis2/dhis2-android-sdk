91c9bf3702226c28be08ca7f383444607793d655
simernes <simen_russnes@hotmail.com>
2015-05-26 10:43:27 +0200

    updated json parser for server change

--- app/src/main/java/org/hisp/dhis2/android/sdk/persistence/models/ProgramRuleVariable.java
+++ app/src/main/java/org/hisp/dhis2/android/sdk/persistence/models/ProgramRuleVariable.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @Table(databaseName = Dhis2Database.NAME)
 public class ProgramRuleVariable extends BaseIdentifiableObject {
 
+    @JsonProperty("programRuleVariableSourceType")
     @Column
     public String sourceType;
 

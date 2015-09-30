479ed413e971da7a3c1fe7b30dcd79d9dbdbe816
Araz Abishov <araz.abishov.gsoc@gmail.com>
2015-09-30 12:52:33 +0200

    Code clean-up.

--- models/src/main/java/org/hisp/dhis/android/sdk/models/common/SystemInfo.java
+++ models/src/main/java/org/hisp/dhis/android/sdk/models/common/SystemInfo.java
@@ -28,7 +28,6 @@
 
 package org.hisp.dhis.android.sdk.models.common;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -61,82 +60,66 @@ public final class SystemInfo {
     @JsonProperty("version")
     String version;
 
-    @JsonIgnore
     public DateTime getBuildTime() {
         return buildTime;
     }
 
-    @JsonIgnore
     public void setBuildTime(DateTime buildTime) {
         this.buildTime = buildTime;
     }
 
-    @JsonIgnore
     public DateTime getServerDate() {
         return serverDate;
     }
 
-    @JsonIgnore
     public void setServerDate(DateTime serverDate) {
         this.serverDate = serverDate;
     }
 
-    @JsonIgnore
     public String getCalendar() {
         return calendar;
     }
 
-    @JsonIgnore
     public void setCalendar(String calendar) {
         this.calendar = calendar;
     }
 
-    @JsonIgnore
     public String getDateFormat() {
         return dateFormat;
     }
 
-    @JsonIgnore
     public void setDateFormat(String dateFormat) {
         this.dateFormat = dateFormat;
     }
 
-    @JsonIgnore
     public String getIntervalSinceLastAnalyticsTableSuccess() {
         return intervalSinceLastAnalyticsTableSuccess;
     }
 
-    @JsonIgnore
     public void setIntervalSinceLastAnalyticsTableSuccess(String date) {
         this.intervalSinceLastAnalyticsTableSuccess = date;
     }
 
-    @JsonIgnore
     public String getLastAnalyticsTableSuccess() {
         return lastAnalyticsTableSuccess;
     }
 
-    @JsonIgnore
     public void setLastAnalyticsTableSuccess(String lastAnalyticsTableSuccess) {
         this.lastAnalyticsTableSuccess = lastAnalyticsTableSuccess;
     }
 
-    @JsonIgnore
     public int getRevision() {
         return revision;
     }
 
-    @JsonIgnore
     public void setRevision(int revision) {
         this.revision = revision;
     }
 
-    @JsonIgnore
     public String getVersion() {
         return version;
     }
 
-    @JsonIgnore
     public void setVersion(String version) {
         this.version = version;
     }

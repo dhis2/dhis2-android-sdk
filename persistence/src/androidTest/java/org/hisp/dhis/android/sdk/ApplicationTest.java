3184a40adc33dd911095ae03fc0136a78bbecdb4
Araz Abishov <araz.abishov.gsoc@gmail.com>
2015-10-02 17:21:06 +0200

    Created new persistence module

--- persistence/src/androidTest/java/org/hisp/dhis/android/sdk/ApplicationTest.java
+++ persistence/src/androidTest/java/org/hisp/dhis/android/sdk/ApplicationTest.java
@@ -26,4 +26,16 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-include ':app', ':models', ':ui', ':core', ':persistence'
+package org.hisp.dhis.android.sdk;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
--- persistence/src/test/java/org/hisp/dhis/android/sdk/ExampleUnitTest.java
+++ persistence/src/test/java/org/hisp/dhis/android/sdk/ExampleUnitTest.java
@@ -26,4 +26,18 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-include ':app', ':models', ':ui', ':core', ':persistence'
+package org.hisp.dhis.android.sdk;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}

510567ac8c6fcd69f054689813d536d16a4919d0
erlingfjelstad <erlingfjelstad@gmail.com>
2015-12-14 09:42:14 +0100

    Implemented OptionSetScope

---
+++ core-android/src/main/java/org/hisp/dhis/android/sdk/optionset/IOptionSetScope.java
@@ -0,0 +1,19 @@
+package org.hisp.dhis.android.sdk.optionset;
+
+import org.hisp.dhis.java.sdk.models.optionset.OptionSet;
+
+import java.util.List;
+
+import rx.Observable;
+
+public interface IOptionSetScope {
+    Observable<OptionSet> get(String uid);
+
+    Observable<OptionSet> get(long id);
+
+    Observable<List<OptionSet>> list();
+
+    Observable<Boolean> save(OptionSet object);
+
+    Observable<Boolean> remove(OptionSet object);
+}

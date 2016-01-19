567ed683b29a21b6ca36bf9e248aa85d4ed2ddb0
vladisla <vladisla@student.matnat.uio.no>
2016-01-19 15:31:45 +0100

    Classes to facilitate showing of a confirmation dialog on logout.
    DialogFragment and UiUtils.showConfirmDialog.
    
    Also fixed the FontTextView and the other Font- variants in teh fragment_settings.xml.

--- ui/src/main/java/org/hisp/dhis/client/sdk/ui/fragments/ISettingsPresenter.java
+++ ui/src/main/java/org/hisp/dhis/client/sdk/ui/fragments/ISettingsPresenter.java
@@ -7,7 +7,7 @@ import android.content.Context;
  * Created by Vladislav Georgiev Alfredov on 1/18/16.
  */
 public interface ISettingsPresenter {
-    void logout();
+    void logout(Context context);
     void synchronize(Context context);
     void setUpdateFrequency(Context context, int frequency);
     int getUpdateFrequency(Context context);

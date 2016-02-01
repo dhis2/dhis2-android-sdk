43e9bc5780a8e4819bd6fc29dc2d5062c32f5d4b
erlingfjelstad <erlingfjelstad@gmail.com>
2016-02-01 08:49:22 +0100

    added stub classes of dataentryfragments

---
+++ ui/src/main/java/org/hisp/dhis/client/sdk/ui/fragments/AbsDataEntryFragment.java
@@ -0,0 +1,51 @@
+package org.hisp.dhis.client.sdk.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class AbsDataEntryFragment extends Fragment {
+    protected RecyclerView recyclerView;
+
+
+    public AbsDataEntryFragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void setDataEntryRows() {
+         recyclerView.setAdapter(null);
+    }
+
+    public RecyclerView getRecyclerView() {
+        return recyclerView;
+    }
+
+    public void setRecyclerView(RecyclerView recyclerView) {
+        this.recyclerView = recyclerView;
+    }
+}
---
+++ ui/src/main/java/org/hisp/dhis/client/sdk/ui/fragments/AbsProgramRuleFragment.java
@@ -0,0 +1,6 @@
+package org.hisp.dhis.client.sdk.ui.fragments;
+
+public class AbsProgramRuleFragment extends AbsDataEntryFragment {
+
+
+}

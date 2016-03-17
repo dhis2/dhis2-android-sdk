e7d6738fbc83d35b9cd18c799257bbbb7c7434fe
erlingfjelstad <erlingfjelstad@gmail.com>
2016-03-17 11:42:46 +0100

    Implemented EventStore2

---
+++ core-android/src/main/java/org/hisp/dhis/client/sdk/android/event/EventStore2.java
@@ -0,0 +1,50 @@
+package org.hisp.dhis.client.sdk.android.event;
+
+import com.raizlabs.android.dbflow.sql.language.Select;
+
+import org.hisp.dhis.client.sdk.android.api.persistence.flow.EventFlow;
+import org.hisp.dhis.client.sdk.android.api.persistence.flow.EventFlow_Table;
+import org.hisp.dhis.client.sdk.android.common.AbsIdentifiableObjectStore;
+import org.hisp.dhis.client.sdk.core.common.persistence.ITransactionManager;
+import org.hisp.dhis.client.sdk.core.event.IEventStore;
+import org.hisp.dhis.client.sdk.models.enrollment.Enrollment;
+import org.hisp.dhis.client.sdk.models.event.Event;
+import org.hisp.dhis.client.sdk.models.organisationunit.OrganisationUnit;
+import org.hisp.dhis.client.sdk.models.program.Program;
+
+import java.util.List;
+
+public class EventStore2 extends AbsIdentifiableObjectStore<Event, EventFlow>
+        implements IEventStore {
+    private final ITransactionManager transactionManager;
+
+    public EventStore2(ITransactionManager transactionManager) {
+        super(EventFlow.MAPPER);
+
+        this.transactionManager = transactionManager;
+    }
+
+    @Override
+    public List<Event> query(Enrollment enrollment) {
+        List<EventFlow> eventFlows = new Select()
+                .from(EventFlow.class)
+                .where(EventFlow_Table
+                        .enrollment.is(enrollment.getUId()))
+                .queryList();
+
+        return getMapper().mapToModels(eventFlows);
+    }
+
+    @Override
+    public List<Event> query(OrganisationUnit organisationUnit, Program program) {
+        List<EventFlow> eventFlows = new Select()
+                .from(EventFlow.class)
+                .where(EventFlow_Table
+                        .organisationUnitId.is(organisationUnit.getUId()))
+                .and(EventFlow_Table
+                .programId.is((program.getUId())))
+                .queryList();
+
+        return getMapper().mapToModels(eventFlows);
+    }
+}

e40b52f1c4a5784272e7ce038e96f9f1bab6f8f6
vladisla <vladisla@student.matnat.uio.no>
2016-03-23 10:26:45 +0100

    Added methods to store/retrieve lastSync date from SharedPreferences.
    Also disabled the Help and About buttons in the drawer.

--- ui/src/main/java/org/hisp/dhis/client/sdk/ui/SettingPreferences.java
+++ ui/src/main/java/org/hisp/dhis/client/sdk/ui/SettingPreferences.java
@@ -2,7 +2,11 @@ package org.hisp.dhis.client.sdk.ui;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.preference.PreferenceManager;
+
+import java.util.Date;
 
 import static org.hisp.dhis.client.sdk.ui.utils.Preconditions.isNull;
 
@@ -10,6 +14,7 @@ public final class SettingPreferences {
     public static final String BACKGROUND_SYNCHRONIZATION = "backgroundSynchronization";
     public static final String SYNCHRONIZATION_PERIOD = "synchronizationPeriod";
     public static final String CRASH_REPORTS = "crashReports";
+    public static final String SYNC_DATE = "syncDate";
 
     private static final String SYNCHRONIZATION_DEFAULT = "hoursTwentyFour";
 
@@ -42,4 +47,14 @@ public final class SettingPreferences {
     public static boolean crashReports() {
         return getInstance().sharedPreferences.getBoolean(CRASH_REPORTS, true);
     }
+
+    @Nullable
+    public static long getLastSynced() {
+        return getInstance().sharedPreferences.getLong(SYNC_DATE, 0l);
+    }
+
+    public static void setLastSynched(@NonNull long date) {
+        getInstance().sharedPreferences.edit().putLong(SYNC_DATE, date).commit();
+    }
+
 }

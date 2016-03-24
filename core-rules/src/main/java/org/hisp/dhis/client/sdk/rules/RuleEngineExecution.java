0c22e4338fb5a6483c576cd51623c66543c76c6b
Bekkalizer <markus.bekken@gmail.com>
2016-03-24 11:51:13 +0100

    Minor

--- core-rules/src/main/java/org/hisp/dhis/client/sdk/rules/RuleEngineExecution.java
+++ core-rules/src/main/java/org/hisp/dhis/client/sdk/rules/RuleEngineExecution.java
@@ -2,11 +2,12 @@ package org.hisp.dhis.client.sdk.rules;
 
 import org.hisp.dhis.client.sdk.models.program.ProgramRule;
 import org.hisp.dhis.client.sdk.models.program.ProgramRuleAction;
+import org.hisp.dhis.commons.util.ExpressionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hisp.dhis.commons.util.ExpressionUtils;
+import org.apache.commons.jexl2.*;
 
 /**
  * Created by markusbekken on 23.03.2016.
@@ -26,6 +27,26 @@ public class RuleEngineExecution {
         return effects;
     }
 
+    /**
+     * Evaluates a passed expression from a {@link ProgramRule} to true or false.
+     * @param condition
+     * @return
+     */
+    private static boolean evaluate(final String condition) {
+        boolean isTrue = false;
+        try {
+            isTrue = ExpressionUtils.isTrue(condition, null);
+        } catch(JexlException jxlException) {
+            jxlException.printStackTrace();
+        }
+        return isTrue;
+    }
+
+    /**
+     * Mapping method for creating a {@link RuleEffect} from a {@link ProgramRuleAction} object.
+     * @param action
+     * @return
+     */
     private static RuleEffect createEffect(ProgramRuleAction action) {
         RuleEffect effect = new RuleEffect();
         effect.setProgramRule(action.getProgramRule());

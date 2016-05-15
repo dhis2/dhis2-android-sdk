80cc0c2b95f2e547f930f64b484883ea9c5f3991
Bekkalizer <markus.bekken@gmail.com>
2016-05-15 22:56:46 +0200

    minor:naming

--- core-rules/src/test/java/org/hisp/dhis/client/sdk/rules/RulesEngineFunctionTests.java
+++ core-rules/src/test/java/org/hisp/dhis/client/sdk/rules/RulesEngineFunctionTests.java
@@ -42,7 +42,7 @@ import static org.hisp.dhis.client.sdk.rules.RulesEngineTestHelpers.*;
 
 public class RulesEngineFunctionTests {
     @Test
-    public void ruleEngineExecuteWarningRuleWithCurrentEventVariable() {
+    public void ruleEngineExecuteWarningSimpleD2Function() {
         //Metadata
         String errorMessage = "this error will occur if simpleBoolean has a value";
         ArrayList<ProgramRule> rules = new ArrayList<>();

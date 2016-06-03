2491c00719cdc69c35c4ef1a1a47bff706a05b76
Araz Abishov <araz.abishov.gsoc@gmail.com>
2016-06-03 15:17:34 +0200

    Imported LauncherActivity

---
+++ ui-bindings/src/main/java/org/hisp/dhis/client/sdk/ui/bindings/presenters/LauncherActivity.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2016, University of Oslo
+ *
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.client.sdk.ui.bindings.presenters;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import org.hisp.dhis.client.sdk.ui.bindings.R;
+import org.hisp.dhis.client.sdk.ui.bindings.views.LauncherView;
+
+public class LauncherActivity extends AppCompatActivity implements LauncherView {
+    private LauncherPresenter launcherPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        // injecting dependencies
+        // ((EventCaptureApp) getApplication()).getUserComponent().inject(this);
+
+        launcherPresenter.attachView(this);
+        launcherPresenter.checkIfUserIsLoggedIn();
+    }
+
+    @Override
+    public void navigateToLogin() {
+        // navigateTo(new Intent(this, LoginActivity.class));
+    }
+
+    @Override
+    public void navigateToHome() {
+        // navigateTo(new Intent(this, HomeActivity.class));
+    }
+
+    private void navigateTo(Intent intent) {
+        ActivityCompat.startActivity(this, intent, null);
+        finish();
+    }
+}

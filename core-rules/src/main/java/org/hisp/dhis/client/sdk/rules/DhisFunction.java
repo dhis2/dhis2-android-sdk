35bbd61d4005fa75c472855e414370ccdbb087f3
Bekkalizer <markus.bekken@gmail.com>
2016-06-08 15:12:54 +0200

    Removed ChronoUnit (Java 8) dependency :(

--- core-rules/src/main/java/org/hisp/dhis/client/sdk/rules/DhisFunction.java
+++ core-rules/src/main/java/org/hisp/dhis/client/sdk/rules/DhisFunction.java
@@ -4,37 +4,39 @@ import org.apache.commons.lang3.math.NumberUtils;
 import java.time.format.DateTimeFormatter;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * Created by markusbekken on 20.05.2016.
  */
 abstract class DhisFunction {
+
     private static List<DhisFunction> dhisFunctions = Arrays.asList(
         new DhisFunction("d2:daysBetween", 2) {
             @Override
             public String execute(List<String> parameters, RuleEngineVariableValueMap valueMap, String expression) {
-                return String.valueOf(getUnitsBetween(ChronoUnit.DAYS, parameters, expression));
+                return String.valueOf(getIntervalsBetween(1, parameters, expression));
             }
         },
         new DhisFunction("d2:weeksBetween", 2) {
             @Override
             public String execute(List<String> parameters, RuleEngineVariableValueMap valueMap, String expression) {
-                return String.valueOf(getUnitsBetween(ChronoUnit.WEEKS, parameters, expression));
+                return String.valueOf(getIntervalsBetween(7, parameters, expression));
             }
         },
         new DhisFunction("d2:monthsBetween", 2) {
             @Override
             public String execute(List<String> parameters, RuleEngineVariableValueMap valueMap, String expression) {
-                return String.valueOf(getUnitsBetween(ChronoUnit.MONTHS, parameters, expression));
+                throw new NotImplementedException();
             }
         },
         new DhisFunction("d2:yearsBetween", 2) {
             @Override
             public String execute(List<String> parameters, RuleEngineVariableValueMap valueMap, String expression) {
-                return String.valueOf(getUnitsBetween(ChronoUnit.YEARS, parameters, expression));
+                throw new NotImplementedException();
             }
         },
         new DhisFunction("d2:floor", 1) {
@@ -78,7 +80,7 @@ abstract class DhisFunction {
         new DhisFunction("d2:length", 1)*/);
     //TODO: Implement the rest of the functions
 
-    private static long getUnitsBetween(ChronoUnit unit, List<String> parameters, String expression) {
+    private static int getIntervalsBetween(int daysInInterval, List<String> parameters, String expression) {
         DateTimeFormatter f =  DateTimeFormatter.ofPattern(RuleEngineVariableValueMap.DATE_PATTERN);
 
         LocalDate d1;
@@ -86,7 +88,7 @@ abstract class DhisFunction {
         try {
             d1 = LocalDate.parse(parameters.get(0), f);
             d2 = LocalDate.parse(parameters.get(1), f);
-            return unit.between(d1, d2);
+            return new Long((d2.toEpochDay() - d1.toEpochDay()) / daysInInterval).intValue();
 
         }
         catch (Exception e) {

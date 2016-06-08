2b40f36499c61823ee760d82096bb0b74c7a8597
Araz Abishov <araz.abishov.gsoc@gmail.com>
2016-06-08 15:14:57 +0200

    Changed the way how dependencies are injected into ui-bindings module

--- ui-bindings/src/main/java/org/hisp/dhis/client/sdk/ui/bindings/commons/Inject.java
+++ ui-bindings/src/main/java/org/hisp/dhis/client/sdk/ui/bindings/commons/Inject.java
@@ -7,16 +7,26 @@ import static org.hisp.dhis.client.sdk.utils.Preconditions.isNull;
 public final class Inject {
     // singleton scope
     private static Inject inject;
-    private final DefaultAppModule defaultAppModule;
 
-    // user component which performs injection
+    private DefaultAppModule defaultAppModule;
     private DefaultUserModule defaultUserModule;
     private UserComponent userComponent;
 
-    private Inject(DefaultAppModule defaultAppModule, DefaultUserModule defaultUserModule) {
-        this.defaultAppModule = defaultAppModule;
-        this.defaultUserModule = defaultUserModule;
+    private ModuleProvider moduleProvider;
+
+    private Inject(ModuleProvider provider) {
+        this.moduleProvider = isNull(provider,
+                "ModuleProvider must not be null");
+        this.defaultAppModule = isNull(provider.provideAppModule(),
+                "DefaultAppModule must not be null");
+        this.defaultUserModule = isNull(provider.provideUserModule(null),
+                "DefaultUserModule must not be null");
         this.userComponent = new UserComponent(defaultAppModule, defaultUserModule);
+    }
+
+    private Inject(DefaultAppModule appModule, DefaultUserModule userModule) {
+        this.defaultAppModule = isNull(appModule, "DefaultAppModule must not be null");
+        this.defaultUserModule = isNull(userModule, "DefaultUserModule must not be null");
     }
 
     public static void init(Context context, String authority, String accountType) {
@@ -26,15 +36,21 @@ public final class Inject {
         inject = new Inject(defaultAppModule, defaultUserModule);
     }
 
-    public static void init(DefaultAppModule defaultAppModule, DefaultUserModule defaultUserModule) {
-        inject = new Inject(defaultAppModule, defaultUserModule);
+    public static void init(ModuleProvider provider) {
+        inject = new Inject(provider);
     }
 
     public static UserComponent createUserComponent(String serverUrl) {
         isNull(inject, "you must call init first");
 
-        inject.defaultUserModule = new DefaultUserModuleImpl(serverUrl);
-        inject.userComponent = new UserComponent(inject.defaultAppModule, inject.defaultUserModule);
+        if (inject.moduleProvider != null) {
+            inject.defaultUserModule = inject.moduleProvider.provideUserModule(serverUrl);
+        } else {
+            inject.defaultUserModule = new DefaultUserModuleImpl(serverUrl);
+        }
+
+        inject.userComponent = new UserComponent(
+                inject.defaultAppModule, inject.defaultUserModule);
 
         return inject.userComponent;
     }
@@ -50,5 +66,11 @@ public final class Inject {
 
         inject.defaultUserModule = null;
         inject.userComponent = null;
+    }
+
+    public interface ModuleProvider {
+        DefaultAppModule provideAppModule();
+
+        DefaultUserModule provideUserModule(String serverUrl);
     }
 }

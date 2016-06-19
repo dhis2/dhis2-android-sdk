7af961d3c36d0bdfd5f195a99ddf8844e1fd36e0
Laavanye Bahl <laavanye.bahl@gmail.com>
2016-06-19 13:38:18 +0530

    Moving AbsAdapter and GridDividerDecoration to SDK from dashboard project
    Signed-off-by: Laavanye Bahl <laavanye.bahl@gmail.com>

---
+++ ui/src/main/java/org/hisp/dhis/client/sdk/ui/views/GridDividerDecoration.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2015, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.client.sdk.ui.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import org.hisp.dhis.client.sdk.ui.R;
+
+/**
+ * ItemDecoration implementation that applies and inset margin
+ * around each child of the RecyclerView. It also draws item dividers
+ * that are expected from a vertical list implementation, such as
+ * ListView.
+ */
+public class GridDividerDecoration extends RecyclerView.ItemDecoration {
+
+    private static final int[] ATTRS = {android.R.attr.listDivider};
+
+    private Drawable mDivider;
+    private int mInsets;
+
+    public GridDividerDecoration(Context context) {
+        TypedArray a = context.obtainStyledAttributes(ATTRS);
+        mDivider = a.getDrawable(0);
+        a.recycle();
+
+        mInsets = context.getResources().getDimensionPixelSize(R.dimen.card_insets);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        drawVertical(c, parent);
+        drawHorizontal(c, parent);
+    }
+
+    /**
+     * Draw dividers at each expected grid interval
+     */
+    public void drawVertical(Canvas c, RecyclerView parent) {
+        if (parent.getChildCount() == 0) return;
+
+        final int left = parent.getPaddingLeft();
+        final int right = parent.getWidth() - parent.getPaddingRight();
+
+        final View child = parent.getChildAt(0);
+        if (child.getHeight() == 0) return;
+
+        final RecyclerView.LayoutParams params =
+                (RecyclerView.LayoutParams) child.getLayoutParams();
+        int top = child.getBottom() + params.bottomMargin + mInsets;
+        int bottom = top + mDivider.getIntrinsicHeight();
+
+        final int parentBottom = parent.getHeight() - parent.getPaddingBottom();
+        while (bottom < parentBottom) {
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+
+            top += mInsets + params.topMargin + child.getHeight() + params.bottomMargin + mInsets;
+            bottom = top + mDivider.getIntrinsicHeight();
+        }
+    }
+
+    /**
+     * Draw dividers to the right of each child view
+     */
+    public void drawHorizontal(Canvas c, RecyclerView parent) {
+        final int top = parent.getPaddingTop();
+        final int bottom = parent.getHeight() - parent.getPaddingBottom();
+
+        final int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final View child = parent.getChildAt(i);
+            final RecyclerView.LayoutParams params =
+                    (RecyclerView.LayoutParams) child.getLayoutParams();
+            final int left = child.getRight() + params.rightMargin + mInsets;
+            final int right = left + mDivider.getIntrinsicWidth();
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        //We can supply forced insets for each item view here in the Rect
+        outRect.set(mInsets, mInsets, mInsets, mInsets);
+    }
+}

ae5a39d91a26325444980d2047e438e4c5ebaec6
vladisla <vladisla@student.matnat.uio.no>
2016-07-13 15:45:17 +0200

    Uncommented the AboutFragment related code. (init)

--- ui/src/main/java/org/hisp/dhis/client/sdk/ui/activities/AbsHomeActivity.java
+++ ui/src/main/java/org/hisp/dhis/client/sdk/ui/activities/AbsHomeActivity.java
@@ -143,15 +143,14 @@ public abstract class AbsHomeActivity extends BaseActivity
         } else if (menuItemId == R.id.drawer_item_settings) {
             attachFragmentDelayed(getSettingsFragment());
             isSelected = true;
+        } else if (menuItemId == R.id.drawer_item_about) {
+            attachFragment(getAboutFragment());
+            isSelected = true;
         }
         /*else if (menuItemId == R.id.drawer_item_help) {
             attachFragment(getHelpFragment());
             isSelected = true;
-        } else if (menuItemId == R.id.drawer_item_about) {
-            attachFragment(getAboutFragment());
-            isSelected = true;
-        }*/
-
+        } */
         isSelected = onItemSelected(menuItem) || isSelected;
         if (isSelected) {
             navigationView.setCheckedItem(menuItemId);

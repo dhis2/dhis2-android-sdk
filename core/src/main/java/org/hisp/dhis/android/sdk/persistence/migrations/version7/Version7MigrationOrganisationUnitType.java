4f3f78a1855da12d845ca258a3adf1ef1a17af6a
erlingfjelstad <erlingfjelstad@gmail.com>
2016-07-26 12:20:51 +0200

    Added annotation to migration script for making it work

--- core/src/main/java/org/hisp/dhis/android/sdk/persistence/migrations/version7/Version7MigrationOrganisationUnitType.java
+++ core/src/main/java/org/hisp/dhis/android/sdk/persistence/migrations/version7/Version7MigrationOrganisationUnitType.java
@@ -1,14 +1,20 @@
 package org.hisp.dhis.android.sdk.persistence.migrations.version7;
 
+import com.raizlabs.android.dbflow.annotation.Migration;
 import com.raizlabs.android.dbflow.sql.migration.AlterTableMigration;
 
+import org.hisp.dhis.android.sdk.persistence.Dhis2Database;
 import org.hisp.dhis.android.sdk.persistence.models.OrganisationUnit;
 
+@Migration(version = 7, databaseName = Dhis2Database.NAME)
 public class Version7MigrationOrganisationUnitType  extends AlterTableMigration<OrganisationUnit> {
 
-
     public Version7MigrationOrganisationUnitType(Class<OrganisationUnit> table) {
-        super(table);
+        super(OrganisationUnit.class);
+    }
+
+    public Version7MigrationOrganisationUnitType() {
+        super(OrganisationUnit.class);
     }
 
     @Override

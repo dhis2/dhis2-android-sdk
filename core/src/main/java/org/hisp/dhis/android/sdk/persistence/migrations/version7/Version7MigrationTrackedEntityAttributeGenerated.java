0dcd706284293e9eea9f6718feadcd8e7a87c341
erlingfjelstad <erlingfjelstad@gmail.com>
2016-07-27 09:14:11 +0200

    Migration script for tea generated attribute

---
+++ core/src/main/java/org/hisp/dhis/android/sdk/persistence/migrations/version7/Version7MigrationTrackedEntityAttributeGenerated.java
@@ -0,0 +1,25 @@
+package org.hisp.dhis.android.sdk.persistence.migrations.version7;
+
+import com.raizlabs.android.dbflow.annotation.Migration;
+import com.raizlabs.android.dbflow.sql.migration.AlterTableMigration;
+
+import org.hisp.dhis.android.sdk.persistence.Dhis2Database;
+import org.hisp.dhis.android.sdk.persistence.models.TrackedEntityAttribute;
+
+@Migration(version = 7, databaseName = Dhis2Database.NAME)
+public class Version7MigrationTrackedEntityAttributeGenerated  extends AlterTableMigration<TrackedEntityAttribute> {
+
+    public Version7MigrationTrackedEntityAttributeGenerated(Class<TrackedEntityAttribute> table) {
+        super(TrackedEntityAttribute.class);
+    }
+
+    public Version7MigrationTrackedEntityAttributeGenerated() {
+        super(TrackedEntityAttribute.class);
+    }
+
+    @Override
+    public void onPreMigrate() {
+        super.onPreMigrate();
+        addColumn(Boolean.class, "generated");
+    }
+}

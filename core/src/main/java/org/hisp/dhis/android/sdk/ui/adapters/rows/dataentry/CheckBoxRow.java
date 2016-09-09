846a3b7aacaf61f50450110a103930e85a93c3fe
Simen Skogly Russnes <simen.s.russnes@gmail.com>
2016-09-09 14:40:26 +0200

    Implemented errors to show as you type in data entry

--- core/src/main/java/org/hisp/dhis/android/sdk/ui/adapters/rows/dataentry/CheckBoxRow.java
+++ core/src/main/java/org/hisp/dhis/android/sdk/ui/adapters/rows/dataentry/CheckBoxRow.java
@@ -131,7 +131,7 @@ public class CheckBoxRow extends Row {
             holder.errorLabel.setVisibility(View.GONE);
         } else {
             holder.errorLabel.setVisibility(View.VISIBLE);
-            holder.errorLabel.setText(mWarning);
+            holder.errorLabel.setText(mError);
         }
 
         if(!mMandatory) {

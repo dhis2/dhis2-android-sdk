a8973d6d7839f912f852c434f4a5136e7546f725
luckytommy <thomas.lindsjorn@gmail.com>
2016-09-09 14:20:08 +0200

    Add WARNINGONCOMPLETE ProgramRuleActionType (rule not supported yet, added to model to fix retrofit ConversionException) Fixes: http://crashes.to/s/8876843d44a

--- core/src/main/java/org/hisp/dhis/android/sdk/utils/api/ProgramRuleActionType.java
+++ core/src/main/java/org/hisp/dhis/android/sdk/utils/api/ProgramRuleActionType.java
@@ -30,29 +30,26 @@
 package org.hisp.dhis.android.sdk.utils.api;
 
 public enum ProgramRuleActionType {
-    DISPLAYTEXT( "displaytext" ),
-    DISPLAYKEYVALUEPAIR( "displaykeyvaluepair" ),
-    HIDEFIELD( "hidefield" ),
-    HIDESECTION( "hidesection" ),
-    ASSIGN( "assign" ),
-    SHOWWARNING( "showwarning" ),
-    SHOWERROR( "showerror" ),
+    DISPLAYTEXT("displaytext"),
+    DISPLAYKEYVALUEPAIR("displaykeyvaluepair"),
+    HIDEFIELD("hidefield"),
+    HIDESECTION("hidesection"),
+    ASSIGN("assign"),
+    SHOWWARNING("showwarning"),
+    SHOWERROR("showerror"),
     CREATEEVENT("createevent"),
-    ERRORONCOMPLETE("erroroncomplete");
-
+    ERRORONCOMPLETE("erroroncomplete"),
+    WARNINGONCOMPLETE("warningoncomplete");
+    
     final String value;
 
-    private ProgramRuleActionType( String value )
-    {
+    private ProgramRuleActionType(String value) {
         this.value = value;
     }
 
-    public static ProgramRuleActionType fromValue( String value )
-    {
-        for ( ProgramRuleActionType type : ProgramRuleActionType.values() )
-        {
-            if ( type.value.equalsIgnoreCase( value ) )
-            {
+    public static ProgramRuleActionType fromValue(String value) {
+        for (ProgramRuleActionType type : ProgramRuleActionType.values()) {
+            if (type.value.equalsIgnoreCase(value)) {
                 return type;
             }
         }

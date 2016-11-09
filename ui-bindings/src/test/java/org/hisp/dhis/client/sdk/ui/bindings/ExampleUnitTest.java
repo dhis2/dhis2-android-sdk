f94485be47e4418a4c0153028df79e33c3bf5b3a
luckytommy <thomas.lindsjorn@gmail.com>
2016-11-09 14:40:52 +0100

    Optimize imports

--- ui-bindings/src/test/java/org/hisp/dhis/client/sdk/ui/bindings/ExampleUnitTest.java
+++ ui-bindings/src/test/java/org/hisp/dhis/client/sdk/ui/bindings/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package org.hisp.dhis.client.sdk.ui.bindings;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).

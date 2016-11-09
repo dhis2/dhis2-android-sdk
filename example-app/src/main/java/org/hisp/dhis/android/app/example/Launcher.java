726d55d74ee74251ac313d9c2af992fd9f086bf2
luckytommy <thomas.lindsjorn@gmail.com>
2016-11-09 14:05:15 +0100

    Created example-app module

---
+++ example-app/src/main/java/org/hisp/dhis/android/app/example/Launcher.java
@@ -0,0 +1,12 @@
+package org.hisp.dhis.android.app.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Launcher extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

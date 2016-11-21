2b474da8040a67f5ebd56489fb9e1c655a020e24
luckytommy <thomas.lindsjorn@gmail.com>
2016-11-21 11:36:32 +0100

    Update EditTextRowView to use the new EditText Form Entities

--- ui/src/main/java/org/hisp/dhis/android/sdk/ui/rows/EditTextRowView.java
+++ ui/src/main/java/org/hisp/dhis/android/sdk/ui/rows/EditTextRowView.java
@@ -33,7 +33,6 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.text.Editable;
-import android.text.InputType;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -70,17 +69,7 @@ public final class EditTextRowView implements RowView {
 
         /* in order to improve performance, we pre-fetch
         all hints from resources */
-                /* in order to improve performance, we pre-fetch
-        all hints from resources */
         SparseArray<String> hintCache;
-
-        private final String enterText;
-        private final String enterLongText;
-        private final String enterNumber;
-        private final String enterInteger;
-        private final String enterPositiveInteger;
-        private final String enterPositiveOrZeroInteger;
-        private final String enterNegativeInteger;
 
         public final TextView textViewLabel;
         public final TextInputLayout textInputLayout;
@@ -98,17 +87,15 @@ public final class EditTextRowView implements RowView {
 
             Context context = itemView.getContext();
 
+            // caching hint strings
             hintCache = new SparseArray<>();
             hintCache.append(R.string.enter_text, context.getString(R.string.enter_text));
             hintCache.append(R.string.enter_long_text, context.getString(R.string.enter_long_text));
             hintCache.append(R.string.enter_number, context.getString(R.string.enter_number));
-            // fetching hint strings
-
-            enterNumber = context.getString(R.string.enter_number);
-            enterInteger = context.getString(R.string.enter_integer);
-            enterPositiveInteger = context.getString(R.string.enter_positive_integer);
-            enterPositiveOrZeroInteger = context.getString(R.string.enter_positive_integer_or_zero);
-            enterNegativeInteger = context.getString(R.string.enter_negative_integer);
+            hintCache.append(R.string.enter_integer, context.getString(R.string.enter_integer));
+            hintCache.append(R.string.enter_positive_integer, context.getString(R.string.enter_positive_integer));
+            hintCache.append(R.string.enter_positive_integer_or_zero, context.getString(R.string.enter_positive_integer_or_zero));
+            hintCache.append(R.string.enter_negative_integer, context.getString(R.string.enter_negative_integer));
 
             textViewLabel = (TextView) itemView
                     .findViewById(R.id.textview_row_label);
@@ -135,71 +122,18 @@ public final class EditTextRowView implements RowView {
             configureView(entity);
         }
 
-        private boolean configureView(FormEntityEditText dataEntityText) {
+        private void configureView(FormEntityEditText dataEntityText) {
 
             String hint = isEmpty(dataEntityText.getHint()) ?
-                    enterText : dataEntityText.getHint();
+                    hintCache.get(dataEntityText.getHintResourceId()) : dataEntityText.getHint();
 
-            return configure(hint, dataEntityText.getAndroidInputType(), dataEntityText.getMaxLines());
-
-            switch (dataEntityText.getInputType()) {
-                case TEXT: {
-                    String hint = isEmpty(dataEntityText.getHint()) ?
-                            enterText : dataEntityText.getHint();
-                    return configure(hint, InputType.TYPE_CLASS_TEXT, SHORT_TEXT_LINE_COUNT);
-                }
-                case LONG_TEXT: {
-                    String hint = isEmpty(dataEntityText.getHint()) ?
-                            enterLongText : dataEntityText.getHint();
-                    return configure(hint, InputType.TYPE_CLASS_TEXT, LONG_TEXT_LINE_COUNT);
-                }
-                case NUMBER: {
-                    String hint = isEmpty(dataEntityText.getHint()) ?
-                            enterNumber : dataEntityText.getHint();
-                    return configure(hint,
-                            InputType.TYPE_CLASS_NUMBER |
-                                    InputType.TYPE_NUMBER_FLAG_DECIMAL |
-                                    InputType.TYPE_NUMBER_FLAG_SIGNED, SHORT_TEXT_LINE_COUNT);
-                }
-                case INTEGER: {
-                    String hint = isEmpty(dataEntityText.getHint()) ?
-                            enterInteger : dataEntityText.getHint();
-                    return configure(hint,
-                            InputType.TYPE_CLASS_NUMBER |
-                                    InputType.TYPE_NUMBER_FLAG_SIGNED, SHORT_TEXT_LINE_COUNT);
-                }
-                case INTEGER_NEGATIVE: {
-                    String hint = isEmpty(dataEntityText.getHint()) ?
-                            enterNegativeInteger : dataEntityText.getHint();
-                    return configure(hint,
-                            InputType.TYPE_CLASS_NUMBER |
-                                    InputType.TYPE_NUMBER_FLAG_SIGNED, SHORT_TEXT_LINE_COUNT);
-                }
-                case INTEGER_ZERO_OR_POSITIVE: {
-                    String hint = isEmpty(dataEntityText.getHint()) ?
-                            enterPositiveOrZeroInteger : dataEntityText.getHint();
-                    return configure(hint, InputType.TYPE_CLASS_NUMBER, SHORT_TEXT_LINE_COUNT);
-                }
-                case INTEGER_POSITIVE: {
-                    String hint = isEmpty(dataEntityText.getHint()) ?
-                            enterPositiveInteger : dataEntityText.getHint();
-                    return configure(hint, InputType.TYPE_CLASS_NUMBER, SHORT_TEXT_LINE_COUNT);
-                }
-                default:
-                    return false;
-            }
-        }
-
-        private boolean configure(String hint, int inputType, int maxLines) {
             String textInputLayoutHint = isEmpty(editText.getText()) ? hint : null;
 
             onFocusChangeListener.setHint(hint);
             textInputLayout.setHint(textInputLayoutHint);
 
-            editText.setInputType(inputType);
-            editText.setMaxLines(maxLines);
-
-            return true;
+            editText.setInputType(dataEntityText.getAndroidInputType());
+            editText.setMaxLines(dataEntityText.getMaxLines());
         }
     }
 

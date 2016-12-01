0e54562a8a333a42e09f31ee74227d9f4a3e2ca8
luckytommy <thomas.lindsjorn@gmail.com>
2016-12-01 16:57:34 +0100

    Implement Parcelable in FormEntityText

--- ui/src/main/java/org/hisp/dhis/client/sdk/ui/models/FormEntityText.java
+++ ui/src/main/java/org/hisp/dhis/client/sdk/ui/models/FormEntityText.java
@@ -1,8 +1,10 @@
 package org.hisp.dhis.client.sdk.ui.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
-public class FormEntityText extends FormEntityCharSequence {
+public class FormEntityText extends FormEntityCharSequence implements Parcelable {
 
     public FormEntityText(String id, String label) {
         this(id, label, null);
@@ -17,4 +19,30 @@ public class FormEntityText extends FormEntityCharSequence {
     public Type getType() {
         return Type.TEXT;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(getId());
+        dest.writeString(getLabel());
+        dest.writeString(getValue().toString());
+    }
+
+    public static final Parcelable.Creator<FormEntityText> CREATOR
+            = new Parcelable.Creator<FormEntityText>() {
+        public FormEntityText createFromParcel(Parcel in) {
+
+            FormEntityText formEntityText = new FormEntityText(in.readString(), in.readString());
+            formEntityText.setValue(in.readString(), false);
+            return formEntityText;
+        }
+
+        public FormEntityText[] newArray(int size) {
+            return new FormEntityText[size];
+        }
+    };
 }

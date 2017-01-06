fb46530c0df555ae0cb52b6db3b650614e1ff522
vladisla <vladisla@student.matnat.uio.no>
2017-01-06 15:41:50 +0100

    RelationshipTypeStore implementation and integration test.

---
+++ core/src/main/java/org/hisp/dhis/android/core/relationship/RelationshipTypeStoreImpl.java
@@ -0,0 +1,63 @@
+package org.hisp.dhis.android.core.relationship;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import org.hisp.dhis.android.core.data.database.DbOpenHelper;
+
+import java.util.Date;
+
+import static org.hisp.dhis.android.core.common.StoreUtils.sqLiteBind;
+
+public class RelationshipTypeStoreImpl implements RelationshipTypeStore {
+
+    private static final String INSERT_STATEMENT = "INSERT INTO " +
+            DbOpenHelper.Tables.RELATIONSHIP_TYPE + " (" +
+            RelationshipTypeContract.Columns.UID + ", " +
+            RelationshipTypeContract.Columns.CODE + ", " +
+            RelationshipTypeContract.Columns.NAME + ", " +
+            RelationshipTypeContract.Columns.DISPLAY_NAME + ", " +
+            RelationshipTypeContract.Columns.CREATED + ", " +
+            RelationshipTypeContract.Columns.LAST_UPDATED + ", " +
+            RelationshipTypeContract.Columns.A_IS_TO_B + ", " +
+            RelationshipTypeContract.Columns.B_IS_TO_A + ") " +
+            "VALUES (" + "?, ?, ?, ?, ?, ?, ?, ?" + ");";
+
+    private final SQLiteStatement sqLiteStatement;
+
+    RelationshipTypeStoreImpl(SQLiteDatabase sqLiteDatabase) {
+        this.sqLiteStatement = sqLiteDatabase.compileStatement(INSERT_STATEMENT);
+    }
+
+    @Override
+    public long insert(
+            @NonNull String uid,
+            @Nullable String code,
+            @NonNull String name,
+            @Nullable String displayName,
+            @Nullable Date created,
+            @Nullable Date lastUpdated,
+            @NonNull String aIsToB,
+            @NonNull String bIsToA
+    ) {
+        sqLiteStatement.clearBindings();
+
+        sqLiteBind(sqLiteStatement, 1, uid);
+        sqLiteBind(sqLiteStatement, 2, code);
+        sqLiteBind(sqLiteStatement, 3, name);
+        sqLiteBind(sqLiteStatement, 4, displayName);
+        sqLiteBind(sqLiteStatement, 5, created);
+        sqLiteBind(sqLiteStatement, 6, lastUpdated);
+        sqLiteBind(sqLiteStatement, 7, aIsToB);
+        sqLiteBind(sqLiteStatement, 8, bIsToA);
+
+        return sqLiteStatement.executeInsert();
+    }
+
+    @Override
+    public void close() {
+        sqLiteStatement.close();
+    }
+}

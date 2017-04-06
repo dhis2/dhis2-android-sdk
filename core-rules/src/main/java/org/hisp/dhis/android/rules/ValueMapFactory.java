df02ea68a546a3189b4ee742705fb0a1ce7669a8
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-04-06 09:18:58 +0200

    (WIP) ValueMapFactory implementation and tests.

--- core-rules/src/main/java/org/hisp/dhis/android/rules/ValueMapFactory.java
+++ core-rules/src/main/java/org/hisp/dhis/android/rules/ValueMapFactory.java
@@ -1,6 +1,7 @@
 package org.hisp.dhis.android.rules;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -24,11 +25,15 @@ final class ValueMapFactory {
 
     // context for execution
     private final List<ProgramRuleVariable> programRuleVariables;
+    private final List<TrackedEntityAttributeValue> teAttributeValues;
     private final List<Event> events;
 
-    ValueMapFactory(List<ProgramRuleVariable> variables, List<Event> events) {
+    ValueMapFactory(List<ProgramRuleVariable> variables,
+            List<TrackedEntityAttributeValue> teAttributeValues,
+            List<Event> events) {
         this.dateFormat = new SimpleDateFormat(DATE_PATTERN, Locale.US);
         this.programRuleVariables = variables;
+        this.teAttributeValues = teAttributeValues;
         this.events = events;
     }
 
@@ -37,10 +42,188 @@ final class ValueMapFactory {
         Map<String, ProgramRuleVariableValue> valueMap = new HashMap<>();
 
         // add environment variables
-        valueMap.put(VAR_CURRENT_DATE, create(dateFormat.format(new Date()), "date_type", true));
-        valueMap.put(VAR_EVENT_DATE, create(dateFormat.format(currentEvent.eventDate()), "date_type", true));
+        valueMap.put(VAR_CURRENT_DATE, create(dateFormat.format(
+                new Date()), ValueType.DATE, true));
+        valueMap.put(VAR_EVENT_DATE, create(dateFormat.format(
+                currentEvent.eventDate()), ValueType.DATE, true));
+
+        Map<String, TrackedEntityDataValue> currentEventValues
+                = mapDeToSingleValue(currentEvent.dataValues());
+        Map<String, TrackedEntityAttributeValue> teiValues
+                = mapTeaToSingleValue(teAttributeValues);
+        Map<String, List<TrackedEntityDataValue>> eventsValues
+                = mapDeToAllValues(events);
+
+        // iterate over variables and collect values
+        for (ProgramRuleVariable programRuleVariable : programRuleVariables) {
+            ProgramRuleVariableValue programRuleVariableValue
+                    = match(programRuleVariable, teiValues, currentEventValues, eventsValues);
+            valueMap.put(programRuleVariable.name(), programRuleVariableValue);
+        }
 
         // do not let outer world to alter variable value map
         return Collections.unmodifiableMap(valueMap);
+    }
+
+    @Nonnull
+    private static ProgramRuleVariableValue match(@Nonnull ProgramRuleVariable variable,
+            @Nonnull Map<String, TrackedEntityAttributeValue> attributeValues,
+            @Nonnull Map<String, TrackedEntityDataValue> currentEventValues,
+            @Nonnull Map<String, List<TrackedEntityDataValue>> eventsValues) {
+        switch (variable.sourceType()) {
+            case DATAELEMENT_CURRENT_EVENT:
+                return dataElementCurrentEvent(variable, currentEventValues);
+            case DATAELEMENT_NEWEST_EVENT_PROGRAM:
+                return dataElementNewestEvent(variable, eventsValues);
+            case DATAELEMENT_NEWEST_EVENT_PROGRAM_STAGE:
+                return null;
+            case DATAELEMENT_PREVIOUS_EVENT:
+                return null;
+            case TEI_ATTRIBUTE:
+                return trackedEntityAttribute(variable, attributeValues);
+            case CALCULATED_VALUE:
+                // Do nothing - ASSIGN actions will populate these values
+                break;
+            default: {
+                throw new IllegalArgumentException(String.format(Locale.US,
+                        "sourceType %s is not supported", variable.sourceType()));
+            }
+        }
+
+        return null;
+    }
+
+    @Nonnull
+    private static ProgramRuleVariableValue dataElementCurrentEvent(
+            @Nonnull ProgramRuleVariable variable,
+            @Nonnull Map<String, TrackedEntityDataValue> valueMap) {
+        assert variable.dataElement() != null : variable.dataElementValueType() != null;
+
+        if (valueMap.containsKey(variable.dataElement())) {
+            TrackedEntityDataValue dataValue = valueMap.get(variable.dataElement());
+            return ProgramRuleVariableValue.create(dataValue.value(),
+                    variable.dataElementValueType(), true);
+        }
+
+        return ProgramRuleVariableValue.create(variable.dataElementValueType());
+    }
+
+    // ToDo: tests
+    @Nonnull
+    private static ProgramRuleVariableValue dataElementNewestEvent(
+            @Nonnull ProgramRuleVariable variable,
+            @Nonnull Map<String, List<TrackedEntityDataValue>> valueMap) {
+        assert variable.dataElement() != null : variable.dataElementValueType() != null;
+
+        if (valueMap.containsKey(variable.dataElement())) {
+            List<TrackedEntityDataValue> dataValues = valueMap.get(variable.dataElement());
+
+            if (!dataValues.isEmpty()) {
+                // this data value corresponds to the data value within
+                // latest / newest event for given data element
+                TrackedEntityDataValue latestDataValue = dataValues.get(dataValues.size() - 1);
+                return ProgramRuleVariableValue.create(latestDataValue.value(),
+                        variable.dataElementValueType(), true);
+            }
+        }
+
+        return ProgramRuleVariableValue.create(variable.dataElementValueType());
+    }
+
+    // ToDo: tests
+    @Nonnull
+    private static ProgramRuleVariableValue trackedEntityAttribute(
+            @Nonnull ProgramRuleVariable variable,
+            @Nonnull Map<String, TrackedEntityAttributeValue> valueMap) {
+        assert variable.trackedEntityAttribute() != null
+                : variable.trackedEntityAttributeType() != null;
+
+        if (valueMap.containsKey(variable.trackedEntityAttribute())) {
+            TrackedEntityAttributeValue attributeValue
+                    = valueMap.get(variable.trackedEntityAttribute());
+            return ProgramRuleVariableValue.create(attributeValue.value(),
+                    variable.trackedEntityAttributeType(), true);
+        }
+
+        return ProgramRuleVariableValue.create(variable.trackedEntityAttributeType());
+    }
+
+    /**
+     * Returns a map where the key is uid of TrackedEntityAttribute
+     * and value is the actual TrackedEntityAttributeValue.
+     *
+     * @param values List of attribute values
+     * @return Map of tracked entity attributes to data values
+     */
+    @Nonnull
+    private static Map<String, TrackedEntityAttributeValue> mapTeaToSingleValue(
+            @Nonnull List<TrackedEntityAttributeValue> values) {
+        Map<String, TrackedEntityAttributeValue> valueMap = new HashMap<>(values.size());
+        for (TrackedEntityAttributeValue attributeValue : values) {
+            valueMap.put(attributeValue.trackedEntityAttribute(), attributeValue);
+        }
+        return valueMap;
+    }
+
+    /**
+     * Returns a map where the key is uid of DataElement
+     * and value is the actual TrackedEntityDataValue.
+     *
+     * @param dataValues List of data values
+     * @return Map of data elements to data values
+     */
+    @Nonnull
+    private static Map<String, TrackedEntityDataValue> mapDeToSingleValue(
+            @Nonnull List<TrackedEntityDataValue> dataValues) {
+        Map<String, TrackedEntityDataValue> valueMap = new HashMap<>(dataValues.size());
+        for (TrackedEntityDataValue dataValue : dataValues) {
+            valueMap.put(dataValue.dataElement(), dataValue);
+        }
+        return valueMap;
+    }
+
+    /**
+     * Returns a map where the key is uid of DataElement
+     * and value is the list of data values from given events
+     *
+     * @param events List of events
+     * @return Map of data elements to list of all data values
+     */
+    @Nonnull
+    private static Map<String, List<TrackedEntityDataValue>> mapDeToAllValues(
+            @Nonnull List<Event> events) {
+        if (events.isEmpty()) {
+            return new HashMap<>();
+        }
+
+        // using one event to predict size of the map based on amount of data values.
+        // this helps to improve performance, since map doesn't have to resize
+        // during iteration.
+        Event firstEvent = events.get(0);
+
+        // since keys in the map are data elements, we can approximately set the size of
+        // the map based on data values in event
+        Map<String, List<TrackedEntityDataValue>> valueMap
+                = new HashMap<>(firstEvent.dataValues().size());
+
+        // sort events by event date:
+        Collections.sort(events, Event.EVENT_DATE_COMPARATOR);
+
+        // build the value map
+        for (Event event : events) {
+            for (TrackedEntityDataValue dataValue : event.dataValues()) {
+                // push new list if it is not there
+                // for the given data element
+                if (!valueMap.containsKey(dataValue.dataElement())) {
+                    valueMap.put(dataValue.dataElement(),
+                            new ArrayList<TrackedEntityDataValue>(events.size()));
+                }
+
+                // append data value to the list
+                valueMap.get(dataValue.dataElement()).add(dataValue);
+            }
+        }
+
+        return new HashMap<>();
     }
 }
--- core-rules/src/test/java/org/hisp/dhis/android/rules/ValueMapFactoryTests.java
+++ core-rules/src/test/java/org/hisp/dhis/android/rules/ValueMapFactoryTests.java
@@ -10,6 +10,7 @@ import org.mockito.MockitoAnnotations;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Map;
@@ -25,10 +26,12 @@ public class ValueMapFactoryTests {
     @Mock
     private Event event;
 
+    private SimpleDateFormat dateFormat;
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_PATTERN, Locale.US);
+        dateFormat = new SimpleDateFormat(DATE_PATTERN, Locale.US);
 
         Date eventDate = dateFormat.parse("1994-02-03");
         when(event.eventDate()).thenReturn(eventDate);
@@ -38,6 +41,7 @@ public class ValueMapFactoryTests {
     public void buildShouldReturnImmutableMap() {
         ValueMapFactory valueMapFactory = new ValueMapFactory(
                 new ArrayList<ProgramRuleVariable>(),
+                new ArrayList<TrackedEntityDataValue>(),
                 new ArrayList<Event>()
         );
 
@@ -53,6 +57,7 @@ public class ValueMapFactoryTests {
     public void buildShouldReturnMapWithEnvVariables() throws ParseException {
         ValueMapFactory valueMapFactory = new ValueMapFactory(
                 new ArrayList<ProgramRuleVariable>(),
+                new ArrayList<TrackedEntityDataValue>(),
                 new ArrayList<Event>()
         );
 
@@ -60,8 +65,68 @@ public class ValueMapFactoryTests {
 
         assertThat(valueMap.get("event_date").hasValue()).isTrue();
         assertThat(valueMap.get("event_date").value()).isEqualTo("1994-02-03");
+        assertThat(valueMap.get("current_date").hasValue()).isTrue();
+        assertThat(valueMap.get("current_date").value()).isEqualTo(dateFormat.format(new Date()));
+    }
 
-        // ToDo: inject current date through constructor in order to achieve testability?
-        // assertThat(valueMap.get("current_date")).isEqualTo();
+    @Test
+    public void dataElementCurrentEventSourceTypeShouldBeHandledCorrectly() {
+        ProgramRuleVariable ruleVariableOne = ProgramRuleVariable.forDataElement(
+                "test_variable_one", "test_program_stage", "test_dataelement_one", ValueType.TEXT,
+                false, ProgramRuleVariableSourceType.DATAELEMENT_CURRENT_EVENT, new ArrayList<Option>());
+        ProgramRuleVariable ruleVariableTwo = ProgramRuleVariable.forDataElement(
+                "test_variable_two", "test_program_stage", "test_dataelement_two", ValueType.TEXT,
+                false, ProgramRuleVariableSourceType.DATAELEMENT_CURRENT_EVENT, new ArrayList<Option>());
+
+        Event currentEvent = Event.create("test_event_uid", EventStatus.ACTIVE,
+                "test_program_stage", new Date(), new Date(), Arrays.asList(
+                        TrackedEntityDataValue.create(event, "test_dataelement_one", "test_value_one"),
+                        TrackedEntityDataValue.create(event, "test_dataelement_two", "test_value_two")
+                ));
+
+        ValueMapFactory valueMapFactory = new ValueMapFactory(
+                Arrays.asList(ruleVariableOne, ruleVariableTwo), new ArrayList<TrackedEntityDataValue>(), new ArrayList<Event>()
+        );
+
+        // here we will expect correct values to be returned
+        Map<String, ProgramRuleVariableValue> valueMap = valueMapFactory.build(currentEvent);
+
+        // 2 values are coming from ruleVariableOne and ruleVariableTwo,
+        // while 2 others from environment variables
+        assertThat(valueMap.size()).isEqualTo(4);
+
+        // first variable
+        assertThat(valueMap.get("test_variable_one").value()).isEqualTo("test_value_one");
+        assertThat(valueMap.get("test_variable_one").hasValue()).isEqualTo(true);
+        assertThat(valueMap.get("test_variable_one").valueType()).isEqualTo(ValueType.TEXT);
+
+        // second variable
+        assertThat(valueMap.get("test_variable_two").value()).isEqualTo("test_value_two");
+        assertThat(valueMap.get("test_variable_two").hasValue()).isEqualTo(true);
+        assertThat(valueMap.get("test_variable_two").valueType()).isEqualTo(ValueType.TEXT);
+    }
+
+    @Test
+    public void dataElementNewestEventSourceTypeShouldBeHandledCorrectly() {
+        ProgramRuleVariable ruleVariableOne = ProgramRuleVariable.forDataElement(
+                "test_variable_one", "test_program_stage", "test_dataelement_one", ValueType.TEXT,
+                false, ProgramRuleVariableSourceType.DATAELEMENT_NEWEST_EVENT_PROGRAM, new ArrayList<Option>());
+        ProgramRuleVariable ruleVariableTwo = ProgramRuleVariable.forDataElement(
+                "test_variable_two", "test_program_stage", "test_dataelement_two", ValueType.TEXT,
+                false, ProgramRuleVariableSourceType.DATAELEMENT_NEWEST_EVENT_PROGRAM, new ArrayList<Option>());
+
+        Event currentEvent = Event.create("test_event_uid", EventStatus.ACTIVE,
+                "test_program_stage", new Date(), new Date(), Arrays.asList(
+                        TrackedEntityDataValue.create(event, "test_dataelement_one", "test_value_one"),
+                        TrackedEntityDataValue.create(event, "test_dataelement_two", "test_value_two")
+                ));
+
+        ValueMapFactory valueMapFactory = new ValueMapFactory(
+                Arrays.asList(ruleVariableOne, ruleVariableTwo),
+                new ArrayList<TrackedEntityDataValue>(),
+                Arrays.<Event>asList());
+
+        //
+        Map<String, ProgramRuleVariableValue> valueMap = valueMapFactory.build(currentEvent);
     }
 }

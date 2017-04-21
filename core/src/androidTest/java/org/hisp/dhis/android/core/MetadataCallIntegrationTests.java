42b31327528e5ecdbec86d6a77e51e1c1b32a6c4
Vladislav Georgiev Alfredov <vlad.g.alf@gmail.com>
2017-04-21 15:27:55 +0200

    metadataCallIntegrationTests for testing

---
+++ core/src/androidTest/java/org/hisp/dhis/android/core/MetadataCallIntegrationTests.java
@@ -0,0 +1,55 @@
+package org.hisp.dhis.android.core;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.hisp.dhis.android.core.configuration.ConfigurationModel;
+import org.hisp.dhis.android.core.data.api.BasicAuthenticatorFactory;
+import org.hisp.dhis.android.core.data.database.AbsStoreTestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public class MetadataCallIntegrationTests extends AbsStoreTestCase {
+    /**
+     * A quick integration test that is probably flaky, but will help with finding bugs related to the
+     * metadataSyncCall. It works against the demo server.
+     */
+    private D2 d2;
+    Exception e;
+
+    @Before
+    @Override
+    public void setUp() throws IOException {
+        super.setUp();
+
+        ConfigurationModel config = ConfigurationModel.builder()
+                .serverUrl(HttpUrl.parse("https://play.dhis2.org/dev/api/"))
+                .build();
+
+        d2 = new D2.Builder()
+                .configuration(config)
+                .databaseAdapter(databaseAdapter())
+                .okHttpClient(
+                        new OkHttpClient.Builder()
+                                .addInterceptor(BasicAuthenticatorFactory.create(databaseAdapter()))
+                                .build()
+                )
+                .build();
+    }
+
+    @Test
+    public void metadataSyncTest() throws Exception {
+        retrofit2.Response response = null;
+        response = d2.logIn("android", "Android123").call();
+        response = d2.syncMetaData().call();
+        assertThat(response.isSuccessful()).isTrue();
+    }
+}

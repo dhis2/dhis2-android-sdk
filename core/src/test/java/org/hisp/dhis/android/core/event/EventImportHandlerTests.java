e1506b9df9271733f560c7df2554c4b4e0d8855c
erlingfjelstad <erlingfjelstad@gmail.com>
2017-05-30 16:28:16 +0200

    Adding missing tests for Enrollment and Event import handlers

--- core/src/test/java/org/hisp/dhis/android/core/event/EventImportHandlerTests.java
+++ core/src/test/java/org/hisp/dhis/android/core/event/EventImportHandlerTests.java
@@ -1,32 +1,73 @@
 package org.hisp.dhis.android.core.event;
-/*
- * Copyright (c) 2017, University of Oslo
- *
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * Neither the name of the HISP project nor the names of its contributors may
- * be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- $($PACKAGE)
 
+import org.hisp.dhis.android.core.common.State;
+import org.hisp.dhis.android.core.imports.ImportStatus;
+import org.hisp.dhis.android.core.imports.ImportSummary;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(JUnit4.class)
 public class EventImportHandlerTests {
+    @Mock
+    private List<ImportSummary> importSummaries;
+
+    @Mock
+    private ImportSummary importSummary;
+
+    @Mock
+    private EventStore eventStore;
+
+    // object to test
+    private EventImportHandler eventImportHandler;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        when(importSummary.importStatus()).thenReturn(ImportStatus.SUCCESS);
+
+        eventImportHandler = new EventImportHandler(eventStore);
+
+    }
+
+    @Test
+    public void doNothing_shouldDoNothingWhenPassingNullArgument() throws Exception {
+        eventImportHandler.handleEventImportSummaries(null);
+
+        verify(eventStore, never()).setState(anyString(), any(State.class));
+    }
+
+    @Test
+    public void setState_shouldUpdateEventStateSynced() throws Exception {
+        when(importSummary.importStatus()).thenReturn(ImportStatus.SUCCESS);
+        when(importSummary.reference()).thenReturn("test_event_uid");
+
+        eventImportHandler.handleEventImportSummaries(Collections.singletonList(importSummary));
+
+        verify(eventStore, times(1)).setState("test_event_uid", State.SYNCED);
+    }
+
+    @Test
+    public void setState_shouldUpdateEventStateError() throws Exception {
+        when(importSummary.importStatus()).thenReturn(ImportStatus.ERROR);
+        when(importSummary.reference()).thenReturn("test_event_uid");
+
+        eventImportHandler.handleEventImportSummaries(Collections.singletonList(importSummary));
+
+        verify(eventStore, times(1)).setState("test_event_uid", State.ERROR);
+    }
 }

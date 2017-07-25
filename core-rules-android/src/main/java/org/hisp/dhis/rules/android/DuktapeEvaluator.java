8fed6040a727e4b215a004284c3cf00c069704ae
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-07-25 16:27:21 +0400

    Created a new android module for ProgramRules.

---
+++ core-rules-android/src/main/java/org/hisp/dhis/rules/android/DuktapeEvaluator.java
@@ -0,0 +1,14 @@
+package org.hisp.dhis.rules.android;
+
+import org.hisp.dhis.rules.RuleExpressionEvaluator;
+
+import javax.annotation.Nonnull;
+
+public final class DuktapeEvaluator implements RuleExpressionEvaluator {
+
+    @Nonnull
+    @Override
+    public String evaluate(@Nonnull String expression) {
+        return null;
+    }
+}

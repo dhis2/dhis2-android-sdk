31c6c57e0f2440894c4b7cd73708aeb4c6422870
Jorge Sánchez <xurxosf@gmail.com>
2017-11-30 08:36:24 +0100

    optimize test code (retrofit duplication, names ...)

--- core/src/androidTest/java/org/hisp/dhis/android/core/event/EventCallShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/event/EventCallShould.java
@@ -3,15 +3,9 @@ package org.hisp.dhis.android.core.event;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.hisp.dhis.android.core.D2;
-import org.hisp.dhis.android.core.common.BaseIdentifiableObject;
 import org.hisp.dhis.android.core.configuration.ConfigurationModel;
 import org.hisp.dhis.android.core.data.api.BasicAuthenticatorFactory;
-import org.hisp.dhis.android.core.data.api.FieldsConverterFactory;
-import org.hisp.dhis.android.core.data.api.FilterConverterFactory;
 import org.hisp.dhis.android.core.data.database.AbsStoreTestCase;
 import org.hisp.dhis.android.core.data.server.Dhis2MockServer;
 import org.hisp.dhis.android.core.resource.ResourceHandler;
@@ -28,18 +22,36 @@ import java.util.List;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
+
 public class EventCallShould extends AbsStoreTestCase{
 
     private Dhis2MockServer dhis2MockServer;
+    private D2 d2;
 
     @Override
     @Before
     public void setUp() throws IOException {
         deleteDatabase();
         super.setUp();
+
         dhis2MockServer = new Dhis2MockServer();
+
+        ConfigurationModel config = ConfigurationModel.builder()
+                .serverUrl(HttpUrl.parse(dhis2MockServer.getBaseEndpoint()))
+                .build();
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+        d2 = new D2.Builder()
+                .configuration(config)
+                .databaseAdapter(databaseAdapter())
+                .okHttpClient(
+                        new OkHttpClient.Builder()
+                                .addInterceptor(BasicAuthenticatorFactory.create(databaseAdapter()))
+                                .addInterceptor(loggingInterceptor)
+                                .build()
+                ).build();
     }
 
     @Override
@@ -52,8 +64,9 @@ public class EventCallShould extends AbsStoreTestCase{
 
     @Test
     public void download_number_of_events_according_to_default_limit() throws Exception {
-        insertMetadata();
-        EventCall eventCall = givenASimpleEventCall();
+        givenAMetadataInDatabase();
+
+        EventCall eventCall = givenADefaultEventCall();
 
         dhis2MockServer.enqueueMockResponse("events_1.json");
 
@@ -62,30 +75,13 @@ public class EventCallShould extends AbsStoreTestCase{
         verifyDownloadedEvents(50);
     }
 
-    private void insertMetadata() throws Exception {
+    private void givenAMetadataInDatabase() throws Exception {
         dhis2MockServer.enqueueMockResponse("system_info.json");
         dhis2MockServer.enqueueMockResponse("user.json");
         dhis2MockServer.enqueueMockResponse("organisationUnits.json");
         dhis2MockServer.enqueueMockResponse("programs.json");
         dhis2MockServer.enqueueMockResponse("tracked_entities.json");
         dhis2MockServer.enqueueMockResponse("option_sets.json");
-
-        ConfigurationModel config = ConfigurationModel.builder()
-                .serverUrl(HttpUrl.parse(dhis2MockServer.getBaseEndpoint()))
-                .build();
-
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
-
-        D2 d2 = new D2.Builder()
-                .configuration(config)
-                .databaseAdapter(databaseAdapter())
-                .okHttpClient(
-                        new OkHttpClient.Builder()
-                                .addInterceptor(BasicAuthenticatorFactory.create(databaseAdapter()))
-                                .addInterceptor(loggingInterceptor)
-                                .build()
-                ).build();
 
         d2.syncMetaData().call();
     }
@@ -98,21 +94,11 @@ public class EventCallShould extends AbsStoreTestCase{
         assertThat(downloadedEvents.size(), is(numEvents));
     }
 
-    private EventCall givenASimpleEventCall() {
-        ObjectMapper objectMapper = new ObjectMapper()
-                .setDateFormat(BaseIdentifiableObject.DATE_FORMAT.raw())
-                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(dhis2MockServer.getBaseEndpoint())
-                .addConverterFactory(JacksonConverterFactory.create(objectMapper))
-                .addConverterFactory(FilterConverterFactory.create())
-                .addConverterFactory(FieldsConverterFactory.create())
-                .validateEagerly(true)
-                .build();
-
-
-        EventService eventService = retrofit.create(EventService.class);
+    private EventCall givenADefaultEventCall() {
+        //TODO - create a internal factory of dependencies separate of d2
+        // for avoid to expose retrofit to the outside of library
+        // and for avoid create instances duplication on every test
+        EventService eventService = d2.retrofit().create(EventService.class);
 
         EventStore eventStore = new EventStoreImpl(databaseAdapter());
         EventHandler eventHandler = new EventHandler(eventStore);

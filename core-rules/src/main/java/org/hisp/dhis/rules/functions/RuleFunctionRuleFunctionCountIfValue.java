f04b19402db9f9f9a272b677f3d2ee9f9e0369db
Idelcano <ignaciodelcano@gmail.com>
2017-12-01 20:34:46 +0100

    Added count if value function

---
+++ core-rules/src/main/java/org/hisp/dhis/rules/functions/RuleFunctionRuleFunctionCountIfValue.java
@@ -0,0 +1,63 @@
+package org.hisp.dhis.rules.functions;
+
+
+import org.hisp.dhis.rules.RuleVariableValue;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+public class RuleFunctionRuleFunctionCountIfValue extends RuleFunction {
+    static final String D2_COUNT_IF_VALUE = "d2:countifvalue";
+
+    @Nonnull
+    public static RuleFunctionRuleFunctionCountIfValue create() {
+        return new RuleFunctionRuleFunctionCountIfValue();
+    }
+
+    @Nonnull
+    @Override
+    public String evaluate(@Nonnull List<String> arguments,
+            Map<String, RuleVariableValue> valueMap) {
+        if (arguments.size() != 1) {
+            throw new IllegalArgumentException("One argument was expected, " +
+                    arguments.size() + " were supplied");
+        }
+
+        return String.valueOf(countIfValue(arguments.get(0), valueMap));
+    }
+
+    /**
+     * Function which will return count value for given variableName
+     *
+     * @param variableName variable name.
+     * @param valueMap list of variables.
+     * @return return the count of values
+     */
+    public static Integer countIfValue(String variableName, Map<String, RuleVariableValue> valueMap) {
+        RuleVariableValue ruleVariableValue =valueMap.get(variableName);
+        Integer count = 0;
+        if(ruleVariableValue != null) {
+            if(ruleVariableValue.value()!=null) {
+                String valueToCompare = ruleVariableValue.value().replaceFirst("'","");
+                if(valueToCompare.lastIndexOf("'")==valueToCompare.length()-1){
+                    valueToCompare=valueToCompare.substring(0,valueToCompare.length()-1);
+                }
+                if(ruleVariableValue.candidates() != null) {
+                    for(String candidateValue : ruleVariableValue.candidates()){
+                        if (candidateValue != null && candidateValue.equals(valueToCompare)) {
+                            count++;
+                        }
+                    }
+                } else {
+                    if(valueToCompare.equals(ruleVariableValue.value())) {
+                        return 1;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+}
---
+++ core-rules/src/test/java/org/hisp/dhis/rules/functions/RuleFunctionCountIfValueShould.java
@@ -0,0 +1,79 @@
+package org.hisp.dhis.rules.functions;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+import org.hisp.dhis.rules.RuleVariableValue;
+import org.hisp.dhis.rules.models.RuleValueType;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RuleFunctionCountIfValueShould {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+
+    @Test
+    public void return_zero_if_variable_not_exist() {
+        RuleFunction ruleFunction = RuleFunctionRuleFunctionCountIfValue.create();
+
+        HashMap<String, RuleVariableValue> ruleVariableValueHashMap = new HashMap<>();
+        String result = ruleFunction.evaluate(Arrays.asList(""), ruleVariableValueHashMap);
+
+        assertThat(result).isEqualTo("0");
+    }
+
+    @Test
+    public void return_value_if_variable_exist() {
+        RuleFunction ruleFunction = RuleFunctionRuleFunctionCountIfValue.create();
+
+
+        RuleVariableValue variableValue = RuleVariableValue.create(
+                "test_variable_one", RuleValueType.TEXT, Arrays.asList("test_variable_one", "test_variable_one", "test_variable_one" ));
+        RuleVariableValue variableValueTwo = RuleVariableValue.create(
+                "test_variable_two", RuleValueType.TEXT, Arrays.asList("test_variable_two", "-2", "test_variable_two" ));
+        RuleVariableValue variableValueThree = RuleVariableValue.create(
+                "test_variable_three", RuleValueType.TEXT, Arrays.asList("-1", "2", "test_variable_three" ));
+        RuleVariableValue variableValueFour = RuleVariableValue.create(
+                "test_variable_three", RuleValueType.TEXT, Arrays.asList("test_variable_three", null, "test_variable_two" ));
+        Map<String, RuleVariableValue> valueMap = new HashMap<>();
+        valueMap.put("test_variable_one", variableValue);
+        valueMap.put("test_variable_two", variableValueTwo);
+        valueMap.put("test_variable_three", variableValueThree);
+        valueMap.put("test_variable_four", variableValueFour);
+
+
+        String result = ruleFunction.evaluate(Arrays.asList("test_variable_one"),valueMap);
+        assertThat(result).isEqualTo("3");
+        result = ruleFunction.evaluate(Arrays.asList("test_variable_two"),valueMap);
+        assertThat(result).isEqualTo("2");
+        result = ruleFunction.evaluate(Arrays.asList("test_variable_three"),valueMap);
+        assertThat(result).isEqualTo("1");
+        result = ruleFunction.evaluate(Arrays.asList("test_variable_four"),valueMap);
+        assertThat(result).isEqualTo("1");
+    }
+
+    @Test
+    public void thrown_null_pointer_exception_if_first_parameter_is_null() {
+        thrown.expect(NullPointerException.class);
+        RuleFunction ruleFunction = RuleFunctionRuleFunctionCountIfValue.create();
+
+        ruleFunction.evaluate(null,
+                new HashMap<String, RuleVariableValue>());
+    }
+
+    @Test
+    public void thrown_illegal_argument_exception_if_first_parameter_is_empty_list() {
+        thrown.expect(IllegalArgumentException.class);
+        RuleFunction ruleFunction = RuleFunctionRuleFunctionCountIfValue.create();
+
+        ruleFunction.evaluate(new ArrayList<String>(),
+                new HashMap<String, RuleVariableValue>());
+    }
+}

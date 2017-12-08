94b9b5141c6f4eb89a86fe21b82494c9be625b73
Jorge Sánchez <xurxosf@gmail.com>
2017-12-08 11:37:01 +0100

    use HandlerFactory from EventCallFactory to avoid duplicate code

--- core/src/androidTest/java/org/hisp/dhis/android/core/common/EventCallFactory.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/common/EventCallFactory.java
@@ -1,19 +1,11 @@
 package org.hisp.dhis.android.core.common;
 
-import org.hisp.dhis.android.core.D2;
 import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
 import org.hisp.dhis.android.core.event.EventCall;
 import org.hisp.dhis.android.core.event.EventHandler;
 import org.hisp.dhis.android.core.event.EventQuery;
 import org.hisp.dhis.android.core.event.EventService;
-import org.hisp.dhis.android.core.event.EventStore;
-import org.hisp.dhis.android.core.event.EventStoreImpl;
 import org.hisp.dhis.android.core.resource.ResourceHandler;
-import org.hisp.dhis.android.core.resource.ResourceStore;
-import org.hisp.dhis.android.core.resource.ResourceStoreImpl;
-import org.hisp.dhis.android.core.trackedentity.TrackedEntityDataValueHandler;
-import org.hisp.dhis.android.core.trackedentity.TrackedEntityDataValueStore;
-import org.hisp.dhis.android.core.trackedentity.TrackedEntityDataValueStoreImpl;
 
 import java.util.Date;
 
@@ -25,18 +17,9 @@ public class EventCallFactory {
 
         EventService eventService = retrofit.create(EventService.class);
 
-        EventStore eventStore = new EventStoreImpl(databaseAdapter);
+        EventHandler eventHandler = HandlerFactory.createEventHandler(databaseAdapter);
 
-        TrackedEntityDataValueStore trackedEntityDataValueStore =
-                new TrackedEntityDataValueStoreImpl(databaseAdapter);
-
-        TrackedEntityDataValueHandler trackedEntityDataValueHandler =
-                new TrackedEntityDataValueHandler(trackedEntityDataValueStore);
-
-        EventHandler eventHandler = new EventHandler(eventStore, trackedEntityDataValueHandler);
-
-        ResourceStore resourceStore = new ResourceStoreImpl(databaseAdapter);
-        ResourceHandler resourceHandler = new ResourceHandler(resourceStore);
+        ResourceHandler resourceHandler = HandlerFactory.createResourceHandler(databaseAdapter);
 
         EventQuery eventQuery = EventQuery.Builder
                 .create()

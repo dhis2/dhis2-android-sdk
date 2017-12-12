b378be442d9a5007a364185bbabdb60ca0a01d12
idelcano <ignaciodelcano@gmail.com>
2017-12-12 21:43:05 +0100

    removed unnused import

--- core/src/main/java/org/hisp/dhis/android/core/user/LogOutUserCallable.java
+++ core/src/main/java/org/hisp/dhis/android/core/user/LogOutUserCallable.java
@@ -31,7 +31,6 @@ package org.hisp.dhis.android.core.user;
 import android.support.annotation.NonNull;
 
 import org.hisp.dhis.android.core.common.DeletableStore;
-import org.hisp.dhis.android.core.organisationunit.OrganisationUnitStore;
 
 import java.util.List;
 import java.util.concurrent.Callable;

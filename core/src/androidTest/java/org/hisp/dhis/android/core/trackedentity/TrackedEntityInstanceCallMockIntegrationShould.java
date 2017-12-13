abf7039aaad06b7b6e39d889da3c08ddf4419ff7
Jorge Sánchez <xurxosf@gmail.com>
2017-12-13 13:32:57 +0100

    create object mapper with date format to avoid jackson parser to fail with date formats depending on the user timezone

--- core/src/androidTest/java/org/hisp/dhis/android/core/trackedentity/TrackedEntityInstanceCallMockIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/trackedentity/TrackedEntityInstanceCallMockIntegrationShould.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.hisp.dhis.android.core.D2;
+import org.hisp.dhis.android.core.common.BaseIdentifiableObject;
 import org.hisp.dhis.android.core.common.D2Factory;
 import org.hisp.dhis.android.core.common.TrackedEntityInstanceCallFactory;
 import org.hisp.dhis.android.core.data.database.AbsStoreTestCase;
@@ -117,7 +118,8 @@ public class TrackedEntityInstanceCallMockIntegrationShould extends AbsStoreTest
     private TrackedEntityInstance parseEventResponse(String file) throws IOException {
         String expectedEventsResponseJson = new AssetsFileReader().getStringFromFile(file);
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper().setDateFormat(
+                BaseIdentifiableObject.DATE_FORMAT.raw());
 
         TrackedEntityInstance trackedEntityInstance = objectMapper.readValue(
                 expectedEventsResponseJson,

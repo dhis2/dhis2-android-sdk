f2bb845869bd68940b50c92a33a85f8a757e0010
arturomejiamarmol <arturomejiamarmol@gmail.com>
2017-12-20 18:33:03 -0400

    Fixing Timeout issue on instrumented test

--- core/src/androidTest/java/org/hisp/dhis/android/core/LogoutCallMockIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/LogoutCallMockIntegrationShould.java
@@ -56,6 +56,9 @@ public class LogoutCallMockIntegrationShould extends AbsStoreTestCase {
         dhis2MockServer.enqueueMockResponse("programs.json");
         dhis2MockServer.enqueueMockResponse("tracked_entities.json");
         dhis2MockServer.enqueueMockResponse("option_sets.json");
+        dhis2MockServer.enqueueMockResponse("categories.json");
+        dhis2MockServer.enqueueMockResponse("categories_combo.json");
+
 
         d2.syncMetaData().call();
     }
@@ -67,7 +70,8 @@ public class LogoutCallMockIntegrationShould extends AbsStoreTestCase {
         dhis2MockServer.enqueueMockResponse("programs.json");
         dhis2MockServer.enqueueMockResponse("tracked_entities.json");
         dhis2MockServer.enqueueMockResponse("option_sets.json");
-
+        dhis2MockServer.enqueueMockResponse("categories.json");
+        dhis2MockServer.enqueueMockResponse("categories_combo.json");
         d2.syncMetaData().call();
     }
 

0bae1b210968b977d67bf4139513be4933a0b175
ifoche <nacho.foche@gmail.com>
2017-12-21 13:37:48 +0100

    reorder LogoutCallMockIntegrationShould tests to place tests first and aux private methods later

--- core/src/androidTest/java/org/hisp/dhis/android/core/LogoutCallMockIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/LogoutCallMockIntegrationShould.java
@@ -56,62 +56,6 @@ public class LogoutCallMockIntegrationShould extends AbsStoreTestCase {
         dhis2MockServer.shutdown();
     }
 
-    private void givenALoginInDatabase() throws Exception {
-        dhis2MockServer.enqueueMockResponse("login.json", new Date());
-
-        Response<User> response = d2.logIn("user", "password").call();
-
-        assertThat(response.isSuccessful(), is(true));
-    }
-
-    private void givenALoginWithSierraLeonaOUInDatabase() throws Exception {
-        dhis2MockServer.enqueueMockResponse("admin/login.json", new Date());
-
-        Response<User> response = d2.logIn("user", "password").call();
-
-        assertThat(response.isSuccessful(), is(true));
-    }
-
-    private void givenAMetadataInDatabase() throws Exception {
-        dhis2MockServer.enqueueMockResponse("system_info.json");
-        dhis2MockServer.enqueueMockResponse("user.json");
-        dhis2MockServer.enqueueMockResponse("organisationUnits.json");
-        dhis2MockServer.enqueueMockResponse("categories.json");
-        dhis2MockServer.enqueueMockResponse("category_combos.json");
-        dhis2MockServer.enqueueMockResponse("programs.json");
-        dhis2MockServer.enqueueMockResponse("tracked_entities.json");
-        dhis2MockServer.enqueueMockResponse("option_sets.json");
-        Response response = d2.syncMetaData().call();
-
-        assertThat(response.isSuccessful(), is(true));
-    }
-
-    private void givenAMetadataWithDescendantsInDatabase() throws Exception {
-        dhis2MockServer.enqueueMockResponse("system_info.json");
-        dhis2MockServer.enqueueMockResponse("admin/user.json");
-        dhis2MockServer.enqueueMockResponse("admin/organisation_units.json");
-        dhis2MockServer.enqueueMockResponse("categories.json");
-        dhis2MockServer.enqueueMockResponse("category_combos.json");
-        dhis2MockServer.enqueueMockResponse("programs.json");
-        dhis2MockServer.enqueueMockResponse("tracked_entities.json");
-        dhis2MockServer.enqueueMockResponse("option_sets.json");
-
-        Response response = d2.syncMetaData().call();
-
-        assertThat(response.isSuccessful(), is(true));
-    }
-
-    private void givenAEventInDatabase() throws Exception {
-        EventEndPointCall eventCall = EventCallFactory.create(
-                d2.retrofit(), databaseAdapter(), "DiszpKrYNg8", 0);
-
-        dhis2MockServer.enqueueMockResponse("events_1.json");
-
-        Response response = eventCall.call();
-
-        assertThat(response.isSuccessful(), is(true));
-    }
-
     @Test
     public void have_empty_database_when_wipe_db_after_sync_meta_data() throws Exception {
         givenALoginInDatabase();
@@ -191,21 +135,8 @@ public class LogoutCallMockIntegrationShould extends AbsStoreTestCase {
         verifyExistsAsignedOrgUnitAndDescendants();
     }
 
-    private void verifyExistsAsignedOrgUnitAndDescendants() {
-        //Sierra leona
-        DatabaseAssert.assertThatDatabase(databaseAdapter())
-                .ifValueExist(OrganisationUnitModel.TABLE,
-                        OrganisationUnitModel.Columns.UID,
-                        "ImspTQPwCqd");
-
-        //Sierra leona descendant
-        DatabaseAssert.assertThatDatabase(databaseAdapter())
-                .ifValueExist(OrganisationUnitModel.TABLE,
-                        OrganisationUnitModel.Columns.CODE,
-                        "OU_278371");
-    }
     @Test
-    public void realize_login_and_sync_metadata_successfully_after_logout()
+    public void complete_login_and_sync_metadata_successfully_after_logout()
             throws Exception {
         givenALoginInDatabase();
 
@@ -232,5 +163,75 @@ public class LogoutCallMockIntegrationShould extends AbsStoreTestCase {
                 .isNotEmptyTable(OrganisationUnitModel.TABLE)
                 .isNotEmptyTable(ProgramModel.TABLE)
                 .isNotEmptyTable(ResourceModel.TABLE);
+    }
+
+    private void givenALoginInDatabase() throws Exception {
+        dhis2MockServer.enqueueMockResponse("login.json", new Date());
+
+        Response<User> response = d2.logIn("user", "password").call();
+
+        assertThat(response.isSuccessful(), is(true));
+    }
+
+    private void givenALoginWithSierraLeonaOUInDatabase() throws Exception {
+        dhis2MockServer.enqueueMockResponse("admin/login.json", new Date());
+
+        Response<User> response = d2.logIn("user", "password").call();
+
+        assertThat(response.isSuccessful(), is(true));
+    }
+
+    private void givenAMetadataInDatabase() throws Exception {
+        dhis2MockServer.enqueueMockResponse("system_info.json");
+        dhis2MockServer.enqueueMockResponse("user.json");
+        dhis2MockServer.enqueueMockResponse("organisationUnits.json");
+        dhis2MockServer.enqueueMockResponse("categories.json");
+        dhis2MockServer.enqueueMockResponse("category_combos.json");
+        dhis2MockServer.enqueueMockResponse("programs.json");
+        dhis2MockServer.enqueueMockResponse("tracked_entities.json");
+        dhis2MockServer.enqueueMockResponse("option_sets.json");
+        Response response = d2.syncMetaData().call();
+
+        assertThat(response.isSuccessful(), is(true));
+    }
+
+    private void givenAMetadataWithDescendantsInDatabase() throws Exception {
+        dhis2MockServer.enqueueMockResponse("system_info.json");
+        dhis2MockServer.enqueueMockResponse("admin/user.json");
+        dhis2MockServer.enqueueMockResponse("admin/organisation_units.json");
+        dhis2MockServer.enqueueMockResponse("categories.json");
+        dhis2MockServer.enqueueMockResponse("category_combos.json");
+        dhis2MockServer.enqueueMockResponse("programs.json");
+        dhis2MockServer.enqueueMockResponse("tracked_entities.json");
+        dhis2MockServer.enqueueMockResponse("option_sets.json");
+
+        Response response = d2.syncMetaData().call();
+
+        assertThat(response.isSuccessful(), is(true));
+    }
+
+    private void givenAEventInDatabase() throws Exception {
+        EventEndPointCall eventCall = EventCallFactory.create(
+                d2.retrofit(), databaseAdapter(), "DiszpKrYNg8", 0);
+
+        dhis2MockServer.enqueueMockResponse("events_1.json");
+
+        Response response = eventCall.call();
+
+        assertThat(response.isSuccessful(), is(true));
+    }
+
+    private void verifyExistsAsignedOrgUnitAndDescendants() {
+        //Sierra leona
+        DatabaseAssert.assertThatDatabase(databaseAdapter())
+                .ifValueExist(OrganisationUnitModel.TABLE,
+                        OrganisationUnitModel.Columns.UID,
+                        "ImspTQPwCqd");
+
+        //Sierra leona descendant
+        DatabaseAssert.assertThatDatabase(databaseAdapter())
+                .ifValueExist(OrganisationUnitModel.TABLE,
+                        OrganisationUnitModel.Columns.CODE,
+                        "OU_278371");
     }
 }

b1b988c15e06c7d9d054a01d44589968f0014c0a
Luis Delgado Romera <luisyng@gmail.com>
2018-01-10 16:43:20 +0100

    aggregate-datasets: add dataSet and dataElement json in other tests

--- core/src/androidTest/java/org/hisp/dhis/android/core/LogoutCallMockIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/LogoutCallMockIntegrationShould.java
@@ -206,6 +206,8 @@ public class LogoutCallMockIntegrationShould extends AbsStoreTestCase {
         dhis2MockServer.enqueueMockResponse("programs.json");
         dhis2MockServer.enqueueMockResponse("tracked_entities.json");
         dhis2MockServer.enqueueMockResponse("option_sets.json");
+        dhis2MockServer.enqueueMockResponse("data_sets.json");
+        dhis2MockServer.enqueueMockResponse("data_elements.json");
 
         Response response = d2.syncMetaData().call();
 

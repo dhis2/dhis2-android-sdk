b6c73e60fda267a9e52210a89bc237275ef26710
idelcano <ignaciodelcano@gmail.com>
2018-01-26 17:45:44 +0100

    Feature sync stop on wrong events fk (#326)
    Remove unnecesary deferrable foreign key condition, allow sync continue on event sqlconstraint excpetion, and fix tests
    
    # Conflicts:
    #	core/src/androidTest/java/org/hisp/dhis/android/core/data/database/migrations/DataBaseMigrationShould.java
    #	core/src/androidTest/java/org/hisp/dhis/android/core/event/EventStoreShould.java
    #	core/src/androidTest/java/org/hisp/dhis/android/core/trackedentity/TrackedEntityDataValueStoreShould.java

--- core/src/androidTest/java/org/hisp/dhis/android/core/data/server/RealServerMother.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/data/server/RealServerMother.java
@@ -1,7 +1,7 @@
 package org.hisp.dhis.android.core.data.server;
 
 public class RealServerMother {
-    public static String url = "https://play.dhis2.org/demo/api/";
+    public static String url = "https://play.dhis2.org/2.28/api/";
     public static String user = "android";
     public static String password = "Android123";
 }

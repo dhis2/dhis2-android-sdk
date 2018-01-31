45c06b522f320a86786fb0638cfe867f772064d1
idelcano <ignaciodelcano@gmail.com>
2018-01-31 06:42:57 +0100

    Added store class

---
+++ core/src/main/java/org/hisp/dhis/android/core/common/Store.java
@@ -0,0 +1,29 @@
+package org.hisp.dhis.android.core.common;
+import static org.hisp.dhis.android.core.utils.StoreUtils.parse;
+
+import android.database.Cursor;
+import android.support.annotation.Nullable;
+
+import java.util.Date;
+
+public class Store {
+    @Nullable
+    protected String getStringFromCursor(Cursor cursor, int index) {
+        return cursor.getString(index) == null ? null : cursor.getString(index);
+    }
+
+    @Nullable
+    protected Date getDateFromCursor(Cursor cursor, int index) {
+        return cursor.getString(index) == null ? null : parse(cursor.getString(index));
+    }
+
+    @Nullable
+    protected Boolean getBooleanFromCursor(Cursor cursor, int index) {
+        return cursor.getString(index) == null ? null : cursor.getInt(index) > 0;
+    }
+
+    @Nullable
+    protected Integer getIntegerFromCursor(Cursor cursor, int index) {
+        return cursor.getString(index) == null ? null : cursor.getInt(index);
+    }
+}

c9187b4e0003c16e1caacf1ed2d811f870872ebf
Marcos Campos <marcamsn@gmail.com>
2018-02-01 12:42:46 +0100

    indicators: make private the IndicatorHandler constructor

--- core/src/main/java/org/hisp/dhis/android/core/indicator/IndicatorHandler.java
+++ core/src/main/java/org/hisp/dhis/android/core/indicator/IndicatorHandler.java
@@ -33,7 +33,7 @@ import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
 
 public class IndicatorHandler extends GenericHandlerImpl<Indicator, IndicatorModel> {
 
-    IndicatorHandler(IdentifiableObjectStore<IndicatorModel> indicatorStore) {
+    private IndicatorHandler(IdentifiableObjectStore<IndicatorModel> indicatorStore) {
         super(indicatorStore);
     }
 

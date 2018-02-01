8ec26b3da9c4a6013e2063e298c1e5cb818d31eb
ifoche <nacho.foche@gmail.com>
2018-02-01 08:45:53 +0100

    add test execution instructions

--- core/src/androidTest/java/org/hisp/dhis/android/core/audit/MetadataAuditRealIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/audit/MetadataAuditRealIntegrationShould.java
@@ -9,7 +9,6 @@ import org.hisp.dhis.android.core.common.D2Factory;
 import org.hisp.dhis.android.core.data.database.AbsStoreTestCase;
 import org.hisp.dhis.android.core.data.server.RealServerMother;
 import org.junit.Before;
-import org.junit.Test;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
@@ -35,9 +34,15 @@ public class MetadataAuditRealIntegrationShould extends AbsStoreTestCase {
                         .build());
     }
 
-    //The goal of this test is verify the end to end the whole rabbitmq sync feature
-    //this test must to be commented because need a message broker (server), rabbitmq configuration
-    //in dhis2 server and manual metadata change on server
+    // The goal of this test is verify the end to end the whole rabbitmq sync feature
+    // this test must to be commented because it needs a message broker (server), rabbitmq
+    // configuration in dhis2 server and manual metadata change on server.
+    // To execute this test:
+    // 1) Uncomment @Test annotation
+    // 2) Change MetadataAuditConnection to match your broker IP, username and port
+    // 3) Eventually setup a breakpoint
+    // 4) Launch the test
+    // 5) Manually Modify some piece of metadata on a DHIS2 instance connected to the same broker
     //@Test
     public void notify_metadata_synced_in_local_when_a_change_occurs_on_the_server()
             throws Exception {

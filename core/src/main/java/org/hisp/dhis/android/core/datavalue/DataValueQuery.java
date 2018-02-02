5ba0bf23e69273c14a1ca326b792bd522d7d6a2d
Marcos Campos <marcamsn@gmail.com>
2018-02-02 17:03:34 +0100

    datavalues: add DataValueQuery

---
+++ core/src/main/java/org/hisp/dhis/android/core/datavalue/DataValueQuery.java
@@ -0,0 +1,20 @@
+package org.hisp.dhis.android.core.datavalue;
+
+import com.google.auto.value.AutoValue;
+
+import org.hisp.dhis.android.core.common.BaseQuery;
+
+import java.util.Set;
+
+@AutoValue
+public abstract class DataValueQuery extends BaseQuery {
+    public abstract Set<String> dataSetUids();
+
+    public abstract Set<String> periodIds();
+
+    public abstract Set<String> orgUnitUids();
+
+    public static DataValueQuery create(Set<String> dataSetUids, Set<String> periodIds, Set<String> orgUnitUids) {
+        return new AutoValue_DataValueQuery(1, BaseQuery.DEFAULT_PAGE_SIZE, false, dataSetUids, periodIds, orgUnitUids);
+    }
+}

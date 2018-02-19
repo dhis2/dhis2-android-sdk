c884d22a48ccc0943557ad2a210165d578feaa01
idelcano <ignaciodelcano@gmail.com>
2018-02-19 10:44:36 +0100

    Persist system info version in db

--- core/src/main/java/org/hisp/dhis/android/sdk/controllers/metadata/MetaDataController.java
+++ core/src/main/java/org/hisp/dhis/android/sdk/controllers/metadata/MetaDataController.java
@@ -655,6 +655,7 @@ public final class MetaDataController extends ResourceController {
 
         }
         SystemInfo serverSystemInfo = dhisApi.getSystemInfo();
+        serverSystemInfo.save();
         AppPreferencesImpl appPreferences = new AppPreferencesImpl(context);
         appPreferences.setApiVersion(serverSystemInfo.getVersion());
         DateTime serverDateTime = serverSystemInfo.getServerDate();

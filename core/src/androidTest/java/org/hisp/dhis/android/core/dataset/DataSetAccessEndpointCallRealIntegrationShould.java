b0b38f8cc9b766154f8a97363cd2ce087756bdf5
Marcos Campos <marcamsn@gmail.com>
2018-02-23 17:39:50 +0100

    user-access-2.29: add DataSetAccessEndpointCallRealIntegrationShould

---
+++ core/src/androidTest/java/org/hisp/dhis/android/core/dataset/DataSetAccessEndpointCallRealIntegrationShould.java
@@ -0,0 +1,60 @@
+package org.hisp.dhis.android.core.dataset;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.hisp.dhis.android.core.D2;
+import org.hisp.dhis.android.core.common.D2Factory;
+import org.hisp.dhis.android.core.common.GenericCallData;
+import org.hisp.dhis.android.core.data.database.AbsStoreTestCase;
+import org.hisp.dhis.android.core.resource.ResourceHandler;
+import org.hisp.dhis.android.core.resource.ResourceStoreImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public class DataSetAccessEndpointCallRealIntegrationShould extends AbsStoreTestCase {
+    /**
+     * A quick integration test that is probably flaky, but will help with finding bugs related to the
+     * metadataSyncCall. It works against the dev server.
+     */
+    private D2 d2;
+    private DataSetAccessEndpointCall dataSetAccessCall;
+
+    @Before
+    @Override
+    public void setUp() throws IOException {
+        super.setUp();
+        d2 = D2Factory.create("https://play.dhis2.org/dev/api/", databaseAdapter());
+        dataSetAccessCall = createCall();
+    }
+
+    private DataSetAccessEndpointCall createCall() {
+        ResourceHandler resourceHandler =
+                new ResourceHandler(new ResourceStoreImpl(databaseAdapter()));
+        GenericCallData data = GenericCallData.create(databaseAdapter(), resourceHandler, d2.retrofit());
+
+        return DataSetAccessEndpointCall.FACTORY.create(data);
+    }
+
+    // @Test
+    public void download_data_sets() throws Exception {
+        if (!d2.isUserLoggedIn().call()) {
+            retrofit2.Response loginResponse = d2.logIn("android", "Android123").call();
+            assertThat(loginResponse.isSuccessful()).isTrue();
+        }
+
+        retrofit2.Response dataSetResponse = dataSetAccessCall.call();
+        assertThat(dataSetResponse.isSuccessful()).isTrue();
+    }
+
+    @Test
+    public void stub() {
+    }
+}

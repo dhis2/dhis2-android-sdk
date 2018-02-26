2582e5f77cfb201873f675d11985b3e4e13b356d
Marcos Campos <marcamsn@gmail.com>
2018-02-26 15:50:36 +0100

    user-access-2.29: fix link organisationUnitDataSet

--- core/src/main/java/org/hisp/dhis/android/core/dataset/DataSetParentCall.java
+++ core/src/main/java/org/hisp/dhis/android/core/dataset/DataSetParentCall.java
@@ -104,7 +104,7 @@ public class DataSetParentCall extends TransactionalCall {
         periodHandler.generateAndPersist();
 
         linkManager.saveDataSetDataElementAndIndicatorLinks(dataSets);
-        linkManager.saveDataSetOrganisationUnitLinks(organisationUnits);
+        linkManager.saveDataSetOrganisationUnitLinks(organisationUnits, dataSetUids);
 
         return dataElementResponse;
     }

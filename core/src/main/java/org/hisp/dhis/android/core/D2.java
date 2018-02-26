25e5147f04142693819218a2ead95e7100101b42
Marcos Campos <marcamsn@gmail.com>
2018-02-26 17:59:54 +0100

    user-access-2.29: save only OrganisationUnitProgramLinks with access

--- core/src/main/java/org/hisp/dhis/android/core/D2.java
+++ core/src/main/java/org/hisp/dhis/android/core/D2.java
@@ -391,7 +391,7 @@ public final class D2 {
         resourceHandler = new ResourceHandler(resourceStore);
 
         organisationUnitHandler = new OrganisationUnitHandler(organisationUnitStore,
-                userOrganisationUnitLinkStore, organisationUnitProgramLinkStore);
+                userOrganisationUnitLinkStore, organisationUnitProgramLinkStore, null);
 
         TrackedEntityDataValueHandler trackedEntityDataValueHandler =
                 new TrackedEntityDataValueHandler(trackedEntityDataValueStore);

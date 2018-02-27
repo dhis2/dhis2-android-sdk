8ce6a2f8d5e1e7e0b6edfa363484d33134eb54df
Luis Delgado Romera <luisyng@gmail.com>
2018-02-27 18:02:00 +0100

    user-access-2.29: remove unused import

--- core/src/main/java/org/hisp/dhis/android/core/common/DataAccess.java
+++ core/src/main/java/org/hisp/dhis/android/core/common/DataAccess.java
@@ -28,7 +28,6 @@
 
 package org.hisp.dhis.android.core.common;
 
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.fasterxml.jackson.annotation.JsonCreator;

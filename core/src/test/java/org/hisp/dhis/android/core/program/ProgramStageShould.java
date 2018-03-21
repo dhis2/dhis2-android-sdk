defc043e666cce1acefb4925b6e7ac79c24d2c89
Luis Delgado Romera <luisyng@gmail.com>
2018-03-21 12:50:47 +0100

    program-stage-refactor: pojo test

--- core/src/test/java/org/hisp/dhis/android/core/program/ProgramStageShould.java
+++ core/src/test/java/org/hisp/dhis/android/core/program/ProgramStageShould.java
@@ -28,11 +28,10 @@
 
 package org.hisp.dhis.android.core.program;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.hisp.dhis.android.core.Inject;
 import org.hisp.dhis.android.core.common.BaseIdentifiableObject;
+import org.hisp.dhis.android.core.common.BaseObjectShould;
 import org.hisp.dhis.android.core.common.FormType;
+import org.hisp.dhis.android.core.common.ObjectShould;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -40,107 +39,16 @@ import java.text.ParseException;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
-public class ProgramStageShould {
+public class ProgramStageShould  extends BaseObjectShould implements ObjectShould {
 
+    public ProgramStageShould() {
+        super("program/program_stage.json");
+    }
+
+    @Override
     @Test
     public void map_from_json_string() throws IOException, ParseException {
-        ObjectMapper objectMapper = Inject.objectMapper();
-        ProgramStage programStage = objectMapper.readValue("{\n" +
-                "\n" +
-                "    \"lastUpdated\": \"2013-04-10T12:15:02.041\",\n" +
-                "    \"id\": \"eaDHS084uMp\",\n" +
-                "    \"href\": \"https://play.dhis2.org/dev/api/programStages/eaDHS084uMp\",\n" +
-                "    \"created\": \"2013-03-04T11:41:07.541\",\n" +
-                "    \"name\": \"ANC 1st visit\",\n" +
-                "    \"allowGenerateNextVisit\": false,\n" +
-                "    \"validCompleteOnly\": false,\n" +
-                "    \"displayName\": \"ANC 1st visit\",\n" +
-                "    \"reportDateToUse\": \"false\",\n" +
-                "    \"description\": \"ANC 1st visit\",\n" +
-                "    \"externalAccess\": false,\n" +
-                "    \"openAfterEnrollment\": false,\n" +
-                "    \"repeatable\": false,\n" +
-                "    \"captureCoordinates\": false,\n" +
-                "    \"formType\": \"DEFAULT\",\n" +
-                "    \"remindCompleted\": false,\n" +
-                "    \"displayGenerateEventBox\": false,\n" +
-                "    \"generatedByEnrollmentDate\": false,\n" +
-                "    \"autoGenerateEvent\": true,\n" +
-                "    \"sortOrder\": 1,\n" +
-                "    \"hideDueDate\": false,\n" +
-                "    \"blockEntryForm\": false,\n" +
-                "    \"minDaysFromStart\": 0,\n" +
-                "    \"access\": {\n" +
-                "        \"read\": true,\n" +
-                "        \"updateWithSection\": true,\n" +
-                "        \"externalize\": false,\n" +
-                "        \"delete\": true,\n" +
-                "        \"write\": true,\n" +
-                "        \"manage\": false\n" +
-                "    },\n" +
-                "    \"program\": {\n" +
-                "        \"id\": \"uy2gU8kT1jF\"\n" +
-                "    },\n" +
-                "    \"notificationTemplates\": [ ],\n" +
-                "    \"programStageDataElements\": [\n" +
-                "        {\n" +
-                "            \"id\": \"EQCf1l2Mdr8\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"muxw4SGzUwJ\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"KWybjio9UZT\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"ejm0g2hwHHc\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"yvV3txhSCyc\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"fzQrjBpbwQD\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"BbvkNf9PCxX\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"MbdCfd4HaMQ\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"SBn4XCFRbyT\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"F0PZ4nZ86vo\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"gFBRqVFh60H\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"ljAoyjH4GYA\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"MAdsNY2gOlv\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"IpVUTCDdlGW\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"psBtdqepNVM\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"UzB6pZxZ2Rb\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"id\": \"FQZEMbBVabW\"\n" +
-                "        }\n" +
-                "    ],\n" +
-                "    \"translations\": [ ],\n" +
-                "    \"userGroupAccesses\": [ ],\n" +
-                "    \"attributeValues\": [ ],\n" +
-                "    \"programStageSections\": [ ]\n" +
-                "\n" +
-                "}", ProgramStage.class);
+        ProgramStage programStage = objectMapper.readValue(jsonStream, ProgramStage.class);
 
         assertThat(programStage.lastUpdated()).isEqualTo(
                 BaseIdentifiableObject.DATE_FORMAT.parse("2013-04-10T12:15:02.041"));

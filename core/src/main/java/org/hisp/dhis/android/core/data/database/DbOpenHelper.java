e3cd99110839a388d0f3546e16b9cb1600a7b770
Marcos Campos <marcamsn@gmail.com>
2018-03-22 11:28:07 +0100

    program-stage-access: add migration

--- core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
+++ core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
@@ -101,7 +101,7 @@ import static org.hisp.dhis.android.core.user.UserOrganisationUnitLinkModel.Colu
 })
 public class DbOpenHelper extends CustomSQLBriteOpenHelper {
 
-    public static final int VERSION = 11;
+    public static final int VERSION = 12;
     public String mockedSqlDatabase = "";
     private static final String CREATE_CONFIGURATION_TABLE =
             "CREATE TABLE " + ConfigurationModel.CONFIGURATION + " (" +

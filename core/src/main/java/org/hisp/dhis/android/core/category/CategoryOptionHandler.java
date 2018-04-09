eb317a6756e26995308ac24b3e8400546c80798b
Marcos Campos <marcamsn@gmail.com>
2018-04-09 13:51:34 +0200

    category-option-start-end-date: fix unit tests

--- core/src/main/java/org/hisp/dhis/android/core/category/CategoryOptionHandler.java
+++ core/src/main/java/org/hisp/dhis/android/core/category/CategoryOptionHandler.java
@@ -3,11 +3,14 @@ package org.hisp.dhis.android.core.category;
 
 import org.hisp.dhis.android.core.common.GenericHandler;
 import org.hisp.dhis.android.core.common.IdentifiableHandlerImpl;
+import org.hisp.dhis.android.core.common.IdentifiableObjectStore;
 import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
 
-public final class CategoryOptionHandler {
+public final class CategoryOptionHandler extends IdentifiableHandlerImpl<CategoryOption, CategoryOptionModel> {
 
-    private CategoryOptionHandler() {}
+    CategoryOptionHandler(IdentifiableObjectStore<CategoryOptionModel> categoryOptionStore) {
+        super(categoryOptionStore);
+    }
 
     public static GenericHandler<CategoryOption, CategoryOptionModel> create(DatabaseAdapter databaseAdapter) {
         return new IdentifiableHandlerImpl<>(CategoryOptionStore.create(databaseAdapter));

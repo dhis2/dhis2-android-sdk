eee31055e6b144211653c6e4809f5b53e91ba6a9
Luis Delgado Romera <luisyng@gmail.com>
2018-04-16 09:56:32 +0200

    system-settings: handler should

---
+++ core/src/test/java/org/hisp/dhis/android/core/settings/SystemSettingHandlerShould.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2017, University of Oslo
+ *
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.android.core.settings;
+
+import org.assertj.core.util.Lists;
+import org.hisp.dhis.android.core.common.ObjectWithoutUidStore;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(JUnit4.class)
+public class SystemSettingHandlerShould {
+
+    @Mock
+    private ObjectWithoutUidStore<SystemSettingModel> store;
+
+    @Mock
+    private SystemSettingModelBuilder modelBuilder;
+
+    @Mock
+    private SystemSetting pojo;
+
+    @Mock
+    private SystemSettingModel m1;
+
+    @Mock
+    private SystemSettingModel m2;
+
+    // object to test
+    private SystemSettingHandlerImpl handler;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        handler = new SystemSettingHandlerImpl(store);
+
+        when(modelBuilder.splitSettings(pojo)).thenReturn(Lists.newArrayList(m1, m2));
+    }
+
+    @Test
+    public void call_model_builder_to_split_settings() throws Exception {
+        handler.handle(pojo, modelBuilder);
+        verify(modelBuilder).splitSettings(pojo);
+    }
+
+    @Test
+    public void call_store_to_persist_models() throws Exception {
+        handler.handle(pojo, modelBuilder);
+        verify(store).updateOrInsertWhere(m1);
+        verify(store).updateOrInsertWhere(m2);
+    }
+}

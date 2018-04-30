ebc2730cf59cd93e83c929079cc1758fc74801f1
Luis Delgado Romera <luisyng@gmail.com>
2018-04-30 12:34:12 +0200

    system-info-date: fix android tests

--- core/src/androidTest/java/org/hisp/dhis/android/core/LogoutCallMockIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/LogoutCallMockIntegrationShould.java
@@ -166,8 +166,8 @@ public class LogoutCallMockIntegrationShould extends AbsStoreTestCase {
     }
 
     private void givenALoginInDatabase() throws Exception {
-        dhis2MockServer.enqueueMockResponse("system_info.json", new Date());
         dhis2MockServer.enqueueMockResponse("login.json", new Date());
+        dhis2MockServer.enqueueMockResponse("system_info.json", new Date());
 
         Response<User> response = d2.logIn("user", "password").call();
 
@@ -175,8 +175,8 @@ public class LogoutCallMockIntegrationShould extends AbsStoreTestCase {
     }
 
     private void givenALoginWithSierraLeonaOUInDatabase() throws Exception {
-        dhis2MockServer.enqueueMockResponse("system_info.json", new Date());
         dhis2MockServer.enqueueMockResponse("admin/login.json", new Date());
+        dhis2MockServer.enqueueMockResponse("system_info.json", new Date());
 
         Response<User> response = d2.logIn("user", "password").call();
 

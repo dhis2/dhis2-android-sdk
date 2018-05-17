3bbb16085037b9429b49b0318dcd8bd8a678e9f9
Marcos Campos <marcamsn@gmail.com>
2018-05-17 16:39:57 +0200

    n-teis-call: fix call by orgUnits

--- core/src/main/java/org/hisp/dhis/android/core/calls/TrackerEntitiesDataCall.java
+++ core/src/main/java/org/hisp/dhis/android/core/calls/TrackerEntitiesDataCall.java
@@ -8,22 +8,20 @@ import org.hisp.dhis.android.core.common.GenericCallData;
 import org.hisp.dhis.android.core.common.IdentifiableObjectStore;
 import org.hisp.dhis.android.core.common.Payload;
 import org.hisp.dhis.android.core.common.SyncCall;
+import org.hisp.dhis.android.core.data.api.OuMode;
 import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
 import org.hisp.dhis.android.core.data.database.Transaction;
 import org.hisp.dhis.android.core.organisationunit.OrganisationUnitModel;
 import org.hisp.dhis.android.core.organisationunit.OrganisationUnitStore;
-import org.hisp.dhis.android.core.resource.ResourceModel;
 import org.hisp.dhis.android.core.systeminfo.SystemInfo;
 import org.hisp.dhis.android.core.systeminfo.SystemInfoCall;
 import org.hisp.dhis.android.core.trackedentity.TeiQuery;
 import org.hisp.dhis.android.core.trackedentity.TeisEndPointCall;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstance;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -79,49 +77,47 @@ public final class TrackerEntitiesDataCall extends SyncCall<List<TrackedEntityIn
     
     private List<TrackedEntityInstance> trackerCall(GenericCallData genericCallData) throws D2CallException {
         Set<String> organisationUnitUids = organisationUnitStore.selectUids();
+        List<TrackedEntityInstance> trackedEntityInstances = new ArrayList<>();
+        TeiQuery.Builder teiQueryBuilder = TeiQuery.Builder.create();
+        int pageSize = TeiQuery.Builder.create().build().getPageSize();
+        int numPages = (int) Math.ceil((double) teiLimit / pageSize);
 
-        List<TrackedEntityInstance> trackedEntityInstances = limitByOrgUnit ? teiSyncByOrgUnit(organisationUnitUids, genericCallData) :
-                teiGlobalSync(organisationUnitUids, genericCallData);
-
-        genericCallData.resourceHandler().handleResource(ResourceModel.Type.TRACKED_ENTITY_INSTANCE,
-                genericCallData.serverDate());
+        if (limitByOrgUnit) {
+            for (String orgUnitUid : organisationUnitUids) {
+                teiQueryBuilder.withOrgUnits(new HashSet<>(Arrays.asList(orgUnitUid)));
+                trackedEntityInstances.addAll(getTrackedEntityInstances(teiQueryBuilder, genericCallData, pageSize,
+                        numPages));
+            }
+        } else {
+            teiQueryBuilder.withOrgUnits(organisationUnitUids).withOuMode(OuMode.DESCENDANTS);
+            trackedEntityInstances = getTrackedEntityInstances(teiQueryBuilder, genericCallData, pageSize, numPages);
+        }
 
         return trackedEntityInstances;
     }
 
-    private List<TrackedEntityInstance> teiSyncByOrgUnit(
-            Set<String> organisationUnitUids, GenericCallData genericCallData) throws D2CallException {
-        List<TrackedEntityInstance> trackedEntityInstances = new ArrayList<>();
+    private List<TrackedEntityInstance> getTrackedEntityInstances(TeiQuery.Builder teiQueryBuilder,
+            GenericCallData genericCallData, int pageSize, int numPages) throws D2CallException {
         Response<Payload<TrackedEntityInstance>> trackerCallResponse = null;
-        TeiQuery.Builder teiQueryBuilder = TeiQuery.Builder.create().withPageLimit(0);
-        int numPages = (int) Math.ceil((double) teiLimit / TeiQuery.Builder.create().build().getPageSize());
+        List<TrackedEntityInstance> trackedEntityInstances = new ArrayList<>();
 
-        for (String orgUnitUid : organisationUnitUids) {
-            teiQueryBuilder.withOrgUnits(new HashSet<>(Arrays.asList(orgUnitUid)));
-            for (int page = 1; page <= numPages; page++) {
-                try {
-                    if (page == numPages) {
-                        teiQueryBuilder.withPage(page).withPageLimit(teiLimit - trackedEntityInstances.size());
-                    }
-                    teiQueryBuilder.withPage(page);
-                    trackerCallResponse = TeisEndPointCall.create(genericCallData, teiQueryBuilder.build()).call();
-
-                    if (trackerCallResponse.isSuccessful()) {
-                        trackedEntityInstances.addAll(trackerCallResponse.body().items());
-                    }
-                } catch (Exception e) {
-                    throw httpExceptionBuilder.httpErrorCode(trackerCallResponse.code()).build();
+        for (int page = 1; page <= numPages; page++) {
+            try {
+                if (page == numPages) {
+                    teiQueryBuilder.withPage(page).withPageLimit(teiLimit - ((page - 1) * pageSize));
                 }
+                teiQueryBuilder.withPage(page);
+                 trackerCallResponse = TeisEndPointCall.create(genericCallData, teiQueryBuilder.build()).call();
+
+                if (trackerCallResponse.isSuccessful()) {
+                    trackedEntityInstances.addAll(trackerCallResponse.body().items());
+                }
+            } catch (Exception e) {
+                throw httpExceptionBuilder.httpErrorCode(trackerCallResponse.code()).build();
             }
         }
         return trackedEntityInstances;
     }
-
-    private List<TrackedEntityInstance> teiGlobalSync(
-            Set<String> organisationUnitUids, GenericCallData genericCallData) throws D2CallException {
-        return new ArrayList<>();
-    }
-
 
     public static TrackerEntitiesDataCall create(DatabaseAdapter databaseAdapter, Retrofit retrofit,
                                                  int teiLimit, boolean limitByOrgUnit) {

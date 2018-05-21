7d391b335494be0c7f73c672e8f924c7e035a097
vgarciabnz <vgarciabnz@gmail.com>
2018-05-21 16:31:42 +0200

    indicator-engine: add several stages tests

--- core/src/test/java/org/hisp/dhis/android/core/utils/services/ProgramIndicatorEngineShould.java
+++ core/src/test/java/org/hisp/dhis/android/core/utils/services/ProgramIndicatorEngineShould.java
@@ -34,10 +34,8 @@ import org.hisp.dhis.android.core.constant.ConstantModel;
 import org.hisp.dhis.android.core.dataelement.DataElementModel;
 import org.hisp.dhis.android.core.enrollment.EnrollmentModel;
 import org.hisp.dhis.android.core.enrollment.EnrollmentStore;
-import org.hisp.dhis.android.core.event.Event;
 import org.hisp.dhis.android.core.event.EventModel;
 import org.hisp.dhis.android.core.event.EventStore;
-import org.hisp.dhis.android.core.program.ProgramIndicator;
 import org.hisp.dhis.android.core.program.ProgramIndicatorModel;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityAttributeValue;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityAttributeValueStore;
@@ -61,12 +59,16 @@ import static org.mockito.Mockito.when;
 public class ProgramIndicatorEngineShould {
 
     private String enrollmentUid = "enrollment-uid";
-    private String eventUid = "event-uid";
+    private String eventUid1 = "qCUMGmMZAhz";
+    private String eventUid2 = "QApcv9Je3bp";
     private String programIndicatorUid = "program-indicator-uid";
     private String trackedEntityInstanceUid = "tei-uid";
 
     @Mock
-    private EventModel eventModel;
+    private EventModel event1;
+
+    @Mock
+    private EventModel event2;
 
     @Mock
     private EnrollmentModel enrollmentModel;
@@ -84,6 +86,9 @@ public class ProgramIndicatorEngineShould {
     private TrackedEntityDataValue value3;
 
     @Mock
+    private TrackedEntityDataValue value4;
+
+    @Mock
     private TrackedEntityAttributeValue attributeValue;
 
     @Mock
@@ -92,10 +97,12 @@ public class ProgramIndicatorEngineShould {
     private String dataElementUid1 = "HhyfnvrrKpN";
     private String dataElementUid2 = "nM4RZkpgMcP";
     private String dataElementUid3 = "vJeQc8NlWu6";
+    private String dataElementUid4 = "jnyUQYDwj7K";
 
     private String attributeUid = "UQ0qSNHEpLt";
 
-    private String programStageUid = "un3rUMhluNu";
+    private String programStageUid1 = "un3rUMhluNu";
+    private String programStageUid2 = "hr7jRePpYMD";
 
     @Mock
     private ConstantModel constantModel;
@@ -129,12 +136,23 @@ public class ProgramIndicatorEngineShould {
         when(value1.dataElement()).thenReturn(dataElementUid1);
         when(value2.dataElement()).thenReturn(dataElementUid2);
         when(value3.dataElement()).thenReturn(dataElementUid3);
+        when(value4.dataElement()).thenReturn(dataElementUid4);
         when(attributeValue.trackedEntityAttribute()).thenReturn(attributeUid);
 
-        when(trackedEntityDataValueStore.queryTrackedEntityDataValues(eventUid))
+        when(trackedEntityDataValueStore.queryTrackedEntityDataValues(eventUid1))
                 .thenReturn(Arrays.asList(value1, value2, value3));
+        when(trackedEntityDataValueStore.queryTrackedEntityDataValues(eventUid2))
+                .thenReturn(Collections.singletonList(value4));
 
-        when(eventStore.queryByUid(eventUid)).thenReturn(eventModel);
+        when(event1.uid()).thenReturn(eventUid1);
+        when(event1.programStage()).thenReturn(programStageUid1);
+        when(eventStore.queryByUid(eventUid1)).thenReturn(event1);
+        when(eventStore.queryByEnrollmentAndProgramStage(enrollmentUid, programStageUid1)).thenReturn(event1);
+
+        when(event2.uid()).thenReturn(eventUid2);
+        when(event2.programStage()).thenReturn(programStageUid2);
+        when(eventStore.queryByUid(eventUid2)).thenReturn(event2);
+        when(eventStore.queryByEnrollmentAndProgramStage(enrollmentUid, programStageUid2)).thenReturn(event2);
 
         when(enrollmentModel.uid()).thenReturn(enrollmentUid);
         when(enrollmentModel.trackedEntityInstance()).thenReturn(trackedEntityInstanceUid);
@@ -147,6 +165,7 @@ public class ProgramIndicatorEngineShould {
         when(dataElementStore.selectByUid(dataElementUid1, DataElementModel.factory)).thenReturn(dataElementModel);
         when(dataElementStore.selectByUid(dataElementUid2, DataElementModel.factory)).thenReturn(dataElementModel);
         when(dataElementStore.selectByUid(dataElementUid3, DataElementModel.factory)).thenReturn(dataElementModel);
+        when(dataElementStore.selectByUid(dataElementUid4, DataElementModel.factory)).thenReturn(dataElementModel);
 
         when(trackedEntityAttributeValueStore.queryByTrackedEntityInstance(trackedEntityInstanceUid))
                 .thenReturn(Collections.singletonList(attributeValue));
@@ -157,11 +176,11 @@ public class ProgramIndicatorEngineShould {
 
     @Test
     public void parse_one_dataelement() throws Exception {
-        when(programIndicator.expression()).thenReturn(de(programStageUid, dataElementUid1));
+        when(programIndicator.expression()).thenReturn(de(programStageUid1, dataElementUid1));
 
         when(value1.value()).thenReturn("3.5");
 
-        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid, programIndicatorUid);
+        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid1, programIndicatorUid);
 
         assertThat(result).isEqualTo("3.5");
     }
@@ -169,12 +188,12 @@ public class ProgramIndicatorEngineShould {
     @Test
     public void parse_operation_two_dataelements() throws Exception {
         when(programIndicator.expression()).thenReturn(
-                de(programStageUid, dataElementUid1) + " + " + de(programStageUid, dataElementUid2));
+                de(programStageUid1, dataElementUid1) + " + " + de(programStageUid1, dataElementUid2));
 
         when(value1.value()).thenReturn("3.5");
         when(value2.value()).thenReturn("2");
 
-        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid, programIndicatorUid);
+        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid1, programIndicatorUid);
 
         assertThat(result).isEqualTo("3.5 + 2.0");
     }
@@ -182,14 +201,14 @@ public class ProgramIndicatorEngineShould {
     @Test
     public void parse_operation_with_parenthesis() throws Exception {
         when(programIndicator.expression()).thenReturn(
-                "(" + de(programStageUid, dataElementUid1) + " + " + de(programStageUid, dataElementUid2) +
-                ") / " + de(programStageUid, dataElementUid3));
+                "(" + de(programStageUid1, dataElementUid1) + " + " + de(programStageUid1, dataElementUid2) +
+                ") / " + de(programStageUid1, dataElementUid3));
 
         when(value1.value()).thenReturn("2.5");
         when(value2.value()).thenReturn("2");
         when(value3.value()).thenReturn("1.5");
 
-        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid, programIndicatorUid);
+        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid1, programIndicatorUid);
 
         assertThat(result).isEqualTo("(2.5 + 2.0) / 1.5");
     }
@@ -197,12 +216,12 @@ public class ProgramIndicatorEngineShould {
     @Test
     public void parse_dataelement_and_constant() throws Exception {
         when(programIndicator.expression()).thenReturn(
-                de(programStageUid, dataElementUid1) + " + " + cons(constantUid1));
+                de(programStageUid1, dataElementUid1) + " + " + cons(constantUid1));
 
         when(value1.value()).thenReturn("3.5");
         when(constantModel.value()).thenReturn("2");
 
-        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid, programIndicatorUid);
+        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid1, programIndicatorUid);
 
         assertThat(result).isEqualTo("3.5 + 2");
     }
@@ -236,9 +255,9 @@ public class ProgramIndicatorEngineShould {
         when(programIndicator.expression()).thenReturn(var("event_date"));
 
         Date eventDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").parse("2018-05-05T00:00:00.000");
-        when(eventModel.eventDate()).thenReturn(eventDate);
+        when(event1.eventDate()).thenReturn(eventDate);
 
-        String result = programIndicatorEngine.parseIndicatorExpression(enrollmentUid, eventUid, programIndicatorUid);
+        String result = programIndicatorEngine.parseIndicatorExpression(enrollmentUid, eventUid1, programIndicatorUid);
 
         assertThat(result).isEqualTo("\"2018-05-05\"");
     }
@@ -246,13 +265,13 @@ public class ProgramIndicatorEngineShould {
     @Test
     public void parse_value_count_variable() throws Exception {
         when(programIndicator.expression()).thenReturn(
-                "(" + de(programStageUid, dataElementUid1) + " + " + de(programStageUid, dataElementUid2) +
+                "(" + de(programStageUid1, dataElementUid1) + " + " + de(programStageUid1, dataElementUid2) +
                         ") / " + var("value_count"));
 
         when(value1.value()).thenReturn("3.5");
         when(value2.value()).thenReturn("2");
 
-        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid, programIndicatorUid);
+        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid1, programIndicatorUid);
 
         assertThat(result).isEqualTo("(3.5 + 2.0) / 2");
     }
@@ -270,13 +289,53 @@ public class ProgramIndicatorEngineShould {
 
     @Test
     public void do_not_parse_d2_functions() {
-        when(programIndicator.expression()).thenReturn("d2:floor(" + de(programStageUid, dataElementUid1) + ")");
+        when(programIndicator.expression()).thenReturn("d2:floor(" + de(programStageUid1, dataElementUid1) + ")");
 
         when(value1.value()).thenReturn("3.5");
 
-        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid, programIndicatorUid);
+        String result = programIndicatorEngine.parseIndicatorExpression(null, eventUid1, programIndicatorUid);
 
         assertThat(result).isEqualTo("d2:floor(3.5)");
+    }
+
+    @Test
+    public void parse_values_from_different_program_stage_instances() {
+        when(programIndicator.expression()).thenReturn(
+                de(programStageUid1, dataElementUid1) + " + " + de(programStageUid2, dataElementUid4));
+
+        when(value1.value()).thenReturn("3.5");
+        when(value4.value()).thenReturn("2");
+
+        String resultWithoutEvent = programIndicatorEngine.parseIndicatorExpression(enrollmentUid, null,
+                programIndicatorUid);
+        String resultWithEvent1 = programIndicatorEngine.parseIndicatorExpression(enrollmentUid, eventUid1,
+                programIndicatorUid);
+        String resultWithEvent2 = programIndicatorEngine.parseIndicatorExpression(enrollmentUid, eventUid2,
+                programIndicatorUid);
+
+        assertThat(resultWithoutEvent).isEqualTo("3.5 + 2.0");
+        assertThat(resultWithEvent1).isEqualTo("3.5 + 2.0");
+        assertThat(resultWithEvent2).isEqualTo("3.5 + 2.0");
+    }
+
+    @Test
+    public void ignore_values_from_non_existing_events() {
+        when(programIndicator.expression()).thenReturn(
+                de(programStageUid1, dataElementUid1) + " + " + de(programStageUid2, dataElementUid4));
+
+        when(value1.value()).thenReturn("3.5");
+
+        // Event2 does not exist
+        when(eventStore.queryByUid(eventUid2)).thenReturn(null);
+        when(eventStore.queryByEnrollmentAndProgramStage(enrollmentUid, programStageUid2)).thenReturn(null);
+
+        String resultWithoutEvent = programIndicatorEngine.parseIndicatorExpression(enrollmentUid, null,
+                programIndicatorUid);
+        String resultWithEvent1 = programIndicatorEngine.parseIndicatorExpression(enrollmentUid, eventUid1,
+                programIndicatorUid);
+
+        assertThat(resultWithoutEvent).isEqualTo("3.5 + 0");
+        assertThat(resultWithEvent1).isEqualTo("3.5 + 0");
     }
 
 

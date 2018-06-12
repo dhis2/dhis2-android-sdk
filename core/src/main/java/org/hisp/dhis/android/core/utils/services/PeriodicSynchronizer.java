107e846bbdf6064dbb1d6dc4661b0ba4a5a8d457
Marcos Campos <marcamsn@gmail.com>
2018-06-12 17:31:14 +0200

    periodic-synchronizer: update PeriodicSynchronizer

--- core/src/main/java/org/hisp/dhis/android/core/utils/services/PeriodicSynchronizer.java
+++ core/src/main/java/org/hisp/dhis/android/core/utils/services/PeriodicSynchronizer.java
@@ -38,18 +38,17 @@ import android.util.Log;
 import org.hisp.dhis.android.core.D2;
 
 public class PeriodicSynchronizer extends BroadcastReceiver {
-
     public static final int FREQUENCY_ONE_MINUTE = 0;
     public static final int FREQUENCY_15_MINTUES = 1;
     public static final int FREQUENCY_ONE_HOUR = 2;
     public static final int FREQUENCY_ONE_DAY = 3;
     public static final int FREQUENCY_DISABLED = 4;
-    public static final int DEFAULT_UPDATE_FREQUENCY = FREQUENCY_ONE_HOUR;
+    public static final int DEFAULT_UPDATE_FREQUENCY = FREQUENCY_ONE_DAY;
 
-    public static final String CLASS_TAG = "PeriodicSynchronizer";
+    public static final String CLASS_TAG = PeriodicSynchronizer.class.getSimpleName();
 
     private static PeriodicSynchronizer periodicSynchronizer;
-    private int currentInterval = 15;
+    private int currentInterval = getIntervalFromFrequencyIndex(DEFAULT_UPDATE_FREQUENCY);
 
     public static PeriodicSynchronizer getInstance() {
         if (periodicSynchronizer == null) {
@@ -60,16 +59,21 @@ public class PeriodicSynchronizer extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (D2.d2 != null) {
-            try {
-                if (!D2.d2.isUserLoggedIn().call()) {
-                    cancelPeriodicSynchronizer(context);
-                }
-            } catch (Exception e) {
-                cancelPeriodicSynchronizer(context);
+        Log.d(CLASS_TAG, "onReceive.");
+        syncIfIsUserLoggedIn();
+    }
+
+    private static void syncIfIsUserLoggedIn() {
+        try {
+            if (D2.d2.isUserLoggedIn().call()) {
+                Log.d(CLASS_TAG, "Sync.");
+                D2Synchronizer.syncReservedValues();
+            } else {
+                Log.d(CLASS_TAG, "Not sync.");
             }
+        } catch (Exception e) {
+            Log.d(CLASS_TAG, e.getMessage());
         }
-        Log.d(CLASS_TAG, "PeriodicSynchronizer onReceive method");
     }
 
     public static void activatePeriodicSynchronizer(Context context, int minutes) {
@@ -77,51 +81,59 @@ public class PeriodicSynchronizer extends BroadcastReceiver {
             return;
         }
 
-        Log.d(CLASS_TAG, "Activate periodic synchronizer " + minutes);
-        Intent i = new Intent(context, PeriodicSynchronizer.class);
-        PendingIntent existingPi = PendingIntent.getBroadcast(context, 0, i, PendingIntent.FLAG_NO_CREATE);
+        Log.i(CLASS_TAG, "Activate periodic synchronizer each " + minutes + "minutes.");
 
-        if (existingPi == null) {
-            PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
-            long intervalMillis = 1000 * 60 * minutes; // Millisec * Second * Minute
+        Intent intent = new Intent(context, PeriodicSynchronizer.class);
+        PendingIntent existingPendingIntent = PendingIntent.getBroadcast(
+                context, 0, intent, PendingIntent.FLAG_NO_CREATE);
+
+        if (existingPendingIntent == null) {
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                    context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            long intervalMillis = 1000 * 60 * minutes;
             AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), intervalMillis, pi);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), intervalMillis,
+                    pendingIntent);
         }
+
+        syncIfIsUserLoggedIn();
     }
 
     public static void cancelPeriodicSynchronizer(Context context) {
-        Log.d(CLASS_TAG, "Cancel periodic synchronizer");
-        Intent intent = new Intent(context, PeriodicSynchronizer.class);
-        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_NO_CREATE);
+        Log.i(CLASS_TAG, "Cancel periodic synchronizer.");
 
-        if (sender != null) {
+        Intent intent = new Intent(context, PeriodicSynchronizer.class);
+        PendingIntent existingPendingIntent = PendingIntent.getBroadcast(
+                context, 0, intent, PendingIntent.FLAG_NO_CREATE);
+
+        if (existingPendingIntent != null) {
             AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            alarmManager.cancel(sender);
-            sender.cancel();
+            alarmManager.cancel(existingPendingIntent);
+            existingPendingIntent.cancel();
         }
     }
 
-    public static int getInterval(Context context) {
-        // TODO Get frequency from context
-        return getIntervalFromIndex(FREQUENCY_ONE_MINUTE);
+    public static int getInterval() {
+        // TODO Get interval from app settings frequency index
+        return getIntervalFromFrequencyIndex(DEFAULT_UPDATE_FREQUENCY);
     }
 
-    public static int getIntervalFromIndex(int frequencyIndex) {
+    public static int getIntervalFromFrequencyIndex(int frequencyIndex) {
         int minutes;
         switch (frequencyIndex) {
-            case FREQUENCY_ONE_MINUTE: // 1 minutes
+            case FREQUENCY_ONE_MINUTE:
                 minutes = 1;
                 break;
-            case FREQUENCY_15_MINTUES: // 15 minutes
+            case FREQUENCY_15_MINTUES:
                 minutes = 15;
                 break;
-            case FREQUENCY_ONE_HOUR: // 1 hour
+            case FREQUENCY_ONE_HOUR:
                 minutes = 60;
                 break;
-            case FREQUENCY_ONE_DAY:// 1 day
+            case FREQUENCY_ONE_DAY:
                 minutes = 60 * 24;
                 break;
-            case FREQUENCY_DISABLED: //disabled
+            case FREQUENCY_DISABLED:
                 minutes = 0;
                 break;
             default:
@@ -130,8 +142,7 @@ public class PeriodicSynchronizer extends BroadcastReceiver {
         return minutes;
     }
 
-    public static void reActivate(Context context) {
-        int interval = getInterval(context);
+    public static void reActivate(Context context, int interval) {
         if (interval != getInstance().currentInterval) {
             cancelPeriodicSynchronizer(context);
             activatePeriodicSynchronizer(context, interval);

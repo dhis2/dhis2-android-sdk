8b0695ff9ae1e4e64fad7a2d716d5f44c0695b45
Abel Castro <abelcsz@gmail.com>
2018-07-22 17:31:08 +0200

    Add support for greyed fields in data set sections to local DB

--- core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
+++ core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
@@ -42,7 +42,7 @@ import java.util.Map;
 
 public class DbOpenHelper extends SQLBriteOpenHelper {
 
-    public static final int VERSION = 8;
+    public static final int VERSION = 9;
 
     public DbOpenHelper(@NonNull Context context, @Nullable String databaseName) {
         super(context, databaseName, null, VERSION);

44fa1657e73a10e3d3abe1d5b8177c9b9f1edbaf
Marcos Campos <marcamsn@gmail.com>
2018-07-25 11:21:11 +0200

    sync-down-deleted-events: delete events on enrollmentHandler

--- core/src/main/java/org/hisp/dhis/android/core/enrollment/EnrollmentHandler.java
+++ core/src/main/java/org/hisp/dhis/android/core/enrollment/EnrollmentHandler.java
@@ -3,13 +3,17 @@ package org.hisp.dhis.android.core.enrollment;
 import android.support.annotation.NonNull;
 
 import org.hisp.dhis.android.core.common.GenericHandler;
+import org.hisp.dhis.android.core.common.OrphanCleaner;
+import org.hisp.dhis.android.core.common.OrphanCleanerImpl;
 import org.hisp.dhis.android.core.common.State;
 import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
 import org.hisp.dhis.android.core.enrollment.note.Note;
 import org.hisp.dhis.android.core.enrollment.note.NoteHandler;
 import org.hisp.dhis.android.core.enrollment.note.NoteModel;
 import org.hisp.dhis.android.core.enrollment.note.NoteModelBuilder;
+import org.hisp.dhis.android.core.event.Event;
 import org.hisp.dhis.android.core.event.EventHandler;
+import org.hisp.dhis.android.core.event.EventModel;
 
 import java.util.List;
 
@@ -19,13 +23,16 @@ public class EnrollmentHandler {
     private final EnrollmentStore enrollmentStore;
     private final EventHandler eventHandler;
     private final GenericHandler<Note, NoteModel> noteHandler;
+    private final OrphanCleaner<Enrollment, Event> eventOrphanCleaner;
 
     EnrollmentHandler(@NonNull DatabaseAdapter databaseAdapter,
-                             @NonNull EnrollmentStore enrollmentStore,
-                             @NonNull EventHandler eventHandler) {
+                      @NonNull EnrollmentStore enrollmentStore,
+                      @NonNull EventHandler eventHandler,
+                      @NonNull OrphanCleaner<Enrollment, Event> eventOrphanCleaner) {
         this.enrollmentStore = enrollmentStore;
         this.eventHandler = eventHandler;
         this.noteHandler = NoteHandler.create(databaseAdapter);
+        this.eventOrphanCleaner = eventOrphanCleaner;
     }
 
     public void handle(@NonNull List<Enrollment> enrollments) {
@@ -81,13 +88,16 @@ public class EnrollmentHandler {
             eventHandler.handleMany(enrollment.events());
             noteHandler.handleMany(enrollment.notes(), new NoteModelBuilder(enrollment));
         }
+        eventOrphanCleaner.deleteOrphan(enrollment, enrollment.events());
     }
 
     public static EnrollmentHandler create(DatabaseAdapter databaseAdapter) {
         return new EnrollmentHandler(
                 databaseAdapter,
                 new EnrollmentStoreImpl(databaseAdapter),
-                EventHandler.create(databaseAdapter)
+                EventHandler.create(databaseAdapter),
+                new OrphanCleanerImpl<Enrollment, Event>(EventModel.TABLE,
+                        EventModel.Columns.ENROLLMENT, databaseAdapter)
         );
     }
 }

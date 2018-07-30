ba6d98fc2d848e31def98ba8f1bcf375f6dc8fd3
Abel Castro <abelcsz@gmail.com>
2018-07-30 14:55:14 +0200

    Add link model between CategortOption and OrganisationUnit

---
+++ core/src/main/java/org/hisp/dhis/android/core/category/CategoryOptionOrganisationUnitLinkModel.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2017, University of Oslo
+ *
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.android.core.category;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteStatement;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.gabrielittner.auto.value.cursor.ColumnName;
+import com.google.auto.value.AutoValue;
+
+import org.hisp.dhis.android.core.common.BaseModel;
+import org.hisp.dhis.android.core.utils.Utils;
+
+import static org.hisp.dhis.android.core.utils.StoreUtils.sqLiteBind;
+
+@AutoValue
+public abstract class CategoryOptionOrganisationUnitLinkModel extends BaseModel {
+    public static final String TABLE = "CategoryOptionOrganisationUnitLink";
+
+    public static class Columns extends BaseModel.Columns {
+        public static final String CATEGORY_OPTION = "categoryOption";
+        public static final String ORGANISATION_UNIT = "organisationUnit";
+
+        @Override
+        public String[] all() {
+            return Utils.appendInNewArray(super.all(), CATEGORY_OPTION, ORGANISATION_UNIT);
+        }
+
+        @Override
+        public String[] whereUpdate() {
+            return new String[]{CATEGORY_OPTION, ORGANISATION_UNIT};
+        }
+    }
+
+    public static CategoryOptionOrganisationUnitLinkModel create(Cursor cursor) {
+        return AutoValue_CategoryOptionOrganisationUnitLinkModel.createFromCursor(cursor);
+    }
+
+    public static Builder builder() {
+        return new $$AutoValue_CategoryOptionOrganisationUnitLinkModel.Builder();
+    }
+
+    @Nullable
+    @ColumnName(Columns.CATEGORY_OPTION)
+    public abstract String categoryOption();
+
+    @Nullable
+    @ColumnName(Columns.ORGANISATION_UNIT)
+    public abstract String organisationUnit();
+
+    @Override
+    public void bindToStatement(@NonNull SQLiteStatement sqLiteStatement) {
+        sqLiteBind(sqLiteStatement, 1, categoryOption());
+        sqLiteBind(sqLiteStatement, 2, organisationUnit());
+    }
+
+    @Override
+    public void bindToUpdateWhereStatement(@NonNull SQLiteStatement sqLiteStatement) {
+        sqLiteBind(sqLiteStatement, 3, categoryOption());
+        sqLiteBind(sqLiteStatement, 4, organisationUnit());
+    }
+
+    @AutoValue.Builder
+    public static abstract class Builder extends BaseModel.Builder<Builder> {
+        public abstract Builder categoryOption(String categoryOption);
+
+        public abstract Builder organisationUnit(String organisationUnit);
+
+        public abstract CategoryOptionOrganisationUnitLinkModel build();
+    }
+}

1342d4eb7ed47bcd4def81a4d52a76fbb909a554
Abel Castro <abelcsz@gmail.com>
2018-07-30 16:12:20 +0200

    Add support for OrganisationUnit link in CategoryOption hanlder

--- core/src/main/java/org/hisp/dhis/android/core/category/CategoryOptionHandler.java
+++ core/src/main/java/org/hisp/dhis/android/core/category/CategoryOptionHandler.java
@@ -1,18 +1,39 @@
 package org.hisp.dhis.android.core.category;
 
 
-import org.hisp.dhis.android.core.common.GenericHandler;
+import org.hisp.dhis.android.core.common.HandleAction;
 import org.hisp.dhis.android.core.common.IdentifiableHandlerImpl;
 import org.hisp.dhis.android.core.common.IdentifiableObjectStore;
+import org.hisp.dhis.android.core.common.LinkModelHandler;
+import org.hisp.dhis.android.core.common.LinkModelHandlerImpl;
+import org.hisp.dhis.android.core.common.ObjectWithUid;
 import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
+import org.hisp.dhis.android.core.dataset.SectionDataElementLinkModel;
 
 public final class CategoryOptionHandler extends IdentifiableHandlerImpl<CategoryOption, CategoryOptionModel> {
 
-    private CategoryOptionHandler(IdentifiableObjectStore<CategoryOptionModel> categoryOptionStore) {
+    private final LinkModelHandler<ObjectWithUid, CategoryOptionOrganisationUnitLinkModel>
+            categoryOptionOrganisationUnitLinkHandler;
+
+    private CategoryOptionHandler(IdentifiableObjectStore<CategoryOptionModel> categoryOptionStore,
+                                  LinkModelHandler<ObjectWithUid, CategoryOptionOrganisationUnitLinkModel>
+                                          categoryOptionOrganisationUnitLinkHandler) {
         super(categoryOptionStore);
+
+        this.categoryOptionOrganisationUnitLinkHandler = categoryOptionOrganisationUnitLinkHandler;
     }
 
-    public static GenericHandler<CategoryOption, CategoryOptionModel> create(DatabaseAdapter databaseAdapter) {
-        return new IdentifiableHandlerImpl<>(CategoryOptionStore.create(databaseAdapter));
+    public static CategoryOptionHandler create(DatabaseAdapter databaseAdapter) {
+        return new CategoryOptionHandler(CategoryOptionStore.create(databaseAdapter),
+                new LinkModelHandlerImpl<ObjectWithUid, CategoryOptionOrganisationUnitLinkModel>(
+                        CategoryOptionOrganisationUnitLinkStore.create(databaseAdapter)));
+    }
+
+    @Override
+    protected void afterObjectHandled(CategoryOption categoryOption, HandleAction action) {
+
+        categoryOptionOrganisationUnitLinkHandler.handleMany(categoryOption.uid(), categoryOption.organizationUnits(),
+                new CategoryOptionOrganisationUnitLinkModelBuilder(categoryOption));
+
     }
 }

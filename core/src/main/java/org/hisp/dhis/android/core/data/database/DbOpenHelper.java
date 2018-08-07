3dfda5460f21b8d4027bcc8a59b78348e64233aa
Abel Castro <abelcsz@gmail.com>
2018-08-07 23:51:58 +0200

    Add categoryCombo field to DataSetDataElementLink table

--- core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
+++ core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
@@ -42,7 +42,7 @@ import java.util.Map;
 
 public class DbOpenHelper extends SQLBriteOpenHelper {
 
-    public static final int VERSION = 15;
+    public static final int VERSION = 16;
 
     public DbOpenHelper(@NonNull Context context, @Nullable String databaseName) {
         super(context, databaseName, null, VERSION);

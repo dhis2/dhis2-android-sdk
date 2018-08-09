a39632621ece4420915c301272b760d2fb7b5bb5
Abel Castro <abelcsz@gmail.com>
2018-08-09 13:30:13 +0200

    Add DataSetCompleteRegistrationTableInfo

---
+++ core/src/main/java/org/hisp/dhis/android/core/dataset/DataSetCompleteRegistrationTableInfo.java
@@ -0,0 +1,43 @@
+package org.hisp.dhis.android.core.dataset;
+
+import org.hisp.dhis.android.core.arch.db.TableInfo;
+import org.hisp.dhis.android.core.common.BaseModel;
+import org.hisp.dhis.android.core.utils.Utils;
+
+public final class DataSetCompleteRegistrationTableInfo {
+
+    private DataSetCompleteRegistrationTableInfo() {}
+
+    public static final TableInfo TABLE_INFO = new TableInfo() {
+
+        @Override
+        public String name() {
+            return "DataSetCompleteRegistration";
+        }
+
+        @Override
+        public BaseModel.Columns columns() {
+            return new DataSetCompleteRegistrationTableInfo.Columns();
+        }
+    };
+
+    static class Columns extends BaseModel.Columns {
+        @Override
+        public String[] all() {
+            return Utils.appendInNewArray(super.all(),
+                    DataSetCompleteRegistrationFields.PERIOD,
+                    DataSetCompleteRegistrationFields.DATA_SET,
+                    DataSetCompleteRegistrationFields.ORGANISATION_UNIT,
+                    DataSetCompleteRegistrationFields.ATTRIBUTE_OPTION_COMBO,
+                    DataSetCompleteRegistrationFields.DATE);
+        }
+
+        @Override
+        public String[] whereUpdate() {
+            return new String[]{DataSetCompleteRegistrationFields.PERIOD,
+                    DataSetCompleteRegistrationFields.DATA_SET,
+                    DataSetCompleteRegistrationFields.ORGANISATION_UNIT,
+                    DataSetCompleteRegistrationFields.ATTRIBUTE_OPTION_COMBO};
+        }
+    }
+}

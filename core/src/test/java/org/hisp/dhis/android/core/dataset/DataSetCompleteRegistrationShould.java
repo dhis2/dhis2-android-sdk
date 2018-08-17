bae78ad8831fb2cb879bdd8a80a08186bf5e2d6a
Abel Castro <abelcsz@gmail.com>
2018-08-17 13:03:05 +0200

    Remove unused imports from DataSetCompleteRegistrationShould

--- core/src/test/java/org/hisp/dhis/android/core/dataset/DataSetCompleteRegistrationShould.java
+++ core/src/test/java/org/hisp/dhis/android/core/dataset/DataSetCompleteRegistrationShould.java
@@ -31,7 +31,6 @@ package org.hisp.dhis.android.core.dataset;
 
 import org.hisp.dhis.android.core.common.BaseObjectShould;
 import org.hisp.dhis.android.core.common.ObjectShould;
-import org.hisp.dhis.android.core.common.SafeDateFormat;
 import org.junit.Test;
 
 import java.io.IOException;

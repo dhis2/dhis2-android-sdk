7beb388c0479f5eb8f4a16a21a2cf5986cc61dda
Abel Castro <abelcsz@gmail.com>
2018-08-21 12:27:08 +0200

    Add systemName column to SystemInfo table on local DB

--- core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
+++ core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
@@ -42,7 +42,7 @@ import java.util.Map;
 
 public class DbOpenHelper extends SQLBriteOpenHelper {
 
-    public static final int VERSION = 16;
+    public static final int VERSION = 17;
 
     public DbOpenHelper(@NonNull Context context, @Nullable String databaseName) {
         super(context, databaseName, null, VERSION);

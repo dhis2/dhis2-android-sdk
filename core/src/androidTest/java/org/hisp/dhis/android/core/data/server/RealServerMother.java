581782052e3ca03fbc130e43e56d6db179e7a364
Luis Delgado Romera <luisyng@gmail.com>
2018-09-06 14:42:58 +0200

    relationship-full-support: add android current as default server for tests

--- core/src/androidTest/java/org/hisp/dhis/android/core/data/server/RealServerMother.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/data/server/RealServerMother.java
@@ -4,7 +4,8 @@ public class RealServerMother {
     public static String url2_29 = "https://play.dhis2.org/2.29/api/";
     public static String url2_30 = "https://play.dhis2.org/2.30/api/";
     public static String url_dev = "https://play.dhis2.org/dev/api/";
-    public static String url = url2_29;
+    public static String android_current = "https://play.dhis2.org/android-current/api/";
+    public static String url = android_current;
     public static String user = "android";
     public static String password = "Android123";
 }

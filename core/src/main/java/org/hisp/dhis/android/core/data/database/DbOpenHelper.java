05bd56d23823d8644309193904ae5af5a6aae495
vgarciabnz <vgarciabnz@gmail.com>
2018-10-02 15:05:56 +0200

    Disable FK constraints to skip issues in 4.4

--- core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
+++ core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
@@ -57,7 +57,8 @@ public class DbOpenHelper extends SQLBriteOpenHelper {
         super.onOpen(db);
 
         // enable foreign key support in database
-        db.setForeignKeyConstraintsEnabled(true);
+        // TODO Only enable FK constraints if android version > 4.4
+        //db.setForeignKeyConstraintsEnabled(true);
         db.enableWriteAheadLogging();
     }
 

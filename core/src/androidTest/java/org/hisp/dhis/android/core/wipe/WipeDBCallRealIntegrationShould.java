72562b9e08fe7eb79a125771f4c7b83853f4e6be
Marcos Campos <marcamsn@gmail.com>
2018-10-03 09:14:59 +0200

    wipe-module: add tests on WipeDBCallRealIntegrationShould

--- core/src/androidTest/java/org/hisp/dhis/android/core/wipe/WipeDBCallRealIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/wipe/WipeDBCallRealIntegrationShould.java
@@ -7,9 +7,12 @@ import org.hisp.dhis.android.core.data.database.AbsStoreTestCase;
 import org.hisp.dhis.android.core.data.database.DatabaseAssert;
 import org.hisp.dhis.android.core.data.server.RealServerMother;
 import org.hisp.dhis.android.core.event.EventEndpointCall;
+import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceStoreImpl;
 import org.junit.Before;
 
 import java.io.IOException;
+
+import static com.google.common.truth.Truth.assertThat;
 
 public class WipeDBCallRealIntegrationShould extends AbsStoreTestCase {
     private D2 d2;
@@ -24,7 +27,7 @@ public class WipeDBCallRealIntegrationShould extends AbsStoreTestCase {
 
     //@Test
     public void have_empty_database_when_wipe_db_after_sync_metadata() throws Exception {
-        d2.logIn("android", "Android123").call();
+        d2.logIn(RealServerMother.user, RealServerMother.password).call();
 
         d2.syncMetaData().call();
 
@@ -37,7 +40,7 @@ public class WipeDBCallRealIntegrationShould extends AbsStoreTestCase {
 
     //@Test
     public void have_empty_database_when_wipe_db_after_sync_data() throws Exception {
-        d2.logIn("android", "Android123").call();
+        d2.logIn(RealServerMother.user, RealServerMother.password).call();
 
         d2.syncMetaData().call();
 
@@ -50,5 +53,40 @@ public class WipeDBCallRealIntegrationShould extends AbsStoreTestCase {
         d2.wipeModule().wipeEverything();
 
         DatabaseAssert.assertThatDatabase(databaseAdapter()).isEmpty();
+    }
+
+    //@Test
+    public void do_not_have_metadata_when_wipe_metadata_after_sync_metadata() throws Exception {
+        d2.logIn(RealServerMother.user, RealServerMother.password).call();
+
+        d2.syncMetaData().call();
+
+        DatabaseAssert.assertThatDatabase(databaseAdapter()).isNotEmpty();
+
+        d2.wipeModule().wipeMetadata();
+
+        DatabaseAssert.assertThatDatabase(databaseAdapter()).isEmpty();
+    }
+
+    //@Test
+    public void do_not_have_data_when_wipe_data_after_sync() throws Exception {
+        d2.logIn(RealServerMother.user, RealServerMother.password).call();
+
+        d2.syncMetaData().call();
+
+        d2.downloadTrackedEntityInstances(5, false).call();
+
+        TrackedEntityInstanceStoreImpl trackedEntityInstanceStore =
+                new TrackedEntityInstanceStoreImpl(databaseAdapter());
+
+        boolean hasTrackedEntities = trackedEntityInstanceStore.queryAll().values().iterator().hasNext();
+
+        assertThat(hasTrackedEntities).isTrue();
+
+        d2.wipeModule().wipeData();
+
+        hasTrackedEntities = trackedEntityInstanceStore.queryAll().values().iterator().hasNext();
+
+        assertThat(hasTrackedEntities).isFalse();
     }
 }

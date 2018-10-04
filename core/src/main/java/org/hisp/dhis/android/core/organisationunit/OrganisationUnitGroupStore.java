5a89202f9610f9b1b41493db77f676dffd7537ea
Luis Delgado Romera <luisyng@gmail.com>
2018-10-04 10:25:52 +0200

    object-factory-store-constructor: org unit group, org unit

--- core/src/main/java/org/hisp/dhis/android/core/organisationunit/OrganisationUnitGroupStore.java
+++ core/src/main/java/org/hisp/dhis/android/core/organisationunit/OrganisationUnitGroupStore.java
@@ -28,11 +28,13 @@
 
 package org.hisp.dhis.android.core.organisationunit;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteStatement;
 import android.support.annotation.NonNull;
 
 import org.hisp.dhis.android.core.arch.db.binders.IdentifiableStatementBinder;
 import org.hisp.dhis.android.core.arch.db.binders.StatementBinder;
+import org.hisp.dhis.android.core.common.CursorModelFactory;
 import org.hisp.dhis.android.core.common.IdentifiableObjectStore;
 import org.hisp.dhis.android.core.common.StoreFactory;
 import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
@@ -55,8 +57,16 @@ public class OrganisationUnitGroupStore {
         }
     };
 
+    private static final CursorModelFactory<OrganisationUnitGroupModel> FACTORY
+            = new CursorModelFactory<OrganisationUnitGroupModel>() {
+        @Override
+        public OrganisationUnitGroupModel fromCursor(Cursor cursor) {
+            return OrganisationUnitGroupModel.create(cursor);
+        }
+    };
+
     public static IdentifiableObjectStore<OrganisationUnitGroupModel> create(DatabaseAdapter databaseAdapter) {
         return StoreFactory.objectWithUidStore(databaseAdapter, OrganisationUnitGroupModel.TABLE,
-                new OrganisationUnitGroupModel.Columns().all(), BINDER);
+                new OrganisationUnitGroupModel.Columns().all(), BINDER, FACTORY);
     }
 }

5ce1ed30d6a2719a1c2fc09aebe9a7a17f92e221
Abel Castro <abelcsz@gmail.com>
2018-10-15 00:02:41 +0100

    [SDK-PERFORMANCE] Modify TrustManager to validate JMeter certificate

--- core/src/androidTest/java/org/hisp/dhis/android/core/common/D2Factory.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/common/D2Factory.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2004-2018, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.hisp.dhis.android.core.common;
 
 import android.support.test.InstrumentationRegistry;
@@ -11,31 +39,77 @@ import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
 
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 public class D2Factory {
     public static D2 create(String url, DatabaseAdapter databaseAdapter) {
-        ConfigurationModel config = ConfigurationModel.builder()
-                .serverUrl(HttpUrl.parse(url))
-                .build();
 
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        try {
 
-        return new D2.Builder()
-                .configuration(config)
-                .databaseAdapter(databaseAdapter)
-                .okHttpClient(
-                        new OkHttpClient.Builder()
-                                .addInterceptor(BasicAuthenticatorFactory.create(databaseAdapter))
-                                .addInterceptor(loggingInterceptor)
-                                .addNetworkInterceptor(new StethoInterceptor())
-                                .readTimeout(30, TimeUnit.SECONDS)
-                                .build()
-                )
-                .context(InstrumentationRegistry.getTargetContext().getApplicationContext())
-                .build();
+            ConfigurationModel config = ConfigurationModel.builder()
+                    .serverUrl(HttpUrl.parse(url))
+                    .build();
+
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            return new D2.Builder()
+                    .configuration(config)
+                    .databaseAdapter(databaseAdapter)
+                    .okHttpClient(
+                            new OkHttpClient.Builder()
+                                    .addInterceptor(BasicAuthenticatorFactory.create(databaseAdapter))
+                                    .addInterceptor(loggingInterceptor)
+                                    .addNetworkInterceptor(new StethoInterceptor())
+                                    .readTimeout(30, TimeUnit.SECONDS)
+                                    .sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0])
+                                    .hostnameVerifier(new HostnameVerifier() {
+                                        @Override
+                                        public boolean verify(String hostname, SSLSession session) {
+                                            return true;
+                                        }
+                                    })
+                                    .build()
+                    )
+                    .context(InstrumentationRegistry.getTargetContext().getApplicationContext())
+                    .build();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }

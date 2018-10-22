e5cda0d651f6ef4a6e59de8273593f5fb6b34f61
Luis Delgado Romera <luisyng@gmail.com>
2018-10-22 16:23:44 +0200

    [ANDROSDK-368] Make all stores private

--- core/src/main/java/org/hisp/dhis/android/core/category/CategoryStore.java
+++ core/src/main/java/org/hisp/dhis/android/core/category/CategoryStore.java
@@ -14,7 +14,7 @@ import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
 import static org.hisp.dhis.android.core.utils.StoreUtils.sqLiteBind;
 
 
-public final class CategoryStore {
+final class CategoryStore {
 
     private CategoryStore() {}
 

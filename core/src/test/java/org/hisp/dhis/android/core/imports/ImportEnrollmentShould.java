f3c1fcf49dda7e226f368fc827aaa6412e6f9406
Luis Delgado Romera <luisyng@gmail.com>
2018-11-08 16:30:43 +0100

    [ANDROSDK-436] imports package

--- core/src/test/java/org/hisp/dhis/android/core/imports/ImportEnrollmentShould.java
+++ core/src/test/java/org/hisp/dhis/android/core/imports/ImportEnrollmentShould.java
@@ -1,80 +1,27 @@
 package org.hisp.dhis.android.core.imports;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.hisp.dhis.android.core.Inject;
+import org.hisp.dhis.android.core.common.BaseObjectShould;
+import org.hisp.dhis.android.core.common.ObjectShould;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(JUnit4.class)
-public class ImportEnrollmentShould {
+public class ImportEnrollmentShould extends BaseObjectShould implements ObjectShould {
 
+    public ImportEnrollmentShould() {
+        super("imports/import_enrollment.json");
+    }
+
+    @Override
     @Test
-    public void map_from_json_string() throws Exception {
-        ObjectMapper objectMapper = Inject.objectMapper();
-
-        ImportEnrollment importEnrollment = objectMapper.readValue("{\n" +
-                "          \"responseType\": \"ImportSummaries\",\n" +
-                "          \"status\": \"SUCCESS\",\n" +
-                "          \"imported\": 0,\n" +
-                "          \"updated\": 1,\n" +
-                "          \"deleted\": 0,\n" +
-                "          \"ignored\": 0,\n" +
-                "          \"importSummaries\": [\n" +
-                "            {\n" +
-                "              \"responseType\": \"ImportSummary\",\n" +
-                "              \"status\": \"SUCCESS\",\n" +
-                "              \"importCount\": {\n" +
-                "                \"imported\": 0,\n" +
-                "                \"updated\": 1,\n" +
-                "                \"ignored\": 0,\n" +
-                "                \"deleted\": 0\n" +
-                "              },\n" +
-                "              \"reference\": \"XaBZwKbHVxS\",\n" +
-                "              \"events\": {\n" +
-                "                \"responseType\": \"ImportSummaries\",\n" +
-                "                \"status\": \"SUCCESS\",\n" +
-                "                \"imported\": 0,\n" +
-                "                \"updated\": 0,\n" +
-                "                \"deleted\": 0,\n" +
-                "                \"ignored\": 1,\n" +
-                "                \"importSummaries\": [\n" +
-                "                  {\n" +
-                "                    \"responseType\": \"ImportSummary\",\n" +
-                "                    \"status\": \"SUCCESS\",\n" +
-                "                    \"importCount\": {\n" +
-                "                      \"imported\": 0,\n" +
-                "                      \"updated\": 0,\n" +
-                "                      \"ignored\": 0,\n" +
-                "                      \"deleted\": 0\n" +
-                "                    },\n" +
-                "                    \"reference\": \"xqpUvfxT4PZ\"\n" +
-                "                  },\n" +
-                "                  {\n" +
-                "                    \"responseType\": \"ImportSummary\",\n" +
-                "                    \"status\": \"SUCCESS\",\n" +
-                "                    \"importCount\": {\n" +
-                "                      \"imported\": 0,\n" +
-                "                      \"updated\": 0,\n" +
-                "                      \"ignored\": 1,\n" +
-                "                      \"deleted\": 0\n" +
-                "                    },\n" +
-                "                    \"conflicts\": [\n" +
-                "                      {\n" +
-                "                        \"object\": \"bx6fsa0t90x\",\n" +
-                "                        \"value\": \"value_not_bool\"\n" +
-                "                      }\n" +
-                "                    ],\n" +
-                "                    \"reference\": \"DB2DLIEi2sX\"\n" +
-                "                  }\n" +
-                "                ]\n" +
-                "              }\n" +
-                "            }\n" +
-                "          ]\n" +
-                "        }", ImportEnrollment.class);
+    public void map_from_json_string() throws IOException, ParseException {
+        ImportEnrollment importEnrollment = objectMapper.readValue(jsonStream, ImportEnrollment.class);
 
         assertThat(importEnrollment.imported()).isEqualTo(0);
         assertThat(importEnrollment.updated()).isEqualTo(1);

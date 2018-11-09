d93e286ad3b6b52671b2c17fab20067ec645577d
Luis Delgado Romera <luisyng@gmail.com>
2018-11-09 16:53:16 +0100

    [ANDROSDK-190] Change ResourcesFileReader implementation

--- core/src/sharedTest/java/org/hisp/dhis/android/core/data/file/ResourcesFileReader.java
+++ core/src/sharedTest/java/org/hisp/dhis/android/core/data/file/ResourcesFileReader.java
@@ -1,15 +1,28 @@
 package org.hisp.dhis.android.core.data.file;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public final class ResourcesFileReader implements IFileReader {
     @Override
     public String getStringFromFile(String filename) throws IOException {
         InputStream fileStream = this.getClass().getClassLoader().getResourceAsStream(filename);
-        Scanner scanner = new java.util.Scanner(fileStream).useDelimiter("\\A");
-        return scanner.hasNext() ? scanner.next() : "";
+
+        StringBuilder textBuilder = new StringBuilder();
+        try (Reader reader = new BufferedReader(new InputStreamReader
+                (fileStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                textBuilder.append((char) c);
+            }
+        }
+
+        return textBuilder.toString();
     }
 }
 

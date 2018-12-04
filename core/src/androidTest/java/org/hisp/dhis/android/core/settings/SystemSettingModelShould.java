c255bff787cbd4f875868ab0a2fd901b8d28e37e
Luis Delgado Romera <luisyng@gmail.com>
2018-12-04 14:25:48 +0100

    [SYSTEM-SETTING-MODEL-SHOULD-MOVE] Move SystemSettingModelShould

--- core/src/androidTest/java/org/hisp/dhis/android/core/settings/SystemSettingModelShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/settings/SystemSettingModelShould.java
@@ -26,13 +26,12 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package settings;
+package org.hisp.dhis.android.core.settings;
 
 import android.content.ContentValues;
 import android.database.MatrixCursor;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.hisp.dhis.android.core.settings.SystemSettingModel;
 import org.hisp.dhis.android.core.settings.SystemSettingModel.Columns;
 import org.hisp.dhis.android.core.utils.Utils;
 import org.junit.Test;

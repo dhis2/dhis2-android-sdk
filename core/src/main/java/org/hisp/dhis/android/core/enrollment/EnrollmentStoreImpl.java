15fce2710ed477fe512369e9466c0fb94d014181
Marcos Campos <marcamsn@gmail.com>
2018-12-13 16:25:51 +0100

    [ANDROSDK-494] Adapt EnrollmentStoreImpl

--- core/src/main/java/org/hisp/dhis/android/core/enrollment/EnrollmentStoreImpl.java
+++ core/src/main/java/org/hisp/dhis/android/core/enrollment/EnrollmentStoreImpl.java
@@ -31,292 +31,109 @@ package org.hisp.dhis.android.core.enrollment;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteStatement;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
-import org.hisp.dhis.android.core.common.Coordinates;
-import org.hisp.dhis.android.core.common.State;
-import org.hisp.dhis.android.core.common.StoreWithStateImpl;
+import org.hisp.dhis.android.core.arch.db.binders.StatementBinder;
+import org.hisp.dhis.android.core.common.CoordinateHelper;
+import org.hisp.dhis.android.core.common.CursorModelFactory;
+import org.hisp.dhis.android.core.common.IdentifiableObjectWithStateStoreImpl;
+import org.hisp.dhis.android.core.common.SQLStatementBuilder;
+import org.hisp.dhis.android.core.common.SQLStatementWrapper;
 import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
-import org.hisp.dhis.android.core.enrollment.EnrollmentModel.Columns;
-import org.hisp.dhis.android.core.enrollment.note.Note;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.hisp.dhis.android.core.utils.StoreUtils.parse;
 import static org.hisp.dhis.android.core.utils.StoreUtils.sqLiteBind;
 
-@SuppressWarnings({
-        "PMD.AvoidDuplicateLiterals",
-        "PMD.CyclomaticComplexity",
-        "PMD.ModifiedCyclomaticComplexity",
-        "PMD.StdCyclomaticComplexity",
-        "PMD.AvoidInstantiatingObjectsInLoops"
-})
-public class EnrollmentStoreImpl extends StoreWithStateImpl implements EnrollmentStore {
+public class EnrollmentStoreImpl extends IdentifiableObjectWithStateStoreImpl<Enrollment> implements EnrollmentStore {
 
-    private static final String INSERT_STATEMENT = "INSERT INTO " + EnrollmentModel.TABLE + " (" +
-            Columns.UID + ", " +
-            Columns.CREATED + ", " +
-            Columns.LAST_UPDATED + ", " +
-            Columns.CREATED_AT_CLIENT + ", " +
-            Columns.LAST_UPDATED_AT_CLIENT + ", " +
-            Columns.ORGANISATION_UNIT + ", " +
-            Columns.PROGRAM + ", " +
-            Columns.ENROLLMENT_DATE + ", " +
-            Columns.INCIDENT_DATE + ", " +
-            Columns.FOLLOW_UP + ", " +
-            Columns.ENROLLMENT_STATUS + ", " +
-            Columns.TRACKED_ENTITY_INSTANCE + ", " +
-            Columns.LATITUDE + ", " +
-            Columns.LONGITUDE + ", " +
-            Columns.STATE + ") " +
-            "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
-
-    private static final String UPDATE_STATEMENT = "UPDATE " + EnrollmentModel.TABLE + " SET " +
-            Columns.UID + " =?, " +
-            Columns.CREATED + " =?, " +
-            Columns.LAST_UPDATED + " =?, " +
-            Columns.CREATED_AT_CLIENT + " =? , " +
-            Columns.LAST_UPDATED_AT_CLIENT + " =? , " +
-            Columns.ORGANISATION_UNIT + " =?, " +
-            Columns.PROGRAM + " =?, " +
-            Columns.ENROLLMENT_DATE + " =?, " +
-            Columns.INCIDENT_DATE + " =?, " +
-            Columns.FOLLOW_UP + " =?, " +
-            Columns.ENROLLMENT_STATUS + " =?, " +
-            Columns.TRACKED_ENTITY_INSTANCE + " =?, " +
-            Columns.LATITUDE + " =?, " +
-            Columns.LONGITUDE + " =?, " +
-            Columns.STATE + " =? " +
-            " WHERE " +
-            Columns.UID + " =?;";
-
-    private static final String DELETE_STATEMENT = "DELETE FROM " +
-            EnrollmentModel.TABLE + " WHERE " +
-            Columns.UID + " =?;";
-
-    private static final String FIELDS =
-            "  Enrollment.uid, " +
-                    "  Enrollment.created, " +
-                    "  Enrollment.lastUpdated, " +
-                    "  Enrollment.createdAtClient, " +
-                    "  Enrollment.lastUpdatedAtClient, " +
-                    "  Enrollment.organisationUnit, " +
-                    "  Enrollment.program, " +
-                    "  Enrollment.enrollmentDate, " +
-                    "  Enrollment.incidentDate, " +
-                    "  Enrollment.followup, " +
-                    "  Enrollment.status, " +
-                    "  Enrollment.trackedEntityInstance, " +
-                    "  Enrollment.latitude, " +
-                    "  Enrollment.longitude, " +
-                    "  Enrollment.state ";
-
-    private static final String QUERY_STATEMENT_TO_POST = "SELECT " +
-            FIELDS +
-            "FROM (Enrollment " +
-            "  INNER JOIN TrackedEntityInstance ON Enrollment.trackedEntityInstance = TrackedEntityInstance.uid " +
-            ") " +
-            "WHERE TrackedEntityInstance.state = 'TO_POST' OR TrackedEntityInstance.state = 'TO_UPDATE'" +
-            " OR TrackedEntityInstance.state = 'TO_DELETE' " +
-            " OR Enrollment.state = 'TO_POST' OR Enrollment.state = 'TO_UPDATE' OR Enrollment.state = 'TO_DELETE';";
-
-    private static final String QUERY_STATEMENT = "SELECT " +
-            FIELDS +
-            " FROM Enrollment;";
-
-    private static final String QUERY_BY_UID = "SELECT " +
-            FIELDS +
-            " FROM Enrollment WHERE Enrollment.uid = '?'";
-
-    private final SQLiteStatement insertStatement;
-    private final SQLiteStatement updateStatement;
-    private final SQLiteStatement deleteStatement;
-
-    public EnrollmentStoreImpl(DatabaseAdapter databaseAdapter) {
-        super(databaseAdapter, EnrollmentModel.TABLE);
-        this.insertStatement = databaseAdapter.compileStatement(INSERT_STATEMENT);
-        this.updateStatement = databaseAdapter.compileStatement(UPDATE_STATEMENT);
-        this.deleteStatement = databaseAdapter.compileStatement(DELETE_STATEMENT);
+    private EnrollmentStoreImpl(DatabaseAdapter databaseAdapter,
+                                SQLStatementWrapper statementWrapper,
+                                SQLStatementBuilder builder,
+                                StatementBinder<Enrollment> binder,
+                                CursorModelFactory<Enrollment> modelFactory) {
+        super(databaseAdapter, statementWrapper, builder, binder, modelFactory);
     }
 
     @Override
-    public long insert(@NonNull String uid, @Nullable Date created, @Nullable Date lastUpdated,
-            @Nullable String createdAtClient, @Nullable String lastUpdatedAtClient,
-            @NonNull String organisationUnit, @NonNull String program,
-            @Nullable Date enrollmentDate,
-            @Nullable Date incidentDate, @Nullable Boolean followUp,
-            @Nullable EnrollmentStatus enrollmentStatus, @NonNull String trackedEntityInstance,
-            @Nullable String latitude, @Nullable String longitude, @Nullable State state) {
-        sqLiteBind(insertStatement, 1, uid);
-        sqLiteBind(insertStatement, 2, created);
-        sqLiteBind(insertStatement, 3, lastUpdated);
-        sqLiteBind(insertStatement, 4, createdAtClient);
-        sqLiteBind(insertStatement, 5, lastUpdatedAtClient);
-        sqLiteBind(insertStatement, 6, organisationUnit);
-        sqLiteBind(insertStatement, 7, program);
-        sqLiteBind(insertStatement, 8, enrollmentDate);
-        sqLiteBind(insertStatement, 9, incidentDate);
-        sqLiteBind(insertStatement, 10, followUp);
-        sqLiteBind(insertStatement, 11, enrollmentStatus);
-        sqLiteBind(insertStatement, 12, trackedEntityInstance);
-        sqLiteBind(insertStatement, 13, latitude);
-        sqLiteBind(insertStatement, 14, longitude);
-        sqLiteBind(insertStatement, 15, state);
+    public Map<String, List<Enrollment>> queryEnrollmentsToPost() {
+        String enrollmentsToPostQuery = "SELECT Enrollment.* FROM " +
+                "(Enrollment INNER JOIN TrackedEntityInstance " +
+                "ON Enrollment.trackedEntityInstance = TrackedEntityInstance.uid) " +
+                "WHERE TrackedEntityInstance.state = 'TO_POST' " +
+                "OR TrackedEntityInstance.state = 'TO_UPDATE' " +
+                "OR TrackedEntityInstance.state = 'TO_DELETE' " +
+                "OR Enrollment.state = 'TO_POST' " +
+                "OR Enrollment.state = 'TO_UPDATE' " +
+                "OR Enrollment.state = 'TO_DELETE';";
 
-        long insert = databaseAdapter.executeInsert(EnrollmentModel.TABLE, insertStatement);
-        insertStatement.clearBindings();
+        List<Enrollment> enrollmentList = enrollmentListFromQuery(enrollmentsToPostQuery);
 
-        return insert;
-    }
-
-    @Override
-    public int delete(@NonNull String uid) {
-        sqLiteBind(deleteStatement, 1, uid);
-
-        int rowId = deleteStatement.executeUpdateDelete();
-        deleteStatement.clearBindings();
-
-        return rowId;
-    }
-
-    @Override
-    public int update(@NonNull String uid, @NonNull Date created, @NonNull Date lastUpdated,
-            @Nullable String createdAtClient, @Nullable String lastUpdatedAtClient,
-            @NonNull String organisationUnit, @NonNull String program,
-            @NonNull Date enrollmentDate, @Nullable Date incidentDate,
-            @Nullable Boolean followUp, @NonNull EnrollmentStatus enrollmentStatus,
-            @NonNull String trackedEntityInstance, @Nullable String latitude,
-            @Nullable String longitude, @NonNull State state, @NonNull String whereEnrollmentUid) {
-
-        sqLiteBind(updateStatement, 1, uid);
-        sqLiteBind(updateStatement, 2, created);
-        sqLiteBind(updateStatement, 3, lastUpdated);
-        sqLiteBind(updateStatement, 4, createdAtClient);
-        sqLiteBind(updateStatement, 5, lastUpdatedAtClient);
-        sqLiteBind(updateStatement, 6, organisationUnit);
-        sqLiteBind(updateStatement, 7, program);
-        sqLiteBind(updateStatement, 8, enrollmentDate);
-        sqLiteBind(updateStatement, 9, incidentDate);
-        sqLiteBind(updateStatement, 10, followUp);
-        sqLiteBind(updateStatement, 11, enrollmentStatus);
-        sqLiteBind(updateStatement, 12, trackedEntityInstance);
-        sqLiteBind(updateStatement, 13, latitude);
-        sqLiteBind(updateStatement, 14, longitude);
-        sqLiteBind(updateStatement, 15, state);
-
-        // bind the where clause
-        sqLiteBind(updateStatement, 16, whereEnrollmentUid);
-
-        int rowId = databaseAdapter.executeUpdateDelete(EnrollmentModel.TABLE, updateStatement);
-        updateStatement.clearBindings();
-
-        return rowId;
-    }
-
-    @Override
-    public Map<String, List<Enrollment>> query() {
-        Cursor cursor = databaseAdapter.query(QUERY_STATEMENT_TO_POST);
-        return mapFromCursor(cursor);
-    }
-
-    @Override
-    public Map<String, List<Enrollment>> queryAll() {
-        Cursor cursor = databaseAdapter.query(QUERY_STATEMENT);
-        return mapFromCursor(cursor);
-    }
-
-    @Override
-    public EnrollmentModel queryByUid(String enrollmentUid) {
-        String queryStatement = QUERY_BY_UID.replace("?", enrollmentUid);
-
-        Cursor cursor = databaseAdapter.query(queryStatement);
-
-        EnrollmentModel object = null;
-        try {
-            if (cursor.getCount() > 0) {
-                cursor.moveToFirst();
-                object = EnrollmentModel.create(cursor);
-            }
-        } finally {
-            cursor.close();
-        }
-
-        return object;
-    }
-
-    private Map<String, List<Enrollment>> mapFromCursor(Cursor cursor) {
         Map<String, List<Enrollment>> enrollmentMap = new HashMap<>();
-
-        try {
-            if (cursor.getCount() > 0) {
-                cursor.moveToFirst();
-                do {
-                    String uid = cursor.getString(0);
-                    Date created = cursor.getString(1) == null ? null : parse(cursor.getString(1));
-                    Date lastUpdated = cursor.getString(2) == null ? null : parse(
-                            cursor.getString(2));
-                    String createdAtClient = cursor.getString(3) == null ? null : cursor.getString(
-                            3);
-                    String lastUpdatedAtClient = cursor.getString(4) == null ? null
-                            : cursor.getString(4);
-                    String organisationUnit = cursor.getString(5) == null ? null : cursor.getString(
-                            5);
-                    String program = cursor.getString(6) == null ? null : cursor.getString(6);
-                    Date enrollmentDate = cursor.getString(7) == null ? null : parse(
-                            cursor.getString(7));
-                    Date incidentDate = cursor.getString(8) == null ? null : parse(
-                            cursor.getString(8));
-                    Boolean followUp =
-                            cursor.getString(9) == null || cursor.getInt(9) == 0 ? Boolean.FALSE
-                                    : Boolean.TRUE;
-                    EnrollmentStatus status =
-                            cursor.getString(10) == null ? null : EnrollmentStatus.valueOf(
-                                    cursor.getString(10));
-                    String trackedEntityInstance = cursor.getString(11) == null ? null
-                            : cursor.getString(11);
-                    Double latitude = cursor.getString(12) == null ?
-                            null : Double.parseDouble(cursor.getString(12));
-                    Double longitude = cursor.getString(13) == null ?
-                            null : Double.parseDouble(cursor.getString(13));
-                    String stateStr = cursor.getString(14);
-
-                    Boolean deleted = stateStr != null && stateStr.equals(State.TO_DELETE.toString());
-
-                    if (enrollmentMap.get(trackedEntityInstance) == null) {
-                        enrollmentMap.put(trackedEntityInstance, new ArrayList<Enrollment>());
-                    }
-
-                    Coordinates coordinates = null;
-
-                    if (latitude != null && longitude != null) {
-                        coordinates = Coordinates.create(latitude, longitude);
-                    }
-
-                    enrollmentMap.get(trackedEntityInstance).add(Enrollment.create(
-                            uid, created, lastUpdated, createdAtClient, lastUpdatedAtClient,
-                            organisationUnit, program, enrollmentDate, incidentDate, followUp,
-                            status, trackedEntityInstance, coordinates, deleted, null,
-                            Collections.<Note>emptyList()
-                    ));
-                }
-                while (cursor.moveToNext());
-            }
-
-        } finally {
-            cursor.close();
+        for (Enrollment enrollment : enrollmentList) {
+            addEnrollmentToMap(enrollmentMap, enrollment);
         }
 
         return enrollmentMap;
     }
 
-    @Override
-    public int delete() {
-        return databaseAdapter.delete(EnrollmentModel.TABLE);
+    private List<Enrollment> enrollmentListFromQuery(String query) {
+        List<Enrollment> enrollmentList = new ArrayList<>();
+        Cursor cursor = databaseAdapter.query(query);
+        addObjectsToCollection(cursor, enrollmentList);
+        return enrollmentList;
+    }
+
+    private void addEnrollmentToMap(Map<String, List<Enrollment>> enrollmentMap, Enrollment enrollment) {
+        if (enrollmentMap.get(enrollment.trackedEntityInstance()) == null) {
+            enrollmentMap.put(enrollment.trackedEntityInstance(), new ArrayList<Enrollment>());
+        }
+
+        enrollmentMap.get(enrollment.trackedEntityInstance()).add(enrollment);
+    }
+
+    private static final StatementBinder<Enrollment> BINDER = new StatementBinder<Enrollment>() {
+        @Override
+        public void bindToStatement(@NonNull Enrollment o, @NonNull SQLiteStatement sqLiteStatement) {
+            sqLiteBind(sqLiteStatement, 1, o.uid());
+            sqLiteBind(sqLiteStatement, 2, o.created());
+            sqLiteBind(sqLiteStatement, 3, o.lastUpdated());
+            sqLiteBind(sqLiteStatement, 4, o.createdAtClient());
+            sqLiteBind(sqLiteStatement, 5, o.lastUpdatedAtClient());
+            sqLiteBind(sqLiteStatement, 6, o.organisationUnit());
+            sqLiteBind(sqLiteStatement, 7, o.program());
+            sqLiteBind(sqLiteStatement, 8, o.enrollmentDate());
+            sqLiteBind(sqLiteStatement, 9, o.incidentDate());
+            sqLiteBind(sqLiteStatement, 10, o.followUp());
+            sqLiteBind(sqLiteStatement, 11, o.status());
+            sqLiteBind(sqLiteStatement, 12, o.trackedEntityInstance());
+            sqLiteBind(sqLiteStatement, 13, CoordinateHelper.getLatitude(o.coordinate()));
+            sqLiteBind(sqLiteStatement, 14, CoordinateHelper.getLongitude(o.coordinate()));
+            sqLiteBind(sqLiteStatement, 15, o.state());
+        }
+    };
+
+    private static final CursorModelFactory<Enrollment> FACTORY = new CursorModelFactory<Enrollment>() {
+        @Override
+        public Enrollment fromCursor(Cursor cursor) {
+            return Enrollment.create(cursor);
+        }
+    };
+
+    public static EnrollmentStore create(DatabaseAdapter databaseAdapter) {
+        SQLStatementBuilder statementBuilder = new SQLStatementBuilder(
+                EnrollmentTableInfo.TABLE_INFO.name(),
+                EnrollmentTableInfo.TABLE_INFO.columns());
+        SQLStatementWrapper statementWrapper = new SQLStatementWrapper(statementBuilder, databaseAdapter);
+
+        return new EnrollmentStoreImpl(
+                databaseAdapter,
+                statementWrapper,
+                statementBuilder,
+                BINDER,
+                FACTORY
+        );
     }
 }

b3e6774bc56a2e1aec75c2b65e424d02ce8f6c35
Pawel Szymanski <pawel.szymanski@ehealthafrica.org>
2018-12-22 11:49:05 +0100

    ANDROSDK-499 Added comments

--- core/src/main/java/org/hisp/dhis/android/core/sms/domain/repository/SmsRepository.java
+++ core/src/main/java/org/hisp/dhis/android/core/sms/domain/repository/SmsRepository.java
@@ -5,12 +5,33 @@ import io.reactivex.Observable;
 
 public interface SmsRepository {
 
+    /**
+     * Sends given text by sms
+     *
+     * @param number                Recipient phone number
+     * @param value                 Text data to send
+     * @param sendingTimeoutSeconds After this time error will be returned.
+     * @return Observable that emits current status of sending
+     */
     Observable<SmsSendingState> sendSms(String number, String value, int sendingTimeoutSeconds);
 
+    /**
+     * Starts process of listening to result confirmation sms
+     *
+     * @param waitingTimeoutSeconds After this time error will be returned.
+     * @return Completable that is completed when result sms is successfully received
+     */
     Completable listenToConfirmationSms(int waitingTimeoutSeconds);
 
+    /**
+     * Sending status Observable may emit WAITING_TOTAL_CONFIRMATION, as a protection for sending too
+     * many messages. Then this method has to be called to resume sms sending.
+     */
     void confirmTotalCount();
 
+    /**
+     * Returned when sms sending error is returned from OS.
+     */
     class ReceivedErrorException extends Exception {
         private int errorCode;
 
@@ -23,9 +44,15 @@ public interface SmsRepository {
         }
     }
 
+    /**
+     * Returned when timeout occurs
+     */
     class TimeoutException extends Exception {
     }
 
+    /**
+     * Shows the current status of sending task.
+     */
     class SmsSendingState {
         private State state;
         private int sent;
@@ -37,14 +64,23 @@ public interface SmsRepository {
             this.total = total;
         }
 
+        /**
+         * @return Amount of messages sent to this moment
+         */
         public int getSent() {
             return sent;
         }
 
+        /**
+         * @return Total number of messages that will be sent
+         */
         public int getTotal() {
             return total;
         }
 
+        /**
+         * @return Current sending state enum
+         */
         public State getState() {
             return state;
         }

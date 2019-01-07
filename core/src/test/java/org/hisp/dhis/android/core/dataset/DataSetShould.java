b8bd07efa039ec2346241d32c844a7d14339bdf1
Luis Delgado Romera <luisyng@gmail.com>
2019-01-07 18:12:45 +0100

    [ANDROSDK-526] Fix tests

--- core/src/test/java/org/hisp/dhis/android/core/dataset/DataSetShould.java
+++ core/src/test/java/org/hisp/dhis/android/core/dataset/DataSetShould.java
@@ -35,6 +35,7 @@ import org.hisp.dhis.android.core.common.DataAccess;
 import org.hisp.dhis.android.core.common.ObjectShould;
 import org.hisp.dhis.android.core.common.ObjectStyle;
 import org.hisp.dhis.android.core.common.ObjectWithUid;
+import org.hisp.dhis.android.core.indicator.Indicator;
 import org.hisp.dhis.android.core.period.PeriodType;
 import org.junit.Test;
 
@@ -81,7 +82,7 @@ public class DataSetShould extends BaseObjectShould implements ObjectShould {
         assertThat(dataSet.dataSetElements().size()).isEqualTo(3);
         assertThat(dataSet.dataInputPeriods().size()).isEqualTo(1);
         assertThat(dataSet.dataInputPeriods().get(0).period()).isEqualTo(ObjectWithUid.create("201801"));
-        assertThat(dataSet.indicators().get(0)).isEqualTo(ObjectWithUid.create("OEWO2PpiUKx"));
+        assertThat(dataSet.indicators().get(0)).isEqualTo(Indicator.builder().uid("OEWO2PpiUKx").build());
         assertThat(dataSet.sections().size()).isEqualTo(1);
         assertThat(dataSet.sections().get(0).uid()).isEqualTo("Y2rk0vzgvAx");
         assertThat(dataSet.compulsoryDataElementOperands().size()).isEqualTo(1);

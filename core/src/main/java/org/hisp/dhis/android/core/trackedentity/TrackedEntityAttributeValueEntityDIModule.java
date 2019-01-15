49edbc920ffe35aece515e7728413fae7263316b
Luis Delgado Romera <luisyng@gmail.com>
2019-01-15 18:18:15 +0100

    [ANDROSDK-546] Move programIndicatorEngine methods from D2 to ProgramIndicatorEngine

--- core/src/main/java/org/hisp/dhis/android/core/trackedentity/TrackedEntityAttributeValueEntityDIModule.java
+++ core/src/main/java/org/hisp/dhis/android/core/trackedentity/TrackedEntityAttributeValueEntityDIModule.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2004-2018, University of Oslo
- * All rights reserved.
+ * Copyright (c) 2017, University of Oslo
  *
+ * All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * Redistributions of source code must retain the above copyright notice, this
@@ -26,27 +26,27 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.hisp.dhis.android.core.program;
+package org.hisp.dhis.android.core.trackedentity;
 
-import org.hisp.dhis.android.core.arch.repositories.collection.ReadOnlyIdentifiableCollectionRepository;
-import org.hisp.dhis.android.core.utils.services.ProgramIndicatorEngine;
+import org.hisp.dhis.android.core.arch.handlers.SyncHandlerWithTransformer;
+import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
 
-import javax.inject.Inject;
-
+import dagger.Module;
+import dagger.Provides;
 import dagger.Reusable;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
-@SuppressFBWarnings("URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
-@Reusable
-public final class ProgramModule {
+@Module
+public final class TrackedEntityAttributeValueEntityDIModule {
 
-    public final ReadOnlyIdentifiableCollectionRepository<Program> programs;
-    public final ProgramIndicatorEngine programIndicatorEngine;
+    @Provides
+    @Reusable
+    public TrackedEntityAttributeValueStore store(DatabaseAdapter databaseAdapter) {
+        return TrackedEntityAttributeValueStoreImpl.create(databaseAdapter);
+    }
 
-    @Inject
-    ProgramModule(ReadOnlyIdentifiableCollectionRepository<Program> programs,
-                  ProgramIndicatorEngine programIndicatorEngine) {
-        this.programs = programs;
-        this.programIndicatorEngine = programIndicatorEngine;
+    @Provides
+    @Reusable
+    public SyncHandlerWithTransformer<TrackedEntityAttributeValue> handler(TrackedEntityAttributeValueStore store) {
+        return new TrackedEntityAttributeValueHandler(store);
     }
 }

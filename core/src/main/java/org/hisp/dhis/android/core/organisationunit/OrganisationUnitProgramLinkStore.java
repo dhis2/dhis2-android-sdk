bc8afc257d3ef58b2e60bbfe5296bf7a35191ecb
Abel Castro <abelcsz@gmail.com>
2019-02-04 16:46:20 +0100

    [ANDROSDK-468] Modify class level acces to allow testing

--- core/src/main/java/org/hisp/dhis/android/core/organisationunit/OrganisationUnitProgramLinkStore.java
+++ core/src/main/java/org/hisp/dhis/android/core/organisationunit/OrganisationUnitProgramLinkStore.java
@@ -39,7 +39,7 @@ import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
 
 import static org.hisp.dhis.android.core.utils.StoreUtils.sqLiteBind;
 
-final class OrganisationUnitProgramLinkStore {
+public final class OrganisationUnitProgramLinkStore {
 
     private OrganisationUnitProgramLinkStore() {}
 

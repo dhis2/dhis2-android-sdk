8faf650710437751d3b281efc553edd6cbe30250
Victor Garcia <vgarciabnz@gmail.com>
2019-02-24 20:12:25 +0100

    [TRAVIS-PERFORMANCE] Interactive androidTest

--- build_androidTest.sh
+++ build_androidTest.sh
@@ -6,4 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR=$DIR/
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
-"$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest
+"$PROJECT_DIR"/gradlew --no-daemon -i connectedAndroidTest

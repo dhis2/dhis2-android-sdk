6daadae4929ef87bbb4cd0cdedff6bbd7b83c910
Luis Delgado Romera <luisyng@gmail.com>
2019-02-26 18:25:21 +0100

    [BUILD-SINGLE-COMMAND] Build single gradle command

--- build.sh
+++ build.sh
@@ -6,7 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR=$DIR/
 
 # This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
-"$PROJECT_DIR"/gradlew --no-daemon clean
-"$PROJECT_DIR"/gradlew --no-daemon build -Dscan
-"$PROJECT_DIR"/gradlew --no-daemon test
-"$PROJECT_DIR"/gradlew --no-daemon connectedAndroidTest
+"$PROJECT_DIR"/gradlew --no-daemon clean build -Dscan connectedAndroidTest

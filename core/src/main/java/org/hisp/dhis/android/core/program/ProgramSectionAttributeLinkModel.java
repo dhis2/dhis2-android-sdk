6fb1b54dd032121af3520babbd92d1f974e36322
Marcos Campos <marcamsn@gmail.com>
2019-02-27 17:55:58 +0100

    [ANDROSDK-690] Unify ProgramSectionAttributeLink handler and store

--- core/src/main/java/org/hisp/dhis/android/core/program/ProgramSectionAttributeLinkModel.java
+++ core/src/main/java/org/hisp/dhis/android/core/program/ProgramSectionAttributeLinkModel.java
@@ -35,6 +35,7 @@ import com.google.auto.value.AutoValue;
 import org.hisp.dhis.android.core.common.BaseModel;
 import org.hisp.dhis.android.core.utils.Utils;
 
+@Deprecated
 @AutoValue
 public abstract class ProgramSectionAttributeLinkModel extends BaseModel {
     public static final String TABLE = "ProgramSectionAttributeLink";

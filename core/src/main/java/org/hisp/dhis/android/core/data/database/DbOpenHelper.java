66436ff24cb1d59ec1dbb9183f923adbd6c251af
Marcos Campos <marcamsn@gmail.com>
2019-03-08 10:16:03 +0100

    [ANDROSDK-664] Fix wrong pointing table foreign key on ProgramSectionAttributeLink

--- core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
+++ core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
@@ -43,7 +43,7 @@ import java.util.Map;
 
 public class DbOpenHelper extends SQLBriteOpenHelper {
 
-    public static final int VERSION = 44;
+    public static final int VERSION = 45;
 
     public DbOpenHelper(@NonNull Context context, @Nullable String databaseName) {
         super(context, databaseName, null, VERSION);

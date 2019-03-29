0f016549e1434891e259e61a4a6d7c6ce237ddaf
Abel Castro <abelcsz@gmail.com>
2019-03-29 07:56:17 +0100

    [ANDROSDK-28] Add DataApprovalStore

---
+++ core/src/main/java/org/hisp/dhis/android/core/dataapproval/DataApprovalStore.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2004-2019, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.android.core.dataapproval;
+
+
+import org.hisp.dhis.android.core.arch.db.binders.StatementBinder;
+import org.hisp.dhis.android.core.arch.db.binders.WhereStatementBinder;
+import org.hisp.dhis.android.core.common.ObjectWithoutUidStore;
+import org.hisp.dhis.android.core.common.StoreFactory;
+import org.hisp.dhis.android.core.data.database.DatabaseAdapter;
+
+import static org.hisp.dhis.android.core.utils.StoreUtils.sqLiteBind;
+
+final class DataApprovalStore {
+
+    private static final StatementBinder<DataApproval> BINDER
+            = (dataApproval, sqLiteStatement) -> {
+        sqLiteBind(sqLiteStatement, 1, dataApproval.workflow());
+        sqLiteBind(sqLiteStatement, 2, dataApproval.organisationUnit());
+        sqLiteBind(sqLiteStatement, 3, dataApproval.period());
+        sqLiteBind(sqLiteStatement, 4, dataApproval.attributeOptionCombo());
+        sqLiteBind(sqLiteStatement, 5, dataApproval.state());
+    };
+
+    private static final WhereStatementBinder<DataApproval> WHERE_UPDATE_BINDER
+            = (dataApproval, sqLiteStatement) -> {
+
+        sqLiteBind(sqLiteStatement, 6, dataApproval.workflow());
+        sqLiteBind(sqLiteStatement, 7, dataApproval.organisationUnit());
+        sqLiteBind(sqLiteStatement, 8, dataApproval.period());
+        sqLiteBind(sqLiteStatement, 9, dataApproval.attributeOptionCombo());
+    };
+
+    public static ObjectWithoutUidStore<DataApproval> create(DatabaseAdapter databaseAdapter) {
+        return StoreFactory.objectWithoutUidStore(databaseAdapter, DataApprovalTableInfo.TABLE_INFO,
+                BINDER, WHERE_UPDATE_BINDER, DataApproval::create);
+    }
+
+    private DataApprovalStore() {}
+
+}

18cd89ab34f3301c0d68cf9dff1aa192ff54022f
Victor Garcia <vgarciabnz@gmail.com>
2019-04-02 08:41:18 +0200

    [ANDROSDK-757] Eq operator like LIKE

--- core/src/main/java/org/hisp/dhis/android/core/trackedentity/search/QueryOperator.java
+++ core/src/main/java/org/hisp/dhis/android/core/trackedentity/search/QueryOperator.java
@@ -29,7 +29,9 @@
 package org.hisp.dhis.android.core.trackedentity.search;
 
 public enum QueryOperator {
-    LIKE ("LIKE"), EQ ("=");
+    LIKE ("LIKE"),
+    EQ ("LIKE")     // Intentionally LIKE to perform case insensitive matching
+    ;
 
     private String sqlOperator;
 

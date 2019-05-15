3ed872902e102f818603d17ca8502ca0b9dfbf10
Luis Delgado Romera <luisyng@gmail.com>
2019-05-15 14:35:38 +0200

    [ANDROSDK-811] Findbugs

--- core/src/main/java/org/hisp/dhis/android/core/trackedentity/TrackedEntityInstanceWithLimitCallFactory.java
+++ core/src/main/java/org/hisp/dhis/android/core/trackedentity/TrackedEntityInstanceWithLimitCallFactory.java
@@ -218,7 +218,7 @@ public final class TrackedEntityInstanceWithLimitCallFactory {
         });
     }
 
-    private class TeiListWithPaging {
+    private static class TeiListWithPaging {
         final boolean isSuccess;
         final List<TrackedEntityInstance> teiList;
         public final Paging paging;

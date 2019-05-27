328400a7da6f8c9a3f901d79d436fc8c52f5683d
Luis Delgado Romera <luisyng@gmail.com>
2019-05-27 09:40:02 +0200

    [ANDROSDK-827] Reduce execution time of EventPostCallMockIntegrationShould

---
+++ core/src/androidTest/java/org/hisp/dhis/android/core/data/database/IntegrationTestObjects.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2004-2019, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.android.core.data.database;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import com.facebook.stetho.Stetho;
+
+import org.hisp.dhis.android.core.AppContextDIModule;
+import org.hisp.dhis.android.core.D2;
+import org.hisp.dhis.android.core.D2DIComponent;
+import org.hisp.dhis.android.core.DaggerD2DIComponent;
+import org.hisp.dhis.android.core.arch.api.retrofit.APIClientDIModule;
+import org.hisp.dhis.android.core.common.D2Factory;
+import org.hisp.dhis.android.core.common.GenericCallData;
+import org.hisp.dhis.android.core.data.server.Dhis2MockServer;
+import org.hisp.dhis.android.core.resource.ResourceHandler;
+import org.hisp.dhis.android.core.resource.ResourceStoreImpl;
+import org.junit.After;
+
+import java.io.IOException;
+import java.util.Date;
+
+import androidx.test.InstrumentationRegistry;
+
+public class IntegrationTestObjects {
+    public final SQLiteDatabase sqLiteDatabase;
+    public final DatabaseAdapter databaseAdapter;
+
+    public Date serverDate = new Date();
+    public ResourceHandler resourceHandler;
+
+    public final String dbName = null;
+
+    public final D2DIComponent d2DIComponent;
+    public final D2 d2;
+    public final Dhis2MockServer dhis2MockServer;
+
+    public IntegrationTestObjects() throws IOException {
+        DbOpenHelper dbOpenHelper = new DbOpenHelper(InstrumentationRegistry.getTargetContext().getApplicationContext()
+                , dbName);
+        sqLiteDatabase = dbOpenHelper.getWritableDatabase();
+        databaseAdapter = new SqLiteDatabaseAdapter(dbOpenHelper);
+        resourceHandler = new ResourceHandler(ResourceStoreImpl.create(databaseAdapter));
+        resourceHandler.setServerDate(serverDate);
+        Stetho.initializeWithDefaults(InstrumentationRegistry.getTargetContext().getApplicationContext());
+
+        dhis2MockServer = new Dhis2MockServer();
+
+        d2 = D2Factory.create(dhis2MockServer.getBaseEndpoint(), databaseAdapter);
+
+        d2DIComponent = DaggerD2DIComponent.builder()
+                .databaseDIModule(new DatabaseDIModule(databaseAdapter))
+                .apiClientDIModule(new APIClientDIModule(d2.retrofit()))
+                .appContextDIModule(new AppContextDIModule(InstrumentationRegistry.getTargetContext().getApplicationContext()))
+                .build();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        sqLiteDatabase.close();
+        dhis2MockServer.shutdown();
+    }
+
+    protected GenericCallData getGenericCallData(D2 d2) {
+        return GenericCallData.create(
+                databaseAdapter, d2.retrofit(), resourceHandler, d2.systemInfoModule().versionManager);
+    }
+}

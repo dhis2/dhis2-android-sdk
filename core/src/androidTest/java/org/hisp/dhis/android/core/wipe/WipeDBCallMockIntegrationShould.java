b1a47564b68d7a5b0833d1e39c2faebd63d2f167
Luis Delgado Romera <luisyng@gmail.com>
2019-05-28 15:39:17 +0200

    [ANDROSDK-827] Reduce execution time of WipeDBCallMockIntegrationShould

--- core/src/androidTest/java/org/hisp/dhis/android/core/wipe/WipeDBCallMockIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/wipe/WipeDBCallMockIntegrationShould.java
@@ -28,53 +28,11 @@
 
 package org.hisp.dhis.android.core.wipe;
 
-import org.hisp.dhis.android.core.D2;
-import org.hisp.dhis.android.core.common.D2Factory;
-import org.hisp.dhis.android.core.data.database.AbsStoreTestCase;
 import org.hisp.dhis.android.core.data.database.DatabaseAssert;
-import org.hisp.dhis.android.core.data.server.Dhis2MockServer;
-import org.junit.After;
-import org.junit.Before;
+import org.hisp.dhis.android.core.utils.integration.BaseIntegrationTestEmptyDispatcher;
 import org.junit.Test;
 
-import java.io.IOException;
-
-public class WipeDBCallMockIntegrationShould extends AbsStoreTestCase {
-
-    private Dhis2MockServer dhis2MockServer;
-    private D2 d2;
-
-    @Override
-    @Before
-    public void setUp() throws IOException {
-        super.setUp();
-
-        dhis2MockServer = new Dhis2MockServer();
-        dhis2MockServer.setRequestDispatcher();
-
-        d2 = D2Factory.create(dhis2MockServer.getBaseEndpoint(), databaseAdapter());
-    }
-
-    @Override
-    @After
-    public void tearDown() throws IOException {
-        super.tearDown();
-
-        dhis2MockServer.shutdown();
-    }
-
-    @Test
-    public void have_empty_database_when_wipe_db_after_sync_meta_data() throws Exception {
-        givenALoginInDatabase();
-
-        givenAMetadataInDatabase();
-
-        DatabaseAssert.assertThatDatabase(databaseAdapter()).isNotEmpty();
-
-        d2.wipeModule().wipeEverything();
-
-        DatabaseAssert.assertThatDatabase(databaseAdapter()).isEmpty();
-    }
+public class WipeDBCallMockIntegrationShould extends BaseIntegrationTestEmptyDispatcher {
 
     @Test
     public void have_empty_database_when_wipe_db_after_sync_data() throws Exception {
@@ -84,11 +42,11 @@ public class WipeDBCallMockIntegrationShould extends AbsStoreTestCase {
 
         givenAEventInDatabase();
 
-        DatabaseAssert.assertThatDatabase(databaseAdapter()).isNotEmpty();
+        DatabaseAssert.assertThatDatabase(databaseAdapter).isNotEmpty();
 
         d2.wipeModule().wipeEverything();
 
-        DatabaseAssert.assertThatDatabase(databaseAdapter()).isEmpty();
+        DatabaseAssert.assertThatDatabase(databaseAdapter).isEmpty();
     }
 
     private void givenALoginInDatabase() throws Exception {

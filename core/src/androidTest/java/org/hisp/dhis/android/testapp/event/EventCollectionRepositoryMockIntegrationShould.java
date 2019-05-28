5edd39e51b5b650e550170a613da463b766d539f
Marcos Campos <marcamsn@gmail.com>
2019-05-28 12:14:59 +0200

    [ANDROSDK-814] Add more event collection repository tests

--- core/src/androidTest/java/org/hisp/dhis/android/testapp/event/EventCollectionRepositoryMockIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/testapp/event/EventCollectionRepositoryMockIntegrationShould.java
@@ -33,7 +33,9 @@ import org.hisp.dhis.android.core.common.BaseNameableObject;
 import org.hisp.dhis.android.core.common.State;
 import org.hisp.dhis.android.core.data.database.SyncedDatabaseMockIntegrationShould;
 import org.hisp.dhis.android.core.event.Event;
+import org.hisp.dhis.android.core.event.EventCreateProjection;
 import org.hisp.dhis.android.core.event.EventStatus;
+import org.hisp.dhis.android.core.maintenance.D2Error;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -301,5 +303,23 @@ public class EventCollectionRepositoryMockIntegrationShould extends SyncedDataba
         assertThat(events.get(1).uid(), is("single1"));
         assertThat(events.get(2).uid(), is("single2"));
         assertThat(events.get(3).uid(), is("event1"));
+    }
+
+    @Test
+    public void add_events_to_the_repository() throws D2Error {
+        List<Event> events1 = d2.eventModule().events.get();
+        assertThat(events1.size(), is(4));
+
+        String eventUid = d2.eventModule().events.add(
+                EventCreateProjection.create("enroll1", "lxAQ7Zs9VYR", "dBwrot7S420",
+                        "DiszpKrYNg8", "bRowv6yZOF2"));
+
+        List<Event> events2 = d2.eventModule().events.get();
+        assertThat(events2.size(), is(5));
+
+        Event event = d2.eventModule().events.uid(eventUid).get();
+        assertThat(event.uid(), is(eventUid));
+
+        d2.eventModule().events.uid(eventUid).delete();
     }
 }

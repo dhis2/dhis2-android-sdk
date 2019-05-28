187238bbe3f4c3c7e84660af20af88bb2c9ce5fd
Luis Delgado Romera <luisyng@gmail.com>
2019-05-28 17:17:31 +0200

    [ANDROSDK-827] Reduce execution time of ReadOnlyIdentifiableCollectionRepositoryImplIntegrationShould

--- core/src/androidTest/java/org/hisp/dhis/android/core/arch/repositories/collection/ReadOnlyIdentifiableCollectionRepositoryImplIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/arch/repositories/collection/ReadOnlyIdentifiableCollectionRepositoryImplIntegrationShould.java
@@ -28,19 +28,14 @@
 
 package org.hisp.dhis.android.core.arch.repositories.collection;
 
-import org.hisp.dhis.android.core.D2;
 import org.hisp.dhis.android.core.arch.handlers.SyncHandler;
 import org.hisp.dhis.android.core.arch.repositories.object.ReadOnlyOneObjectRepositoryFinalImpl;
-import org.hisp.dhis.android.core.common.D2Factory;
-import org.hisp.dhis.android.core.data.database.AbsStoreTestCase;
-import org.hisp.dhis.android.core.data.server.RealServerMother;
 import org.hisp.dhis.android.core.relationship.RelationshipType;
 import org.hisp.dhis.android.core.relationship.RelationshipTypeCollectionRepository;
-import org.junit.Before;
+import org.hisp.dhis.android.core.utils.integration.BaseIntegrationTestEmptyDispatcher;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.io.IOException;
 
 import androidx.test.runner.AndroidJUnit4;
 
@@ -52,23 +47,20 @@ import static org.hisp.dhis.android.core.data.relationship.RelationshipTypeSampl
 import static org.hisp.dhis.android.core.data.relationship.RelationshipTypeSamples.RELATIONSHIP_TYPE_UID_2;
 
 @RunWith(AndroidJUnit4.class)
-public class ReadOnlyIdentifiableCollectionRepositoryImplIntegrationShould extends AbsStoreTestCase {
+public class ReadOnlyIdentifiableCollectionRepositoryImplIntegrationShould extends BaseIntegrationTestEmptyDispatcher {
 
-    private RelationshipTypeCollectionRepository relationshipTypeCollectionRepository;
+    private static RelationshipTypeCollectionRepository relationshipTypeCollectionRepository;
 
-    @Override
-    @Before
-    public void setUp() throws IOException {
-        super.setUp();
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        BaseIntegrationTestEmptyDispatcher.setUpClass();
 
-        D2 d2 = D2Factory.create(RealServerMother.url, databaseAdapter());
-
-        SyncHandler<RelationshipType> handler = getD2DIComponent(d2).relationshipTypeHandler();
+        SyncHandler<RelationshipType> handler = objects.d2DIComponent.relationshipTypeHandler();
 
         handler.handle(RELATIONSHIP_TYPE_1);
         handler.handle(RELATIONSHIP_TYPE_2);
 
-        this.relationshipTypeCollectionRepository = d2.relationshipModule().relationshipTypes;
+        relationshipTypeCollectionRepository = d2.relationshipModule().relationshipTypes;
     }
 
     @Test

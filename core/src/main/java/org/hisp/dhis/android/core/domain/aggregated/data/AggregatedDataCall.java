4bbe15b2101f9c731b3f295b8e91e6a0787d7e9a
Abel Castro <abelcsz@gmail.com>
2019-06-14 11:56:55 +0200

    [ANDROSDK-28] Add DataApproval downloads

--- core/src/main/java/org/hisp/dhis/android/core/domain/aggregated/data/AggregatedDataCall.java
+++ core/src/main/java/org/hisp/dhis/android/core/domain/aggregated/data/AggregatedDataCall.java
@@ -27,32 +27,42 @@
  */
 package org.hisp.dhis.android.core.domain.aggregated.data;
 
+import androidx.annotation.NonNull;
+
 import org.hisp.dhis.android.core.arch.api.executors.internal.RxAPICallExecutor;
 import org.hisp.dhis.android.core.arch.call.D2Progress;
 import org.hisp.dhis.android.core.arch.call.factories.internal.QueryCallFactory;
 import org.hisp.dhis.android.core.arch.call.internal.D2ProgressManager;
 import org.hisp.dhis.android.core.arch.db.stores.internal.IdentifiableObjectStore;
 import org.hisp.dhis.android.core.arch.repositories.collection.ReadOnlyWithDownloadObjectRepository;
+import org.hisp.dhis.android.core.category.CategoryOptionCombo;
+import org.hisp.dhis.android.core.category.internal.CategoryOptionComboStore;
+import org.hisp.dhis.android.core.dataapproval.DataApproval;
+import org.hisp.dhis.android.core.dataapproval.internal.DataApprovalQuery;
 import org.hisp.dhis.android.core.dataset.DataSet;
 import org.hisp.dhis.android.core.dataset.DataSetCompleteRegistration;
 import org.hisp.dhis.android.core.dataset.internal.DataSetCompleteRegistrationQuery;
+import org.hisp.dhis.android.core.dataset.internal.DataSetFields;
 import org.hisp.dhis.android.core.datavalue.DataValue;
 import org.hisp.dhis.android.core.datavalue.internal.DataValueQuery;
 import org.hisp.dhis.android.core.period.Period;
 import org.hisp.dhis.android.core.period.internal.PeriodStore;
 import org.hisp.dhis.android.core.systeminfo.SystemInfo;
+import org.hisp.dhis.android.core.user.UserOrganisationUnitLink;
 import org.hisp.dhis.android.core.user.internal.UserOrganisationUnitLinkStore;
+import org.hisp.dhis.android.core.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
-import androidx.annotation.NonNull;
 import io.reactivex.Observable;
 import io.reactivex.Single;
 
@@ -63,9 +73,11 @@ final class AggregatedDataCall {
     private final QueryCallFactory<DataValue, DataValueQuery> dataValueCallFactory;
     private final QueryCallFactory<DataSetCompleteRegistration,
             DataSetCompleteRegistrationQuery> dataSetCompleteRegistrationCallFactory;
+    private final QueryCallFactory<DataApproval, DataApprovalQuery> dataApprovalCallFactory;
     private final IdentifiableObjectStore<DataSet> dataSetStore;
     private final PeriodStore periodStore;
     private final UserOrganisationUnitLinkStore organisationUnitStore;
+    private final CategoryOptionComboStore categoryOptionComboStore;
     private final RxAPICallExecutor rxCallExecutor;
 
     @Inject
@@ -73,21 +85,25 @@ final class AggregatedDataCall {
                        @NonNull QueryCallFactory<DataValue, DataValueQuery> dataValueCallFactory,
                        @NonNull QueryCallFactory<DataSetCompleteRegistration, DataSetCompleteRegistrationQuery>
                                dataSetCompleteRegistrationCallFactory,
+                       @NonNull QueryCallFactory<DataApproval, DataApprovalQuery> dataApprovalCallFactory,
                        @NonNull IdentifiableObjectStore<DataSet> dataSetStore,
                        @NonNull PeriodStore periodStore,
                        @NonNull UserOrganisationUnitLinkStore organisationUnitStore,
+                       @NonNull CategoryOptionComboStore categoryOptionComboStore,
                        @NonNull RxAPICallExecutor rxCallExecutor) {
         this.systemInfoRepository = systemInfoRepository;
         this.dataValueCallFactory = dataValueCallFactory;
         this.dataSetCompleteRegistrationCallFactory = dataSetCompleteRegistrationCallFactory;
+        this.dataApprovalCallFactory = dataApprovalCallFactory;
         this.dataSetStore = dataSetStore;
         this.periodStore = periodStore;
         this.organisationUnitStore = organisationUnitStore;
+        this.categoryOptionComboStore = categoryOptionComboStore;
         this.rxCallExecutor = rxCallExecutor;
     }
 
     Observable<D2Progress> download() {
-        D2ProgressManager progressManager = new D2ProgressManager(3);
+        D2ProgressManager progressManager = new D2ProgressManager(4);
 
         Observable<D2Progress> observable = systemInfoRepository.download()
                 .toSingle(() -> progressManager.increaseProgressAndCompleteWithCount(SystemInfo.class))
@@ -113,11 +129,33 @@ final class AggregatedDataCall {
                 dataSetCompleteRegistrationCallFactory.create(dataSetCompleteRegistrationQuery)).map(dataValues ->
                         progressManager.increaseProgressAndCompleteWithCount(DataSetCompleteRegistration.class));
 
+
+        List<DataSet> dataSetsWithWorkflow =
+                dataSetStore.selectWhere(DataSetFields.WORKFLOW + " IS NOT NULL");
+
+        List<UserOrganisationUnitLink> userOrganisationUnitLinks = organisationUnitStore.selectAll();
+
+        Set<String> workflowUids = getWorkflowsUidsFrom(dataSetsWithWorkflow);
+        Set<String> attributeOptionComboUids = getAttributeOptionCombosUidsFrom(dataSetsWithWorkflow);
+        Set<String> organisationUnitsUids = getOrganisationUnitsUidsFrom(userOrganisationUnitLinks);
+
+        Date startDate = periodStore.getOldestPeriodStartDate();
+        Date endDate = new Date();
+
+        DataApprovalQuery dataApprovalQuery = DataApprovalQuery.create(workflowUids,
+                organisationUnitsUids, startDate, endDate, attributeOptionComboUids);
+
+        Single<D2Progress> dataApprovalSingle = Single.fromCallable(
+                dataApprovalCallFactory.create(dataApprovalQuery)).map(dataApprovals ->
+                progressManager.increaseProgressAndCompleteWithCount(DataApproval.class));
+
+
         @SuppressWarnings("PMD.NonStaticInitializer")
         ArrayList<Single<D2Progress>> list = new ArrayList<Single<D2Progress>>() {{
             add(Single.just(systemInfoProgress));
             add(dataValueSingle);
             add(dataSetCompleteRegistrationSingle);
+            add(dataApprovalSingle);
         }};
 
         return Single.merge(list).toObservable();
@@ -130,5 +168,36 @@ final class AggregatedDataCall {
             periodIds.add(period.periodId());
         }
         return periodIds;
+    }
+
+    private Set<String> getWorkflowsUidsFrom(Collection<DataSet> dataSetsWithWorkflow) {
+
+        return dataSetsWithWorkflow.stream()
+                .map(dataSet -> dataSet.workflow().uid())
+                .collect(Collectors.toSet());
+    }
+
+    private Set<String> getAttributeOptionCombosUidsFrom(Collection<DataSet> dataSetsWithWorkflow) {
+
+        Set<String> dataSetsWithWorkflowCategoryCombos = dataSetsWithWorkflow.stream()
+                .map(dataSet -> dataSet.categoryCombo().uid())
+                .collect(Collectors.toSet());
+
+        List<CategoryOptionCombo> categoryOptionCombos =
+                categoryOptionComboStore.selectWhere("categoryCombo IN ("
+                        + Utils.commaAndSpaceSeparatedArrayValues(
+                        Utils.withSingleQuotationMarksArray(dataSetsWithWorkflowCategoryCombos))
+                        + ")");
+
+        return categoryOptionCombos.stream()
+                .map(categoryOptionCombo -> categoryOptionCombo.uid())
+                .collect(Collectors.toSet());
+    }
+
+    private Set<String> getOrganisationUnitsUidsFrom(Collection<UserOrganisationUnitLink> userOrganisationUnitLinks) {
+
+        return userOrganisationUnitLinks.stream()
+                .map(userOrganisationUnitLink -> userOrganisationUnitLink.organisationUnit())
+                .collect(Collectors.toSet());
     }
 }

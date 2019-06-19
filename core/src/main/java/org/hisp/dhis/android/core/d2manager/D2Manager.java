44136ba1282ecb01ebf5a35d80d9d05033a66427
Luis Delgado Romera <luisyng@gmail.com>
2019-06-19 09:59:10 +0200

    [ANDROSDK-868] Add RxJava to D2Manager

--- core/src/main/java/org/hisp/dhis/android/core/d2manager/D2Manager.java
+++ core/src/main/java/org/hisp/dhis/android/core/d2manager/D2Manager.java
@@ -38,6 +38,8 @@ import org.hisp.dhis.android.core.data.database.DbOpenHelper;
 import org.hisp.dhis.android.core.data.database.SqLiteDatabaseAdapter;
 
 import androidx.annotation.VisibleForTesting;
+import io.reactivex.Completable;
+import io.reactivex.Single;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.annotations.Nullable;
 import okhttp3.HttpUrl;
@@ -50,60 +52,69 @@ public final class D2Manager {
     private static D2Configuration d2Configuration;
     static DatabaseAdapter databaseAdapter;
     private static ConfigurationManager configurationManager;
+    private static Configuration configuration;
 
     private D2Manager() {
     }
 
-    public static void setD2Configuration(@Nullable D2Configuration d2Config) {
-        if (d2Configuration != null) {
-            throw new IllegalArgumentException("D2 Configuration already set");
-        }
-        d2Configuration = d2Config;
-        databaseAdapter = newDatabaseAdapter();
-        configurationManager = ConfigurationManagerFactory.create(databaseAdapter);
-    }
-
-    @VisibleForTesting
-    static void setDatabaseName(String dbName) {
-        databaseName = dbName;
+    public static Completable setUp(@Nullable D2Configuration d2Config) {
+        return Completable.fromAction(() -> {
+            d2Configuration = d2Config;
+            databaseAdapter = newDatabaseAdapter();
+            configurationManager = ConfigurationManagerFactory.create(databaseAdapter);
+            configuration = configurationManager.get();
+        });
     }
 
     public static boolean isServerUrlSet() {
-        return configurationManager != null && configurationManager.get() != null;
+        return configuration != null;
     }
 
-    public static void setServerUrl(@NonNull String url) {
-        HttpUrl httpUrl = ServerUrlParser.parse(url);
-        Configuration configuration = Configuration.forServerUrl(httpUrl);
-        configurationManager.configure(configuration);
-        instantiateD2(configuration);
+    public static Completable setServerUrl(@NonNull String url) {
+        return Completable.fromAction(() -> {
+            ensureSetUp();
+            HttpUrl httpUrl = ServerUrlParser.parse(url);
+            configuration = Configuration.forServerUrl(httpUrl);
+            configurationManager.configure(configuration);
+        });
     }
 
     public static D2 getD2() throws IllegalStateException {
         if (d2 == null) {
-            if (d2Configuration == null) {
-                throw new IllegalArgumentException("D2 Configuration is not yet set");
-            } else {
-                Configuration configuration = configurationManager.get();
-                if (configuration == null) {
-                    throw new IllegalStateException("Server URL is not configured");
-                } else {
-                    instantiateD2(configuration);
-                    return d2;
-                }
-            }
+            throw new IllegalStateException("D2 is not instantiated yet");
         } else {
             return d2;
         }
     }
 
-    private static void instantiateD2(Configuration configuration) {
-        d2 = new D2.Builder()
-                .configuration(configuration)
-                .databaseAdapter(databaseAdapter)
-                .okHttpClient(OkHttpClientFactory.okHttpClient(d2Configuration, databaseAdapter))
-                .context(d2Configuration.context())
-                .build();
+    private static void ensureSetUp() {
+        if (d2Configuration == null) {
+            throw new IllegalStateException("You have to setUp first");
+        }
+    }
+
+    private static void ensureServerUrl() {
+        if (configuration == null) {
+            throw new IllegalStateException("You have to configure server URL first");
+        }
+    }
+
+    private static boolean isD2Instantiated() {
+        return d2 != null;
+    }
+
+    public static Single<D2> instantiateD2() {
+        return Single.fromCallable(() -> {
+            ensureSetUp();
+            ensureServerUrl();
+            d2 = new D2.Builder()
+                    .configuration(configuration)
+                    .databaseAdapter(databaseAdapter)
+                    .okHttpClient(OkHttpClientFactory.okHttpClient(d2Configuration, databaseAdapter))
+                    .context(d2Configuration.context())
+                    .build();
+            return d2;
+        });
     }
 
     private static DatabaseAdapter newDatabaseAdapter() {
@@ -112,10 +123,16 @@ public final class D2Manager {
     }
 
     @VisibleForTesting
-    static void clear()  {
+    static void setDatabaseName(String dbName) {
+        databaseName = dbName;
+    }
+
+    @VisibleForTesting
+    static void clear() {
         d2Configuration = null;
         d2 = null;
         databaseAdapter =  null;
         configurationManager = null;
+        configuration = null;
     }
 }

daa79a2e70813dae2c285461e1463051c582ee07
Marcos Campos <marcamsn@gmail.com>
2019-07-11 11:38:11 +0200

    Add TEIHelper

---
+++ core/src/main/java/org/hisp/dhis/android/core/arch/helpers/internal/TrackedEntityInstanceHelper.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2004-2019, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.android.core.arch.helpers.internal;
+
+import org.hisp.dhis.android.core.enrollment.Enrollment;
+import org.hisp.dhis.android.core.programorganisationunit.internal.ProgramOrganisationUnitLastUpdated;
+import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstance;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public final class TrackedEntityInstanceHelper {
+
+    private TrackedEntityInstanceHelper() {}
+
+    public static Set<ProgramOrganisationUnitLastUpdated> getProgramOrganisationUnitTuple(
+            List<TrackedEntityInstance> trackedEntityInstances, Date lastSynced) {
+        Set<ProgramOrganisationUnitLastUpdated> programOrganisationUnitSet = new HashSet<>();
+        for (TrackedEntityInstance trackedEntityInstance : trackedEntityInstances) {
+            if (trackedEntityInstance.enrollments() != null) {
+                for (Enrollment enrollment : trackedEntityInstance.enrollments()) {
+                    if (enrollment.organisationUnit() != null && enrollment.program() != null) {
+                        programOrganisationUnitSet.add(
+                                ProgramOrganisationUnitLastUpdated.builder()
+                                        .organisationUnit(enrollment.organisationUnit())
+                                        .program(enrollment.program())
+                                        .lastSynced(lastSynced)
+                                        .build()
+                        );
+                    }
+                }
+            }
+        }
+
+        return programOrganisationUnitSet;
+    }
+}

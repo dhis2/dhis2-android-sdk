ce7e976bf696b6971958834d8c55ed5e8ed0fb38
Luis Delgado Romera <luisyng@gmail.com>
2019-07-11 10:00:41 +0200

    [ANDROSDK-890] Fix return type javadoc

--- core/src/main/java/org/hisp/dhis/android/core/arch/repositories/collection/ReadOnlyCollectionRepository.java
+++ core/src/main/java/org/hisp/dhis/android/core/arch/repositories/collection/ReadOnlyCollectionRepository.java
@@ -75,7 +75,7 @@ public interface ReadOnlyCollectionRepository<M extends Model> {
      * Check if selection of objects with applied filters is empty.
      * Important: this is a blocking method and it should be executed in a separated thread.
      *
-     * @return Element count
+     * @return If selection is empty
      */
     boolean isEmpty();
 

f207d4b8ce1d0b440ef96096630f4e5137309332
Pawel Szymanski <pawel.szymanski@ehealthafrica.org>
2019-07-14 06:58:00 +0200

    Updated metadata ids store

--- core/src/main/java/org/hisp/dhis/android/core/sms/data/localdbrepository/MetadataIdsStore.java
+++ core/src/main/java/org/hisp/dhis/android/core/sms/data/localdbrepository/MetadataIdsStore.java
@@ -2,8 +2,20 @@ package org.hisp.dhis.android.core.sms.data.localdbrepository;
 
 import android.content.Context;
 
-import org.hisp.dhis.android.core.ObjectMapperFactory;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
 import org.hisp.dhis.smscompression.models.SMSMetadata;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Date;
 
 import io.reactivex.Completable;
 import io.reactivex.Single;
@@ -17,16 +29,49 @@ class MetadataIdsStore {
     }
 
     Single<SMSMetadata> getMetadataIds() {
-        return Single.fromCallable(() ->
-                ObjectMapperFactory.objectMapper().readValue(
-                        context.openFileInput(METADATA_FILE), SMSMetadata.class
-                ));
+        return Single.fromCallable(() -> {
+                    InputStream is = null;
+                    try {
+                        is = context.openFileInput(METADATA_FILE);
+                        JsonReader reader = new JsonReader(new InputStreamReader(is));
+                        return getGson().fromJson(reader, SMSMetadata.class);
+                    } finally {
+                        if (is != null) {
+                            is.close();
+                        }
+                    }
+                }
+        );
     }
 
     Completable setMetadataIds(final SMSMetadata metadata) {
-        return Completable.fromAction(() ->
-                ObjectMapperFactory.objectMapper().writeValue(
-                        context.openFileOutput(METADATA_FILE, Context.MODE_PRIVATE), metadata
-                ));
+        return Completable.fromAction(() -> {
+                    OutputStream fos = context.openFileOutput(METADATA_FILE, Context.MODE_PRIVATE);
+                    JsonWriter writer = new JsonWriter(new OutputStreamWriter(fos));
+                    getGson().toJson(metadata, SMSMetadata.class, writer);
+                    writer.flush();
+                    writer.close();
+                }
+        );
+    }
+
+    private Gson getGson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(Date.class, new DateLongFormatTypeAdapter())
+                .create();
+    }
+
+    class DateLongFormatTypeAdapter extends TypeAdapter<Date> {
+
+        @Override
+        public void write(JsonWriter out, Date value) throws IOException {
+            if (value != null) out.value(value.getTime());
+            else out.nullValue();
+        }
+
+        @Override
+        public Date read(JsonReader in) throws IOException {
+            return new Date(in.nextLong());
+        }
     }
 }

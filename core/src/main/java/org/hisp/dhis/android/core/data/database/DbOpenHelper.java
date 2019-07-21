532cb65f9264848cc654f90d4ff0253a8b8dd7da
Victor Garcia <vgarciabnz@gmail.com>
2019-07-21 13:18:46 +0200

    [ANDROSDK-898] Fix index in DataSetOrganisationUnit and UserOrganisationUnit

--- core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
+++ core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
@@ -44,7 +44,7 @@ import androidx.annotation.Nullable;
 
 public class DbOpenHelper extends SQLBriteOpenHelper {
 
-    public static final int VERSION = 50;
+    public static final int VERSION = 51;
 
     public DbOpenHelper(@NonNull Context context, @Nullable String databaseName) {
         super(context, databaseName, null, VERSION);

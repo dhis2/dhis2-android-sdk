b4b69757c2c1515f7c1e23a3e26b3824a9113650
Marcos Campos <marcamsn@gmail.com>
2019-08-06 18:25:42 +0200

    [androsdk-33] Add FileResource

---
+++ core/src/main/java/org/hisp/dhis/android/core/fileresource/FileResource.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2004-2019, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.android.core.fileresource;
+
+import android.database.Cursor;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.gabrielittner.auto.value.cursor.ColumnAdapter;
+import com.google.auto.value.AutoValue;
+
+import org.hisp.dhis.android.core.common.BaseDataModel;
+import org.hisp.dhis.android.core.common.BaseIdentifiableObject;
+import org.hisp.dhis.android.core.data.database.DbDateColumnAdapter;
+
+import java.util.Date;
+
+@AutoValue
+@JsonDeserialize(builder = $$AutoValue_FileResource.Builder.class)
+public abstract class FileResource extends BaseDataModel {
+
+    @Nullable
+    @JsonProperty(BaseIdentifiableObject.UID)
+    public abstract String uid();
+
+    @Nullable
+    @JsonProperty
+    public abstract String name();
+
+    @Nullable
+    @JsonProperty
+    public abstract String displayName();
+
+    @Nullable
+    @JsonProperty()
+    @ColumnAdapter(DbDateColumnAdapter.class)
+    public abstract Date created();
+
+    @Nullable
+    @JsonProperty()
+    @ColumnAdapter(DbDateColumnAdapter.class)
+    public abstract Date lastUpdated();
+
+    @Nullable
+    @JsonProperty
+    public abstract String contentType();
+
+    @Nullable
+    @JsonProperty
+    public abstract Integer contentLength();
+
+    @NonNull
+    public static FileResource create(Cursor cursor) {
+        return AutoValue_FileResource.createFromCursor(cursor);
+    }
+
+    public abstract Builder toBuilder();
+
+    public static Builder builder() {
+        return new $$AutoValue_FileResource.Builder();
+    }
+
+    @AutoValue.Builder
+    @JsonPOJOBuilder(withPrefix = "")
+    public abstract static class Builder extends BaseDataModel.Builder<Builder> {
+
+        @JsonProperty(BaseIdentifiableObject.UID)
+        public abstract Builder uid(@NonNull String uid);
+
+        public abstract Builder name(@NonNull String name);
+
+        public abstract Builder displayName(@NonNull String displayName);
+
+        public abstract Builder created(@NonNull Date created);
+
+        public abstract Builder lastUpdated(@NonNull Date lastUpdated);
+
+        public abstract Builder contentType(@NonNull String contentType);
+
+        public abstract Builder contentLength(@NonNull Integer contentLength);
+
+        public abstract FileResource build();
+    }
+}

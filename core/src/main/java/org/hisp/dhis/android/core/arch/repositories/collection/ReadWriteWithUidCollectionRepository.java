b4e06340e8d4be1287327ab16b638ecc2b3a6b6d
Marcos Campos <marcamsn@gmail.com>
2019-08-09 12:31:41 +0200

    [androsdk-33] Adapt RWCollectionRepository implementations to create fileResource repositories

--- core/src/main/java/org/hisp/dhis/android/core/arch/repositories/collection/ReadWriteWithUidCollectionRepository.java
+++ core/src/main/java/org/hisp/dhis/android/core/arch/repositories/collection/ReadWriteWithUidCollectionRepository.java
@@ -27,7 +27,7 @@
  */
 package org.hisp.dhis.android.core.arch.repositories.collection;
 
-import org.hisp.dhis.android.core.arch.repositories.object.ReadWriteObjectRepository;
+import org.hisp.dhis.android.core.arch.repositories.object.ReadOnlyObjectRepository;
 import org.hisp.dhis.android.core.common.Model;
 import org.hisp.dhis.android.core.common.ObjectWithUidInterface;
 import org.hisp.dhis.android.core.maintenance.D2Error;
@@ -37,7 +37,7 @@ import io.reactivex.Single;
 public interface ReadWriteWithUidCollectionRepository<M extends Model & ObjectWithUidInterface, C>
         extends ReadOnlyCollectionRepository<M> {
 
-    ReadWriteObjectRepository<M> uid(String uid);
+    ReadOnlyObjectRepository<M> uid(String uid);
     Single<String> add(C c);
     String blockingAdd(C c) throws D2Error;
 }

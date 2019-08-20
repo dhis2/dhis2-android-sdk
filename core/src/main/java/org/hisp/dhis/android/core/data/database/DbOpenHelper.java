76ffd846a798627f724fd8c66db15901e469917b
Abel Castro <abelcsz@gmail.com>
2019-08-20 03:08:26 +0200

    [ANDROSDK-839] Add TrackedEntityAttribute foreign key to TrackedEntityAttributeValue table

--- core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
+++ core/src/main/java/org/hisp/dhis/android/core/data/database/DbOpenHelper.java
@@ -44,7 +44,7 @@ import java.util.Map;
 
 public class DbOpenHelper extends SQLBriteOpenHelper {
 
-    public static final int VERSION = 56;
+    public static final int VERSION = 57;
 
     public DbOpenHelper(@NonNull Context context, @Nullable String databaseName) {
         super(context, databaseName, null, VERSION);

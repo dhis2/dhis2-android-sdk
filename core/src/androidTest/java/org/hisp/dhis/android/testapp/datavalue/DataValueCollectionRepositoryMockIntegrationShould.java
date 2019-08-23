596eb19a7a4271633d58fa7acf7185a9c9886de3
Abel Castro <abelcsz@gmail.com>
2019-08-23 10:34:02 +0200

    [ANDROSDK-836] Update DataValueCollectionRepositoryMockIntegrationShould

--- core/src/androidTest/java/org/hisp/dhis/android/testapp/datavalue/DataValueCollectionRepositoryMockIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/testapp/datavalue/DataValueCollectionRepositoryMockIntegrationShould.java
@@ -134,10 +134,13 @@ public class DataValueCollectionRepositoryMockIntegrationShould extends BaseMock
 
     @Test
     public void filter_by_created() throws ParseException {
+
+        List <DataValue> d = d2.dataValueModule().dataValues.blockingGet();
+
         List<DataValue> dataValues =
                 d2.dataValueModule().dataValues
                         .byCreated()
-                        .eq(BaseIdentifiableObject.DATE_FORMAT.parse("2010-01-11T00:00:00.000+0000"))
+                        .eq(BaseIdentifiableObject.DATE_FORMAT.parse("2010-02-11T00:00:00.000+0100"))
                         .blockingGet();
 
         assertThat(dataValues.size(), is(1));

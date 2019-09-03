e99114778a9bb34acdf46a92c280d3e11458ad09
Marcos Campos <marcamsn@gmail.com>
2019-09-03 17:55:43 +0200

    [androsdk-33] Add Tuple

---
+++ core/src/main/java/org/hisp/dhis/android/core/arch/helpers/internal/Tuple.java
@@ -0,0 +1,10 @@
+package org.hisp.dhis.android.core.arch.helpers.internal;
+
+public class Tuple<X, Y> {
+    public final X x;
+    public final Y y;
+    public Tuple(X x, Y y) {
+        this.x = x;
+        this.y = y;
+    }
+}

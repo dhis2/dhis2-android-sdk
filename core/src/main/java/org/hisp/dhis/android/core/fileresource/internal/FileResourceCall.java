0d4362682e6dffadc8d9888c81e1e9456fb1b98b
Marcos Campos <marcamsn@gmail.com>
2019-09-03 17:55:59 +0200

    [androsdk-33] Add file resource call and module downloader

---
+++ core/src/main/java/org/hisp/dhis/android/core/fileresource/internal/FileResourceCall.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2004-2019, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.android.core.fileresource.internal;
+
+import androidx.annotation.NonNull;
+
+import org.hisp.dhis.android.core.arch.api.executors.internal.RxAPICallExecutor;
+import org.hisp.dhis.android.core.arch.call.D2Progress;
+import org.hisp.dhis.android.core.arch.call.internal.D2ProgressManager;
+import org.hisp.dhis.android.core.fileresource.FileResource;
+import org.hisp.dhis.android.core.systeminfo.SystemInfo;
+import org.hisp.dhis.android.core.systeminfo.internal.SystemInfoModuleDownloader;
+
+import javax.inject.Inject;
+
+import dagger.Reusable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
+@Reusable
+public class FileResourceCall {
+
+    private final RxAPICallExecutor rxCallExecutor;
+
+    private final SystemInfoModuleDownloader systemInfoDownloader;
+    private final FileResourceModuleDownloader fileResourceModuleDownloader;
+
+    @Inject
+    public FileResourceCall(@NonNull RxAPICallExecutor rxCallExecutor,
+                            @NonNull SystemInfoModuleDownloader systemInfoDownloader,
+                            @NonNull FileResourceModuleDownloader fileResourceModuleDownloader) {
+        this.rxCallExecutor = rxCallExecutor;
+        this.systemInfoDownloader = systemInfoDownloader;
+        this.fileResourceModuleDownloader = fileResourceModuleDownloader;
+    }
+
+    public Observable<D2Progress> download() {
+        D2ProgressManager progressManager = new D2ProgressManager(1);
+
+        Single<D2Progress> systemInfoDownload = systemInfoDownloader.downloadMetadata().toSingle(() ->
+                progressManager.increaseProgress(SystemInfo.class, false));
+
+        return rxCallExecutor.wrapObservableTransactionally(
+                systemInfoDownload.flatMapObservable(systemInfoProgress -> Observable.create(emitter -> {
+
+                    fileResourceModuleDownloader.downloadMetadata().call();
+                    emitter.onNext(progressManager.increaseProgress(FileResource.class, false));
+
+                    emitter.onComplete();
+
+                })), true);
+    }
+
+    public void blockingDownload() {
+        download().blockingSubscribe();
+    }
+}

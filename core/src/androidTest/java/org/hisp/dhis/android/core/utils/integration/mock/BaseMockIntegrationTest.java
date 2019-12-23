d056f11ce30de64624f77de4a4532a283a203624
Luis Delgado Romera <luisyng@gmail.com>
2019-12-23 13:48:34 +0100

    [ANDROSDK-1056] Remove database from BaseMockIntegrationTest

--- core/src/androidTest/java/org/hisp/dhis/android/core/utils/integration/mock/BaseMockIntegrationTest.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/utils/integration/mock/BaseMockIntegrationTest.java
@@ -28,8 +28,6 @@
 
 package org.hisp.dhis.android.core.utils.integration.mock;
 
-import android.database.sqlite.SQLiteDatabase;
-
 import org.hisp.dhis.android.core.D2;
 import org.hisp.dhis.android.core.arch.api.internal.ServerURLWrapper;
 import org.hisp.dhis.android.core.arch.db.access.DatabaseAdapter;
@@ -41,13 +39,11 @@ public abstract class BaseMockIntegrationTest {
     protected static D2 d2;
     protected static Dhis2MockServer dhis2MockServer;
     protected static DatabaseAdapter databaseAdapter;
-    protected static SQLiteDatabase database;
 
     static boolean setUpClass(MockIntegrationTestDatabaseContent content) throws Exception {
         MockIntegrationTestObjectsFactory.IntegrationTestObjectsWithIsNewInstance tuple = MockIntegrationTestObjectsFactory.getObjects(content);
         objects = tuple.objects;
         d2 = objects.d2;
-        database = objects.database;
         databaseAdapter = objects.databaseAdapter;
         dhis2MockServer = objects.dhis2MockServer;
         ServerURLWrapper.setServerUrl(dhis2MockServer.getBaseEndpoint());

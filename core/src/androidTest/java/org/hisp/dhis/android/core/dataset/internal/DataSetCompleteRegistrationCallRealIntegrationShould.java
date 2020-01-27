22afea432fe813ebf3c3f4e57e108d70743b0135
Victor Garcia <vgarciabnz@gmail.com>
2020-01-27 16:33:25 +0100

    [ANDROSDK-1067] Add real integration test

---
+++ core/src/androidTest/java/org/hisp/dhis/android/core/dataset/internal/DataSetCompleteRegistrationCallRealIntegrationShould.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2004-2019, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.android.core.dataset.internal;
+
+import org.hisp.dhis.android.core.D2;
+import org.hisp.dhis.android.core.D2Factory;
+import org.hisp.dhis.android.core.utils.integration.real.BaseRealIntegrationTest;
+import org.junit.Before;
+
+import java.io.IOException;
+
+public class DataSetCompleteRegistrationCallRealIntegrationShould extends BaseRealIntegrationTest {
+
+    private D2 d2;
+
+    @Before
+    public void setUp() throws IOException {
+        super.setUp();
+
+        d2 = D2Factory.forDatabaseName("complete");
+    }
+
+    // commented out since it is a flaky test that works against a real server.
+    //@Test
+    public void remove_records_deleted_in_the_server() {
+        d2.userModule().logIn(username, password, url).blockingGet();
+        d2.metadataModule().blockingDownload();
+        d2.aggregatedModule().data().download().blockingSubscribe();
+
+        // At this point, delete a record in the server
+
+        d2.aggregatedModule().data().download().blockingSubscribe();
+    }
+}

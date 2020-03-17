b922c86a76d1f7efaab4312c3b111b29ba9ba58b
Victor Garcia <vgarciabnz@gmail.com>
2020-03-17 11:34:31 +0100

    [ANDROSDK-1104] Uncomment test

--- core/src/androidTest/java/org/hisp/dhis/android/core/period/PeriodParserMockIntegrationShould.java
+++ core/src/androidTest/java/org/hisp/dhis/android/core/period/PeriodParserMockIntegrationShould.java
@@ -33,6 +33,7 @@ import com.google.common.collect.Lists;
 import org.hisp.dhis.android.core.utils.integration.mock.BaseMockIntegrationTestEmptyDispatcher;
 import org.hisp.dhis.android.core.utils.runner.D2JunitRunner;
 import org.junit.AfterClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.List;
@@ -53,7 +54,7 @@ public class PeriodParserMockIntegrationShould extends BaseMockIntegrationTestEm
         d2.databaseAdapter().delete(PeriodTableInfo.TABLE_INFO.name());
     }
 
-    // @Test
+    @Test
     public void get_period_passing_period_id() {
         for (String periodId : PERIOD_ID_LIST) {
             Period period = d2.periodModule().periodHelper().blockingGetPeriodForPeriodId(periodId);

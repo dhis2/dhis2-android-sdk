db7d18c5da715887eb7ff4ad2cf8adbdfdea3576
luis <luisyng@gmail.com>
2020-05-11 08:52:12 +0200

    [KOTLIN] Migrate ObjectWithUidInterface

--- core/src/main/java/org/hisp/dhis/android/core/common/ObjectWithUidInterface.kt
+++ core/src/main/java/org/hisp/dhis/android/core/common/ObjectWithUidInterface.kt
@@ -25,9 +25,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.hisp.dhis.android.core.common
 
-package org.hisp.dhis.android.core.common;
-
-public interface ObjectWithUidInterface {
-    String uid();
+interface ObjectWithUidInterface {
+    fun uid(): String
 }

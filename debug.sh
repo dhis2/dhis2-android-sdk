91429c1f6f524e9daed0af3311e4b82d919c3179
Marcos Campos <marcamsn@gmail.com>
2020-05-14 17:58:24 +0200

    [androsdk-1127b] Fix validation rule expression methods on collection repository

---
+++ debug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -xe
+
+# You can run it from any directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR=$DIR/
+
+# This will: compile the project, run lint, run tests under JVM, package apk, check the code quality and run tests on the device/emulator.
+"$PROJECT_DIR"/gradlew clean checkstyleDebug findbugsDebug pmdDebug lintDebug --scan

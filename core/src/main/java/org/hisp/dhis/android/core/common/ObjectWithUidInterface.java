d99023a6fc6ecb7fd24d43fac26637971fc051f4
luis <luisyng@gmail.com>
2020-07-29 16:48:31 +0200

    Revert "[KOTLIN] Migrate ObjectWithUidInterface"
    This reverts commit db7d18c5da715887eb7ff4ad2cf8adbdfdea3576.

--- core/src/main/java/org/hisp/dhis/android/core/common/ObjectWithUidInterface.java
+++ core/src/main/java/org/hisp/dhis/android/core/common/ObjectWithUidInterface.java
@@ -25,8 +25,9 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.android.core.common
 
-interface ObjectWithUidInterface {
-    fun uid(): String
+package org.hisp.dhis.android.core.common;
+
+public interface ObjectWithUidInterface {
+    String uid();
 }

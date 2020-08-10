721daa5ea81650571605dcb08567654033913d6a
luis <luisyng@gmail.com>
2020-08-10 14:59:35 +0200

    [ANDROSDK-1215] Update migration

--- core/src/main/assets/migrations/82.sql
+++ core/src/main/assets/migrations/82.sql
@@ -1,2 +1,3 @@
 # Adds pattern to TrackedEntityAttributeReservedValue
 ALTER TABLE TrackedEntityAttributeReservedValue ADD COLUMN pattern TEXT;
+update TrackedEntityAttributeReservedValue set pattern = key;

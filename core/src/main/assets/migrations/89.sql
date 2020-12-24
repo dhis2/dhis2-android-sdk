6bf10d8b80662aebe35117c2cd080ee568eec71a
luis <luisyng@gmail.com>
2020-12-24 11:06:59 +0100

    [ANDROSDK-917-2] Fix migrations

--- core/src/main/assets/migrations/89.sql
+++ core/src/main/assets/migrations/89.sql
@@ -1,3 +1,5 @@
 # Add organisationUnitId hash to TrackedEntityInstanceSync and EventSync
-ALTER TABLE TrackedEntityInstanceSync ADD COLUMN organisationUnitIdsHash INTEGER;
-ALTER TABLE EventSync ADD COLUMN organisationUnitIdsHash INTEGER;
+DROP TABLE TrackedEntityInstanceSync;
+DROP TABLE EventSync;
+CREATE TABLE TrackedEntityInstanceSync (_id INTEGER PRIMARY KEY AUTOINCREMENT, program TEXT, organisationUnitIdsHash INTEGER, downloadLimit INTEGER NOT NULL, lastUpdated TEXT NOT NULL, FOREIGN KEY (program) REFERENCES Program (uid) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED, UNIQUE (program, organisationUnitIdsHash));
+CREATE TABLE EventSync (_id INTEGER PRIMARY KEY AUTOINCREMENT, program TEXT, organisationUnitIdsHash INTEGER, downloadLimit INTEGER NOT NULL, lastUpdated TEXT NOT NULL, FOREIGN KEY (program) REFERENCES Program (uid) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED, UNIQUE (program, organisationUnitIdsHash));

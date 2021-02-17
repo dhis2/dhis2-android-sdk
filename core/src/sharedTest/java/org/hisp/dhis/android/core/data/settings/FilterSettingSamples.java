5d9c5d9150b0c2923af0815bf66caac146148ace
andresmr <personal@andresmr.com>
2021-02-17 11:10:48 +0100

    [ANDROSDK-1317] [androsdk-1317] FilterSettingStoreIntegrationShould.kt

---
+++ core/src/sharedTest/java/org/hisp/dhis/android/core/data/settings/FilterSettingSamples.java
@@ -0,0 +1,17 @@
+package org.hisp.dhis.android.core.data.settings;
+
+import org.hisp.dhis.android.core.settings.FilterSetting;
+
+public class FilterSettingSamples {
+
+    public static FilterSetting getFilterSetting() {
+        return FilterSetting.builder()
+                .id(1L)
+                .scope("ProgramFilter")
+                .filterType("event")
+                .uid("aBcDfGe")
+                .filter(true)
+                .sort(true)
+                .build();
+    }
+}

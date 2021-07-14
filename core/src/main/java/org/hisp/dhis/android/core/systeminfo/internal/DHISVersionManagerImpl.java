43f2871845409efbe67e031bdd8b089167733b77
Victor Garcia <vgarciabnz@gmail.com>
2021-07-14 14:16:56 +0200

    [DHIS-2.37] Enable version 2.37

--- core/src/main/java/org/hisp/dhis/android/core/systeminfo/internal/DHISVersionManagerImpl.java
+++ core/src/main/java/org/hisp/dhis/android/core/systeminfo/internal/DHISVersionManagerImpl.java
@@ -122,6 +122,16 @@ public class DHISVersionManagerImpl implements DHISVersionManager {
     }
 
     @Override
+    public boolean is2_36() {
+        return getVersion() == DHISVersion.V2_36;
+    }
+
+    @Override
+    public boolean is2_37() {
+        return getVersion() == DHISVersion.V2_37;
+    }
+
+    @Override
     public boolean isGreaterThan(DHISVersion version) {
         return version.compareTo(getVersion()) < 0;
     }

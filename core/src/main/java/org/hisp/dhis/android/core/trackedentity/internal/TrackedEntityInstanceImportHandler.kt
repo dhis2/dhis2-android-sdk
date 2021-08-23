40247cb269ae902357850a0c2bed5f3693dcc92d
Victor Garcia <vgarciabnz@gmail.com>
2021-08-23 17:46:56 +0200

    [ANDROSDK-344] Typo

--- core/src/main/java/org/hisp/dhis/android/core/trackedentity/internal/TrackedEntityInstanceImportHandler.kt
+++ core/src/main/java/org/hisp/dhis/android/core/trackedentity/internal/TrackedEntityInstanceImportHandler.kt
@@ -49,7 +49,8 @@ import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstance
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceInternalAccessor
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceTableInfo
 
-@Reusable class TrackedEntityInstanceImportHandler @Inject internal constructor(
+@Reusable
+internal class TrackedEntityInstanceImportHandler @Inject internal constructor(
     private val trackedEntityInstanceStore: TrackedEntityInstanceStore,
     private val enrollmentImportHandler: EnrollmentImportHandler,
     private val trackerImportConflictStore: TrackerImportConflictStore,

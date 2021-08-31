c23193a6165ab9c00c46e9f2fbe9cdfb1b191de4
Marcos Campos <marcamsn@gmail.com>
2021-08-31 09:33:36 +0200

    [androsdk-1393] Fix tests

--- core/src/main/assets/migrations/108.sql
+++ core/src/main/assets/migrations/108.sql
@@ -5,7 +5,7 @@ CREATE TABLE LocalDataStore (_id INTEGER PRIMARY KEY AUTOINCREMENT, key TEXT NOT
 
 ALTER TABLE ProgramIndicator RENAME TO ProgramIndicator_Old;
 CREATE TABLE ProgramIndicator (_id INTEGER PRIMARY KEY AUTOINCREMENT, uid TEXT NOT NULL UNIQUE, code TEXT, name TEXT, displayName TEXT, created TEXT, lastUpdated TEXT, shortName TEXT, displayShortName TEXT, description TEXT, displayDescription TEXT, displayInForm INTEGER, expression TEXT, dimensionItem TEXT, filter TEXT, decimals INTEGER, program TEXT NOT NULL, aggregationType TEXT, analyticsType TEXT, FOREIGN KEY (program) REFERENCES Program (uid) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED);
-INSERT INTO ProgramIndicator (_id, uid, code, name, displayName, created, lastUpdated, shortName, displayShortName, description, displayDescription, displayInForm, expression, dimensionItem, filter, decimals, program, aggregationType, analyticsType) SELECT _id, uid, code, name, displayName, created, lastUpdated, shortName, displayShortName, description, displayDescription, displayInForm, expression, dimensionItem, filter, decimals, program, aggregationType, analyticsType FROM ProgramIndicator_Old;
+INSERT INTO ProgramIndicator (_id, uid, code, name, displayName, created, lastUpdated, shortName, displayShortName, description, displayDescription, displayInForm, expression, dimensionItem, filter, decimals, program, aggregationType) SELECT _id, uid, code, name, displayName, created, lastUpdated, shortName, displayShortName, description, displayDescription, displayInForm, expression, dimensionItem, filter, decimals, program, aggregationType FROM ProgramIndicator_Old;
 DROP TABLE IF EXISTS ProgramIndicator_Old;
 
 

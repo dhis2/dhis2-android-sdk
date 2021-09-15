1b9063a2dc3b29f1bff378790d1c7d6881bb3929
Victor Garcia <vgarciabnz@gmail.com>
2021-09-15 12:41:55 +0200

    [ANDROSDK-1342] Adapt migration

--- core/src/main/assets/migrations/110.sql
+++ core/src/main/assets/migrations/110.sql
@@ -1,3 +1,9 @@
 # Add allowScreenCapture to GeneralSetting
 
 ALTER TABLE GeneralSetting ADD COLUMN allowScreenCapture INTEGER;
+
+ALTER TABLE SynchronizationSetting RENAME TO SynchronizationSetting_Old;
+CREATE TABLE SynchronizationSetting (_id INTEGER PRIMARY KEY AUTOINCREMENT, dataSync TEXT, metadataSync TEXT, trackerImporterVersion TEXT);
+INSERT INTO SynchronizationSetting (_id, dataSync, metadataSync, trackerImporterVersion) SELECT _id, dataSync, metadataSync, null;
+
+DROP TABLE SynchronizationSetting_Old;

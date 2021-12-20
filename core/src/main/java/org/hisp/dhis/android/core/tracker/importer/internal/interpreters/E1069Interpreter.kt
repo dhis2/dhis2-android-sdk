5337b63665f0e871cbe1536e0f48f279271abe70
Marcos Campos <marcamsn@gmail.com>
2021-12-20 16:44:44 +0100

    [androsdk-1407] Add E1069 error

--- core/src/main/java/org/hisp/dhis/android/core/tracker/importer/internal/interpreters/E1069Interpreter.kt
+++ core/src/main/java/org/hisp/dhis/android/core/tracker/importer/internal/interpreters/E1069Interpreter.kt
@@ -1,19 +1,19 @@
 /*
  *  Copyright (c) 2004-2021, University of Oslo
  *  All rights reserved.
- *  
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
  *  Redistributions of source code must retain the above copyright notice, this
  *  list of conditions and the following disclaimer.
- *  
+ *
  *  Redistributions in binary form must reproduce the above copyright notice,
  *  this list of conditions and the following disclaimer in the documentation
  *  and/or other materials provided with the distribution.
  *  Neither the name of the HISP project nor the names of its contributors may
  *  be used to endorse or promote products derived from this software without
  *  specific prior written permission.
- *  
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,16 +25,19 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.android.testapp.tracker.importer
 
-import org.hisp.dhis.android.core.tracker.importer.internal.ImporterError
-import org.hisp.dhis.android.core.utils.runner.D2JunitRunner
-import org.junit.runner.RunWith
+package org.hisp.dhis.android.core.tracker.importer.internal.interpreters
 
-@RunWith(D2JunitRunner::class)
-internal class E1064MockIntegrationShould : BaseTrackerConflictMockIntegrationShould() {
-    override val importerError = ImporterError.E1064
-    override val errorMessage = "Non-unique attribute value `854-2454` for attribute `cejWyOfXge6`"
-    override val expectedDescription = "The attribute value `854-2454` for the `TB number` attribute is not unique. " +
-        "(Attribute: cejWyOfXge6)"
+import org.hisp.dhis.android.R
+import org.hisp.dhis.android.core.tracker.importer.internal.JobValidationError
+
+internal class E1069Interpreter internal constructor(
+    private val interpreterHelper: InterpreterHelper,
+    override val regex: Regex
+) : ErrorCodeInterpreter {
+    override val unformattedDescription = R.string.E1069
+    override fun companions(error: JobValidationError): List<String> {
+        val programUid = regex.find(error.message)!!.groupValues.last()
+        return listOf(interpreterHelper.programDisplayName(programUid), programUid, error.uid)
+    }
 }

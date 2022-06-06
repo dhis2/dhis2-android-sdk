e6f916b38a955364184f30caf3d58a577a790530
Marcos Campos <marcamsn@gmail.com>
2022-06-06 16:32:19 +0200

    [androsdk-1502] Refactor TrackedEntityInstanceFilterHandler

--- core/src/main/java/org/hisp/dhis/android/core/trackedentity/internal/TrackedEntityInstanceFilterHandler.kt
+++ core/src/main/java/org/hisp/dhis/android/core/trackedentity/internal/TrackedEntityInstanceFilterHandler.kt
@@ -32,7 +32,13 @@ import org.hisp.dhis.android.core.arch.db.stores.internal.IdentifiableObjectStor
 import org.hisp.dhis.android.core.arch.handlers.internal.HandleAction
 import org.hisp.dhis.android.core.arch.handlers.internal.HandlerWithTransformer
 import org.hisp.dhis.android.core.arch.handlers.internal.IdentifiableHandlerImpl
+import org.hisp.dhis.android.core.common.DateFilterPeriod
+import org.hisp.dhis.android.core.common.DatePeriodType
+import org.hisp.dhis.android.core.common.RelativePeriod
+import org.hisp.dhis.android.core.systeminfo.DHISVersion
+import org.hisp.dhis.android.core.systeminfo.DHISVersionManager
 import org.hisp.dhis.android.core.trackedentity.AttributeValueFilter
+import org.hisp.dhis.android.core.trackedentity.EntityQueryCriteria
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceEventFilter
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceFilter
 import javax.inject.Inject
@@ -41,7 +47,8 @@ import javax.inject.Inject
 internal class TrackedEntityInstanceFilterHandler @Inject constructor(
     trackedEntityInstanceFilterStore: IdentifiableObjectStore<TrackedEntityInstanceFilter>,
     private val trackedEntityInstanceEventFilterHandler: HandlerWithTransformer<TrackedEntityInstanceEventFilter>,
-    private val attributeValueFilterHandler: HandlerWithTransformer<AttributeValueFilter>
+    private val attributeValueFilterHandler: HandlerWithTransformer<AttributeValueFilter>,
+    private val versionManager: DHISVersionManager
 ) : IdentifiableHandlerImpl<TrackedEntityInstanceFilter>(trackedEntityInstanceFilterStore) {
 
     override fun beforeCollectionHandled(
@@ -51,10 +58,32 @@ internal class TrackedEntityInstanceFilterHandler @Inject constructor(
         return super.beforeCollectionHandled(oCollection)
     }
 
+    override fun beforeObjectHandled(o: TrackedEntityInstanceFilter): TrackedEntityInstanceFilter {
+        return if (versionManager.isGreaterOrEqualThan(DHISVersion.V2_38)) {
+            super.beforeObjectHandled(o)
+        } else {
+            super.beforeObjectHandled(
+                o.toBuilder().entityQueryCriteria(
+                    EntityQueryCriteria.builder()
+                        .followUp(o.followUp())
+                        .enrollmentStatus(o.enrollmentStatus())
+                        .enrollmentCreatedDate(
+                            DateFilterPeriod.builder()
+                                .startBuffer(o.enrollmentCreatedPeriod()?.periodFrom())
+                                .endBuffer(o.enrollmentCreatedPeriod()?.periodTo())
+                                .period(RelativePeriod.TODAY)
+                                .type(DatePeriodType.RELATIVE)
+                                .build()
+                        )
+                        .build()
+                ).build()
+            )
+        }
+    }
+
     override fun afterObjectHandled(o: TrackedEntityInstanceFilter, action: HandleAction) {
         if (action !== HandleAction.Delete) {
-            trackedEntityInstanceEventFilterHandler.handleMany(o.eventFilters()) {
-                ef: TrackedEntityInstanceEventFilter ->
+            trackedEntityInstanceEventFilterHandler.handleMany(o.eventFilters()) { ef: TrackedEntityInstanceEventFilter ->
                 ef.toBuilder().trackedEntityInstanceFilter(o.uid()).build()
             }
             o.entityQueryCriteria()?.attributeValueFilters()?.let {

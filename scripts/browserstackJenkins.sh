bdf4299fd62086ad04bbae6f5b4afeaaecd794f9
Victor Garcia <vgarciabnz@gmail.com>
2022-10-13 13:22:46 +0200

    [TEST-LAB-APP] Add mock server

--- scripts/browserstackJenkins.sh
+++ scripts/browserstackJenkins.sh
@@ -73,8 +73,9 @@ json=$(jq -n \
                 --arg language "$browserstack_language" \
                 --arg locale "$browserstack_locale" \
                 --arg deviceLogs "$browserstack_deviceLogs" \
+                --arg allowDeviceMockServer "$browserstack_mock_server" \
                 --argjson shards "$shards" \
-                '{devices: $devices, app: $app_url, testSuite: $test_url, class: $class, logs: $logs, video: $video, local: $loc, localIdentifier: $locId, gpsLocation: $gpsLocation, language: $language, locale: $locale, deviceLogs: $deviceLogs, shards: $shards}')
+                '{devices: $devices, app: $app_url, testSuite: $test_url, class: $class, logs: $logs, video: $video, local: $loc, localIdentifier: $locId, gpsLocation: $gpsLocation, language: $language, locale: $locale, deviceLogs: $deviceLogs, shards: $shards, allowDeviceMockServer: $allowDeviceMockServer}')
 
 test_execution_response="$(curl -X POST https://api-cloud.browserstack.com/app-automate/espresso/v2/build -d \ "$json" -H "Content-Type: application/json" -u "$BROWSERSTACK_USR:$BROWSERSTACK_PSW")"
 

47aeeeeac97c0f8da9a108f086fb343d5c92cf0f
Victor Garcia <vgarciabnz@gmail.com>
2023-01-16 15:39:45 +1100

    [ANDROSDK-1584] Deprecate UserCredentials repository

--- core/src/main/java/org/hisp/dhis/android/core/user/UserModule.kt
+++ core/src/main/java/org/hisp/dhis/android/core/user/UserModule.kt
@@ -25,28 +25,27 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.android.core.user;
+package org.hisp.dhis.android.core.user
 
-import org.hisp.dhis.android.core.user.openid.OpenIDConnectHandler;
+import io.reactivex.Completable
+import io.reactivex.Single
+import org.hisp.dhis.android.core.user.openid.OpenIDConnectHandler
 
-import io.reactivex.Completable;
-import io.reactivex.Single;
+@Suppress("TooManyFunctions")
+interface UserModule {
+    fun authenticatedUser(): AuthenticatedUserObjectRepository
+    fun userRoles(): UserRoleCollectionRepository
+    fun authorities(): AuthorityCollectionRepository
+    fun user(): UserObjectRepository
+    fun accountManager(): AccountManager
+    fun logIn(username: String, password: String, serverUrl: String): Single<User>
+    fun blockingLogIn(username: String, password: String, serverUrl: String): User
+    fun logOut(): Completable
+    fun blockingLogOut()
+    fun isLogged(): Single<Boolean>
+    fun blockingIsLogged(): Boolean
+    fun openIdHandler(): OpenIDConnectHandler
 
-public interface UserModule {
-    AuthenticatedUserObjectRepository authenticatedUser();
-    UserRoleCollectionRepository userRoles();
-    AuthorityCollectionRepository authorities();
-    UserCredentialsObjectRepository userCredentials();
-    UserObjectRepository user();
-
-    AccountManager accountManager();
-    
-    Single<User> logIn(String username, String password, String serverUrl);
-    User blockingLogIn(String username, String password, String serverUrl);
-    Completable logOut();
-    void blockingLogOut();
-    Single<Boolean> isLogged();
-    boolean blockingIsLogged();
-
-    OpenIDConnectHandler openIdHandler();
+    @Deprecated(message = "Use user() instead.")
+    fun userCredentials(): UserCredentialsObjectRepository
 }

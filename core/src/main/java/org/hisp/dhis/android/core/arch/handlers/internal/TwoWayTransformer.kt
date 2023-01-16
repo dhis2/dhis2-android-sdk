693c1351c4da354e7269f61fec3e05e6f163f8f8
Victor Garcia <vgarciabnz@gmail.com>
2023-01-16 15:44:40 +1100

    [ANDROSDK-1584] Undo remove internal modifier

--- core/src/main/java/org/hisp/dhis/android/core/arch/handlers/internal/TwoWayTransformer.kt
+++ core/src/main/java/org/hisp/dhis/android/core/arch/handlers/internal/TwoWayTransformer.kt
@@ -27,6 +27,6 @@
  */
 package org.hisp.dhis.android.core.arch.handlers.internal
 
-interface TwoWayTransformer<O, T> : Transformer<O, T> {
+internal interface TwoWayTransformer<O, T> : Transformer<O, T> {
     fun deTransform(t: T): O
 }

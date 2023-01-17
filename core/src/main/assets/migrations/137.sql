ddc7fa6fd0578a1a8f9e391267cbfdd984a3b0e8
Victor Garcia <vgarciabnz@gmail.com>
2023-01-17 14:30:59 +1100

    [ANDROSDK-1584] Add WHERE condition in migration

--- core/src/main/assets/migrations/137.sql
+++ core/src/main/assets/migrations/137.sql
@@ -1,5 +1,5 @@
 # Remove UserCredentials model (ANDROSDK-1584);
 
 ALTER TABLE User ADD COLUMN username TEXT;
-UPDATE User SET username = (SELECT username FROM UserCredentials);
+UPDATE User SET username = (SELECT username FROM UserCredentials) WHERE uid IS NOT NULL;
 DROP TABLE IF EXISTS UserCredentials;

4f72c706d87d11dfa55fab9812940c5808c2efcc
Marcos Campos <marcamsn@gmail.com>
2023-07-05 10:27:33 +0200

    [ANDROSDK-1701] Remove import on TrackedEntityEnrollmentOrphanCleaner

--- core/src/main/java/org/hisp/dhis/android/core/trackedentity/internal/TrackedEntityEnrollmentOrphanCleaner.kt
+++ core/src/main/java/org/hisp/dhis/android/core/trackedentity/internal/TrackedEntityEnrollmentOrphanCleaner.kt
@@ -28,7 +28,6 @@
 package org.hisp.dhis.android.core.trackedentity.internal
 
 import dagger.Reusable
-import javax.inject.Inject
 import org.hisp.dhis.android.core.arch.db.querybuilders.internal.WhereClauseBuilder
 import org.hisp.dhis.android.core.common.State
 import org.hisp.dhis.android.core.enrollment.Enrollment
@@ -36,6 +35,7 @@ import org.hisp.dhis.android.core.enrollment.EnrollmentTableInfo
 import org.hisp.dhis.android.core.enrollment.internal.EnrollmentStore
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstance
 import org.hisp.dhis.android.core.tracker.importer.internal.TrackerImporterBreakTheGlassHelper
+import javax.inject.Inject
 
 @Reusable
 internal class TrackedEntityEnrollmentOrphanCleaner @Inject constructor(

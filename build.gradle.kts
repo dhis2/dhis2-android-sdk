3f300c69e6c2b6e07fe61c0a8652a9641d91352d
andresmr <andres@dhis2.org>
2023-08-11 13:59:38 +0200

    [ANDROSDK-1737] Update kotlin to 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 buildscript {
-    val kotlinVersion = "1.7.21"
+    val kotlinVersion = "1.9.0"
 
     repositories {
         google()

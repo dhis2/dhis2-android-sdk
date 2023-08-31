9d51748731ab778ac5261fc3829fefb543f31a66
Marcos Campos <marcamsn@gmail.com>
2023-08-31 11:24:49 +0200

    [ANDROSDK-1717] Refactor CategoryCollectionRepository to kotlin

--- core/src/main/java/org/hisp/dhis/android/core/category/CategoryCollectionRepository.kt
+++ core/src/main/java/org/hisp/dhis/android/core/category/CategoryCollectionRepository.kt
@@ -25,39 +25,42 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.android.core.category;
+package org.hisp.dhis.android.core.category
 
-import org.hisp.dhis.android.core.arch.repositories.children.internal.ChildrenAppender;
-import org.hisp.dhis.android.core.arch.repositories.collection.internal.ReadOnlyIdentifiableCollectionRepositoryImpl;
-import org.hisp.dhis.android.core.arch.repositories.filters.internal.FilterConnectorFactory;
-import org.hisp.dhis.android.core.arch.repositories.filters.internal.StringFilterConnector;
-import org.hisp.dhis.android.core.arch.repositories.scope.RepositoryScope;
-import org.hisp.dhis.android.core.category.internal.CategoryFields;
-import org.hisp.dhis.android.core.category.internal.CategoryStore;
-
-import java.util.Map;
-
-import javax.inject.Inject;
-
-import dagger.Reusable;
+import dagger.Reusable
+import org.hisp.dhis.android.core.arch.repositories.children.internal.ChildrenAppender
+import org.hisp.dhis.android.core.arch.repositories.collection.internal.ReadOnlyIdentifiableCollectionRepositoryImpl
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.FilterConnectorFactory
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.StringFilterConnector
+import org.hisp.dhis.android.core.arch.repositories.scope.RepositoryScope
+import org.hisp.dhis.android.core.category.internal.CategoryFields
+import org.hisp.dhis.android.core.category.internal.CategoryStore
+import javax.inject.Inject
 
 @Reusable
-public final class CategoryCollectionRepository
-        extends ReadOnlyIdentifiableCollectionRepositoryImpl<Category, CategoryCollectionRepository> {
-
-    @Inject
-    CategoryCollectionRepository(final CategoryStore store,
-                                 final Map<String, ChildrenAppender<Category>> childrenAppenders,
-                                 final RepositoryScope scope) {
-        super(store, childrenAppenders, scope, new FilterConnectorFactory<>(scope,
-                s -> new CategoryCollectionRepository(store, childrenAppenders, s)));
+class CategoryCollectionRepository @Inject internal constructor(
+    store: CategoryStore,
+    childrenAppenders: MutableMap<String, ChildrenAppender<Category>>,
+    scope: RepositoryScope,
+) : ReadOnlyIdentifiableCollectionRepositoryImpl<Category, CategoryCollectionRepository>(
+    store,
+    childrenAppenders,
+    scope,
+    FilterConnectorFactory(
+        scope,
+    ) { s: RepositoryScope ->
+        CategoryCollectionRepository(
+            store,
+            childrenAppenders,
+            s,
+        )
+    },
+) {
+    fun byDataDimensionType(): StringFilterConnector<CategoryCollectionRepository> {
+        return cf.string(CategoryTableInfo.Columns.DATA_DIMENSION_TYPE)
     }
 
-    public StringFilterConnector<CategoryCollectionRepository> byDataDimensionType() {
-        return cf.string(CategoryTableInfo.Columns.DATA_DIMENSION_TYPE);
-    }
-
-    public CategoryCollectionRepository withCategoryOptions() {
-        return cf.withChild(CategoryFields.CATEGORY_OPTIONS);
+    fun withCategoryOptions(): CategoryCollectionRepository {
+        return cf.withChild(CategoryFields.CATEGORY_OPTIONS)
     }
 }

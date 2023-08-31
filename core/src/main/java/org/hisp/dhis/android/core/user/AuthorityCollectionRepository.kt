fa3038707d7126cb29a99f974b8bfc9c220b78a2
Marcos Campos <marcamsn@gmail.com>
2023-08-31 11:31:44 +0200

    [ANDROSDK-1717] Refactor AuthorityCollectionRepository to kotlin

--- core/src/main/java/org/hisp/dhis/android/core/user/AuthorityCollectionRepository.kt
+++ core/src/main/java/org/hisp/dhis/android/core/user/AuthorityCollectionRepository.kt
@@ -25,35 +25,37 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.hisp.dhis.android.core.user
 
-package org.hisp.dhis.android.core.user;
-
-import org.hisp.dhis.android.core.arch.repositories.children.internal.ChildrenAppender;
-import org.hisp.dhis.android.core.arch.repositories.collection.internal.ReadOnlyCollectionRepositoryImpl;
-import org.hisp.dhis.android.core.arch.repositories.filters.internal.FilterConnectorFactory;
-import org.hisp.dhis.android.core.arch.repositories.filters.internal.StringFilterConnector;
-import org.hisp.dhis.android.core.arch.repositories.scope.RepositoryScope;
-import org.hisp.dhis.android.core.user.internal.AuthorityStore;
-
-import java.util.Map;
-
-import javax.inject.Inject;
-
-import dagger.Reusable;
+import dagger.Reusable
+import org.hisp.dhis.android.core.arch.repositories.children.internal.ChildrenAppender
+import org.hisp.dhis.android.core.arch.repositories.collection.internal.ReadOnlyCollectionRepositoryImpl
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.FilterConnectorFactory
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.StringFilterConnector
+import org.hisp.dhis.android.core.arch.repositories.scope.RepositoryScope
+import org.hisp.dhis.android.core.user.internal.AuthorityStore
+import javax.inject.Inject
 
 @Reusable
-public final class AuthorityCollectionRepository
-        extends ReadOnlyCollectionRepositoryImpl<Authority, AuthorityCollectionRepository> {
-
-    @Inject
-    AuthorityCollectionRepository(final AuthorityStore store,
-                                  final Map<String, ChildrenAppender<Authority>> childrenAppenders,
-                                  final RepositoryScope scope) {
-        super(store, childrenAppenders, scope, new FilterConnectorFactory<>(scope,
-                s -> new AuthorityCollectionRepository(store, childrenAppenders, s)));
-    }
-
-    public StringFilterConnector<AuthorityCollectionRepository> byName() {
-        return cf.string(AuthorityTableInfo.Columns.NAME);
+class AuthorityCollectionRepository @Inject internal constructor(
+    store: AuthorityStore,
+    childrenAppenders: MutableMap<String, ChildrenAppender<Authority>>,
+    scope: RepositoryScope,
+) : ReadOnlyCollectionRepositoryImpl<Authority, AuthorityCollectionRepository>(
+    store,
+    childrenAppenders,
+    scope,
+    FilterConnectorFactory(
+        scope,
+    ) { s: RepositoryScope ->
+        AuthorityCollectionRepository(
+            store,
+            childrenAppenders,
+            s,
+        )
+    },
+) {
+    fun byName(): StringFilterConnector<AuthorityCollectionRepository> {
+        return cf.string(AuthorityTableInfo.Columns.NAME)
     }
 }

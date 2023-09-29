f459cc9c3ecc6d37b96f666684dbeeae930a89a5
Marcos Campos <marcamsn@gmail.com>
2023-09-29 08:47:27 +0200

    [ANDROSDK-1717] Refactor OrganisationUnitCollectionRepository to Kotlin

--- core/src/main/java/org/hisp/dhis/android/core/organisationunit/OrganisationUnitCollectionRepository.kt
+++ core/src/main/java/org/hisp/dhis/android/core/organisationunit/OrganisationUnitCollectionRepository.kt
@@ -25,114 +25,117 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.android.core.organisationunit;
+package org.hisp.dhis.android.core.organisationunit
 
-import org.hisp.dhis.android.core.arch.repositories.children.internal.ChildrenAppender;
-import org.hisp.dhis.android.core.arch.repositories.collection.internal.ReadOnlyIdentifiableCollectionRepositoryImpl;
-import org.hisp.dhis.android.core.arch.repositories.filters.internal.DateFilterConnector;
-import org.hisp.dhis.android.core.arch.repositories.filters.internal.EnumFilterConnector;
-import org.hisp.dhis.android.core.arch.repositories.filters.internal.FilterConnectorFactory;
-import org.hisp.dhis.android.core.arch.repositories.filters.internal.IntegerFilterConnector;
-import org.hisp.dhis.android.core.arch.repositories.filters.internal.StringFilterConnector;
-import org.hisp.dhis.android.core.arch.repositories.scope.RepositoryScope;
-import org.hisp.dhis.android.core.common.FeatureType;
-import org.hisp.dhis.android.core.common.IdentifiableColumns;
-import org.hisp.dhis.android.core.dataset.DataSetOrganisationUnitLinkTableInfo;
-import org.hisp.dhis.android.core.organisationunit.OrganisationUnitTableInfo.Columns;
-import org.hisp.dhis.android.core.organisationunit.internal.OrganisationUnitFields;
-import org.hisp.dhis.android.core.organisationunit.internal.OrganisationUnitStore;
-import org.hisp.dhis.android.core.user.UserOrganisationUnitLinkTableInfo;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import javax.inject.Inject;
-
-import dagger.Reusable;
+import dagger.Reusable
+import org.hisp.dhis.android.core.arch.repositories.children.internal.ChildrenAppender
+import org.hisp.dhis.android.core.arch.repositories.collection.internal.ReadOnlyIdentifiableCollectionRepositoryImpl
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.DateFilterConnector
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.EnumFilterConnector
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.FilterConnectorFactory
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.IntegerFilterConnector
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.StringFilterConnector
+import org.hisp.dhis.android.core.arch.repositories.scope.RepositoryScope
+import org.hisp.dhis.android.core.common.FeatureType
+import org.hisp.dhis.android.core.common.IdentifiableColumns
+import org.hisp.dhis.android.core.dataset.DataSetOrganisationUnitLinkTableInfo
+import org.hisp.dhis.android.core.organisationunit.internal.OrganisationUnitFields
+import org.hisp.dhis.android.core.organisationunit.internal.OrganisationUnitStore
+import org.hisp.dhis.android.core.user.UserOrganisationUnitLinkTableInfo
+import javax.inject.Inject
 
 @Reusable
-public class OrganisationUnitCollectionRepository
-        extends ReadOnlyIdentifiableCollectionRepositoryImpl<OrganisationUnit, OrganisationUnitCollectionRepository> {
-
-    @Inject
-    OrganisationUnitCollectionRepository(final OrganisationUnitStore store,
-                                         final Map<String, ChildrenAppender<OrganisationUnit>> childrenAppenders,
-                                         final RepositoryScope scope) {
-        super(store, childrenAppenders, scope, new FilterConnectorFactory<>(scope,
-                s -> new OrganisationUnitCollectionRepository(store, childrenAppenders, s)));
+class OrganisationUnitCollectionRepository @Inject internal constructor(
+    store: OrganisationUnitStore,
+    childrenAppenders: MutableMap<String, ChildrenAppender<OrganisationUnit>>,
+    scope: RepositoryScope,
+) : ReadOnlyIdentifiableCollectionRepositoryImpl<OrganisationUnit, OrganisationUnitCollectionRepository>(
+    store,
+    childrenAppenders,
+    scope,
+    FilterConnectorFactory(
+        scope,
+    ) { s: RepositoryScope ->
+        OrganisationUnitCollectionRepository(
+            store,
+            childrenAppenders,
+            s,
+        )
+    },
+) {
+    fun byParentUid(): StringFilterConnector<OrganisationUnitCollectionRepository> {
+        return cf.string(OrganisationUnitTableInfo.Columns.PARENT)
     }
 
-    public StringFilterConnector<OrganisationUnitCollectionRepository> byParentUid() {
-        return cf.string(Columns.PARENT);
+    fun byPath(): StringFilterConnector<OrganisationUnitCollectionRepository> {
+        return cf.string(OrganisationUnitTableInfo.Columns.PATH)
     }
 
-    public StringFilterConnector<OrganisationUnitCollectionRepository> byPath() {
-        return cf.string(Columns.PATH);
+    fun byOpeningDate(): DateFilterConnector<OrganisationUnitCollectionRepository> {
+        return cf.date(OrganisationUnitTableInfo.Columns.OPENING_DATE)
     }
 
-    public DateFilterConnector<OrganisationUnitCollectionRepository> byOpeningDate() {
-        return cf.date(Columns.OPENING_DATE);
+    fun byClosedDate(): DateFilterConnector<OrganisationUnitCollectionRepository> {
+        return cf.date(OrganisationUnitTableInfo.Columns.CLOSED_DATE)
     }
 
-    public DateFilterConnector<OrganisationUnitCollectionRepository> byClosedDate() {
-        return cf.date(Columns.CLOSED_DATE);
+    fun byLevel(): IntegerFilterConnector<OrganisationUnitCollectionRepository> {
+        return cf.integer(OrganisationUnitTableInfo.Columns.LEVEL)
     }
 
-    public IntegerFilterConnector<OrganisationUnitCollectionRepository> byLevel() {
-        return cf.integer(Columns.LEVEL);
+    fun byGeometryType(): EnumFilterConnector<OrganisationUnitCollectionRepository, FeatureType> {
+        return cf.enumC(OrganisationUnitTableInfo.Columns.GEOMETRY_TYPE)
     }
 
-
-    public EnumFilterConnector<OrganisationUnitCollectionRepository, FeatureType> byGeometryType() {
-        return cf.enumC(Columns.GEOMETRY_TYPE);
+    fun byGeometryCoordinates(): StringFilterConnector<OrganisationUnitCollectionRepository> {
+        return cf.string(OrganisationUnitTableInfo.Columns.GEOMETRY_COORDINATES)
     }
 
-    public StringFilterConnector<OrganisationUnitCollectionRepository> byGeometryCoordinates() {
-        return cf.string(Columns.GEOMETRY_COORDINATES);
-    }
-
-    public OrganisationUnitCollectionRepository byOrganisationUnitScope(OrganisationUnit.Scope scope) {
+    fun byOrganisationUnitScope(scope: OrganisationUnit.Scope): OrganisationUnitCollectionRepository {
         return cf.subQuery(IdentifiableColumns.UID).inLinkTable(
-                UserOrganisationUnitLinkTableInfo.TABLE_INFO.name(),
-                UserOrganisationUnitLinkTableInfo.Columns.ORGANISATION_UNIT,
-                UserOrganisationUnitLinkTableInfo.Columns.ORGANISATION_UNIT_SCOPE,
-                Collections.singletonList(scope.name()));
+            UserOrganisationUnitLinkTableInfo.TABLE_INFO.name(),
+            UserOrganisationUnitLinkTableInfo.Columns.ORGANISATION_UNIT,
+            UserOrganisationUnitLinkTableInfo.Columns.ORGANISATION_UNIT_SCOPE,
+            listOf(scope.name),
+        )
     }
 
-    public OrganisationUnitCollectionRepository byRootOrganisationUnit(Boolean isRoot) {
+    fun byRootOrganisationUnit(isRoot: Boolean): OrganisationUnitCollectionRepository {
         return cf.subQuery(IdentifiableColumns.UID).inLinkTable(
-                UserOrganisationUnitLinkTableInfo.TABLE_INFO.name(),
-                UserOrganisationUnitLinkTableInfo.Columns.ORGANISATION_UNIT,
-                UserOrganisationUnitLinkTableInfo.Columns.ROOT,
-                Collections.singletonList(isRoot ? "1" : "0"));
+            UserOrganisationUnitLinkTableInfo.TABLE_INFO.name(),
+            UserOrganisationUnitLinkTableInfo.Columns.ORGANISATION_UNIT,
+            UserOrganisationUnitLinkTableInfo.Columns.ROOT,
+            listOf(if (isRoot) "1" else "0"),
+        )
     }
 
-    public OrganisationUnitCollectionRepository byProgramUids(List<String> programUids) {
+    fun byProgramUids(programUids: List<String>): OrganisationUnitCollectionRepository {
         return cf.subQuery(IdentifiableColumns.UID).inLinkTable(
-                OrganisationUnitProgramLinkTableInfo.TABLE_INFO.name(),
-                OrganisationUnitProgramLinkTableInfo.Columns.ORGANISATION_UNIT,
-                OrganisationUnitProgramLinkTableInfo.Columns.PROGRAM,
-                programUids);
+            OrganisationUnitProgramLinkTableInfo.TABLE_INFO.name(),
+            OrganisationUnitProgramLinkTableInfo.Columns.ORGANISATION_UNIT,
+            OrganisationUnitProgramLinkTableInfo.Columns.PROGRAM,
+            programUids,
+        )
     }
 
-    public OrganisationUnitCollectionRepository byDataSetUids(List<String> dataSetUids) {
+    fun byDataSetUids(dataSetUids: List<String>): OrganisationUnitCollectionRepository {
         return cf.subQuery(IdentifiableColumns.UID).inLinkTable(
-                DataSetOrganisationUnitLinkTableInfo.TABLE_INFO.name(),
-                DataSetOrganisationUnitLinkTableInfo.Columns.ORGANISATION_UNIT,
-                DataSetOrganisationUnitLinkTableInfo.Columns.DATA_SET,
-                dataSetUids);
+            DataSetOrganisationUnitLinkTableInfo.TABLE_INFO.name(),
+            DataSetOrganisationUnitLinkTableInfo.Columns.ORGANISATION_UNIT,
+            DataSetOrganisationUnitLinkTableInfo.Columns.DATA_SET,
+            dataSetUids,
+        )
     }
 
-    public OrganisationUnitCollectionRepository withProgramUids() {
-        return cf.withChild(OrganisationUnitFields.PROGRAMS);
+    fun withProgramUids(): OrganisationUnitCollectionRepository {
+        return cf.withChild(OrganisationUnitFields.PROGRAMS)
     }
 
-    public OrganisationUnitCollectionRepository withDataSetUids() {
-        return cf.withChild(OrganisationUnitFields.DATA_SETS);
+    fun withDataSetUids(): OrganisationUnitCollectionRepository {
+        return cf.withChild(OrganisationUnitFields.DATA_SETS)
     }
 
-    public OrganisationUnitCollectionRepository withOrganisationUnitGroups() {
-        return cf.withChild(OrganisationUnitFields.ORGANISATION_UNIT_GROUPS);
+    fun withOrganisationUnitGroups(): OrganisationUnitCollectionRepository {
+        return cf.withChild(OrganisationUnitFields.ORGANISATION_UNIT_GROUPS)
     }
 }

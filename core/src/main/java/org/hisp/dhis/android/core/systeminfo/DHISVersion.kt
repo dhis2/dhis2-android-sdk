fa34a283b5cdd9fe74e8365a3b6b24c212e748a4
dan <dan@dhis2.org>
2023-10-23 11:22:10 +0200

    [ANDROSDK-1741] fix: - Grant unsupported versions are not used feature: - Handle unsupported versions smoothly

--- core/src/main/java/org/hisp/dhis/android/core/systeminfo/DHISVersion.kt
+++ core/src/main/java/org/hisp/dhis/android/core/systeminfo/DHISVersion.kt
@@ -27,7 +27,7 @@
  */
 package org.hisp.dhis.android.core.systeminfo
 
-enum class DHISVersion(internal val prefix: String) {
+enum class DHISVersion(internal val prefix: String, internal val supported: Boolean = true) {
     V2_29("2.29"),
     V2_30("2.30"),
     V2_31("2.31"),
@@ -40,13 +40,13 @@ enum class DHISVersion(internal val prefix: String) {
     V2_38("2.38"),
     V2_39("2.39"),
     V2_40("2.40"),
-    V2_41("2.41"),
+    V2_41("2.41", false),
     ;
 
     companion object {
         @JvmStatic
         fun getValue(versionStr: String): DHISVersion? {
-            return values().find { versionStr.startsWith(it.prefix) }
+            return values().find { versionStr.startsWith(it.prefix).and(it.supported) }
         }
 
         @JvmStatic
@@ -56,7 +56,7 @@ enum class DHISVersion(internal val prefix: String) {
 
         @JvmStatic
         fun allowedVersionsAsStr(): Array<String> {
-            return listOf(V2_30, V2_31, V2_32, V2_33, V2_34, V2_35, V2_36, V2_37, V2_38, V2_39, V2_40, V2_41)
+            return values().filter { it.supported }
                 .map { it.prefix }
                 .toTypedArray()
         }

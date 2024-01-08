7a138fa10e06409b1a2a3138bb7221db25820d67
Marcos Campos <marcamsn@gmail.com>
2024-01-08 12:41:34 +0100

    [ANDROSDK-1695] Add relationship store tests

--- core/src/sharedTest/java/org/hisp/dhis/android/core/data/relationship/RelationshipTypeSamples.kt
+++ core/src/sharedTest/java/org/hisp/dhis/android/core/data/relationship/RelationshipTypeSamples.kt
@@ -32,35 +32,59 @@ import org.hisp.dhis.android.core.relationship.RelationshipConstraint
 import org.hisp.dhis.android.core.relationship.RelationshipConstraintType
 import org.hisp.dhis.android.core.relationship.RelationshipEntityType
 import org.hisp.dhis.android.core.relationship.RelationshipType
+import org.hisp.dhis.android.core.relationship.TrackerDataView
 
 object RelationshipTypeSamples {
     var RELATIONSHIP_TYPE_UID_1 = "RELATIONSHIP_TYPE_UID_1"
     var RELATIONSHIP_TYPE_UID_2 = "RELATIONSHIP_TYPE_UID_2"
     var RELATIONSHIP_TYPE_UID_3 = "RELATIONSHIP_TYPE_UID_3"
     var TET_FOR_RELATIONSHIP_3_UID = "nEenWmSyUEp"
+
+    var ATTRIBUTE_1 = "ATTRIBUTE_1"
+    var ATTRIBUTE_2 = "ATTRIBUTE_2"
+    var ATTRIBUTE_3 = "ATTRIBUTE_3"
+    var DATA_ELEMENT_1 = "DATA_ELEMENT_1"
+    var DATA_ELEMENT_2 = "DATA_ELEMENT_2"
+
+    var TRACKER_DATA_VIEW_1 = TrackerDataView
+        .builder()
+        .attributes(listOf(ATTRIBUTE_1, ATTRIBUTE_2, ATTRIBUTE_3))
+        .dataElements(listOf(DATA_ELEMENT_1, DATA_ELEMENT_2))
+        .build()
+
+    var TRACKER_DATA_VIEW_2 = TrackerDataView
+        .builder()
+        .attributes(listOf(ATTRIBUTE_3))
+        .dataElements(emptyList())
+        .build()
+
     var FROM_CONSTRAINT_1 = RelationshipConstraint
         .builder()
         .id(100L)
         .constraintType(RelationshipConstraintType.FROM)
         .relationshipType(ObjectWithUid.create(RELATIONSHIP_TYPE_UID_1))
+        .trackerDataView(TRACKER_DATA_VIEW_1)
         .build()
     var TO_CONSTRAINT_1 = RelationshipConstraint
         .builder()
         .id(200L)
         .constraintType(RelationshipConstraintType.TO)
         .relationshipType(ObjectWithUid.create(RELATIONSHIP_TYPE_UID_1))
+        .trackerDataView(TRACKER_DATA_VIEW_2)
         .build()
     var FROM_CONSTRAINT_2 = RelationshipConstraint
         .builder()
         .id(300L)
         .constraintType(RelationshipConstraintType.FROM)
         .relationshipType(ObjectWithUid.create(RELATIONSHIP_TYPE_UID_2))
+        .trackerDataView(TRACKER_DATA_VIEW_2)
         .build()
     var TO_CONSTRAINT_2 = RelationshipConstraint
         .builder()
         .id(400L)
         .constraintType(RelationshipConstraintType.TO)
         .relationshipType(ObjectWithUid.create(RELATIONSHIP_TYPE_UID_2))
+        .trackerDataView(TRACKER_DATA_VIEW_1)
         .build()
     var FROM_CONSTRAINT_3 = RelationshipConstraint
         .builder()
@@ -69,12 +93,14 @@ object RelationshipTypeSamples {
         .relationshipType(ObjectWithUid.create(RELATIONSHIP_TYPE_UID_3))
         .relationshipEntity(RelationshipEntityType.TRACKED_ENTITY_INSTANCE)
         .trackedEntityType(ObjectWithUid.create(TET_FOR_RELATIONSHIP_3_UID))
+        .trackerDataView(TRACKER_DATA_VIEW_1)
         .build()
     var TO_CONSTRAINT_3 = RelationshipConstraint
         .builder()
         .id(600L)
         .constraintType(RelationshipConstraintType.TO)
         .relationshipType(ObjectWithUid.create(RELATIONSHIP_TYPE_UID_3))
+        .trackerDataView(TRACKER_DATA_VIEW_2)
         .build()
     var RELATIONSHIP_TYPE_1 = RelationshipType
         .builder()

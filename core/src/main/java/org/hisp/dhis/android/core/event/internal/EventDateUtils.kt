03541fe074ea629b2ea93a59370026bc61e762b1
Marcos Campos <marcos.campos@dhis2.org>
2024-03-11 11:10:30 +0100

    [ANDROSDK-1835] Fix crash on event expired method when event and due dates are null

--- core/src/main/java/org/hisp/dhis/android/core/event/internal/EventDateUtils.kt
+++ core/src/main/java/org/hisp/dhis/android/core/event/internal/EventDateUtils.kt
@@ -81,19 +81,20 @@ class EventDateUtils(
                 false
             }
 
-        val eventDateOrDueDate = event.eventDate() ?: event.dueDate()
+        val expiredBecauseOfPeriod = (event.eventDate() ?: event.dueDate())?.let { eventDateOrDueDate ->
+            programPeriodType?.let { periodType ->
 
-        val expiredBecauseOfPeriod = programPeriodType?.let { periodType ->
-            var nextPeriod = periodHelper
-                .blockingGetPeriodForPeriodTypeAndDate(periodType, eventDateOrDueDate!!, 1).startDate()!!
-            val currentDate: Date = getCalendar().time
-            if (expiryDays > 0) {
-                val calendar: Calendar = getCalendar()
-                calendar.time = nextPeriod
-                calendar.add(Calendar.DAY_OF_YEAR, expiryDays)
-                nextPeriod = calendar.time
+                var nextPeriod = periodHelper
+                    .blockingGetPeriodForPeriodTypeAndDate(periodType, eventDateOrDueDate, 1).startDate()!!
+                val currentDate: Date = getCalendar().time
+                if (expiryDays > 0) {
+                    val calendar: Calendar = getCalendar()
+                    calendar.time = nextPeriod
+                    calendar.add(Calendar.DAY_OF_YEAR, expiryDays)
+                    nextPeriod = calendar.time
+                }
+                nextPeriod <= currentDate
             }
-            nextPeriod <= currentDate
         } ?: false
 
         return expiredBecauseOfCompletion || expiredBecauseOfPeriod

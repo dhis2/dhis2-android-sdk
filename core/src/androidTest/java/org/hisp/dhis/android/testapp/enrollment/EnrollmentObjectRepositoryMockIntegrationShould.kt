e9d39781fbf49da2cfeee66136dc3353f43b56a5
Marcos Campos <marcos.campos@dhis2.org>
2024-05-24 11:28:53 +0200

    [ANDROSDK-1394] Add not_update_status_when_passing_same_value test in EnrollmentObjectRepositoryMockIntegrationShould

--- core/src/androidTest/java/org/hisp/dhis/android/testapp/enrollment/EnrollmentObjectRepositoryMockIntegrationShould.kt
+++ core/src/androidTest/java/org/hisp/dhis/android/testapp/enrollment/EnrollmentObjectRepositoryMockIntegrationShould.kt
@@ -1,19 +1,19 @@
 /*
  *  Copyright (c) 2004-2022, University of Oslo
  *  All rights reserved.
- *  
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
  *  Redistributions of source code must retain the above copyright notice, this
  *  list of conditions and the following disclaimer.
- *  
+ *
  *  Redistributions in binary form must reproduce the above copyright notice,
  *  this list of conditions and the following disclaimer in the documentation
  *  and/or other materials provided with the distribution.
  *  Neither the name of the HISP project nor the names of its contributors may
  *  be used to endorse or promote products derived from this software without
  *  specific prior written permission.
- *  
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,167 +25,221 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.hisp.dhis.android.testapp.enrollment
 
-package org.hisp.dhis.android.testapp.enrollment;
+import com.google.common.truth.Truth.assertThat
+import org.hisp.dhis.android.core.common.FeatureType
+import org.hisp.dhis.android.core.common.Geometry
+import org.hisp.dhis.android.core.common.State
+import org.hisp.dhis.android.core.data.enrollment.EnrollmentSamples
+import org.hisp.dhis.android.core.enrollment.EnrollmentCreateProjection
+import org.hisp.dhis.android.core.enrollment.EnrollmentObjectRepository
+import org.hisp.dhis.android.core.enrollment.EnrollmentStatus
+import org.hisp.dhis.android.core.enrollment.EnrollmentTableInfo
+import org.hisp.dhis.android.core.maintenance.D2Error
+import org.hisp.dhis.android.core.maintenance.D2ErrorCode
+import org.hisp.dhis.android.core.organisationunit.OrganisationUnit
+import org.hisp.dhis.android.core.organisationunit.internal.OrganisationUnitStoreImpl
+import org.hisp.dhis.android.core.utils.integration.mock.BaseMockIntegrationTestFullDispatcher
+import org.hisp.dhis.android.core.utils.runner.D2JunitRunner
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.Date
 
-import org.hisp.dhis.android.core.common.FeatureType;
-import org.hisp.dhis.android.core.common.Geometry;
-import org.hisp.dhis.android.core.enrollment.EnrollmentCreateProjection;
-import org.hisp.dhis.android.core.enrollment.EnrollmentObjectRepository;
-import org.hisp.dhis.android.core.enrollment.EnrollmentStatus;
-import org.hisp.dhis.android.core.maintenance.D2Error;
-import org.hisp.dhis.android.core.maintenance.D2ErrorCode;
-import org.hisp.dhis.android.core.organisationunit.OrganisationUnit;
-import org.hisp.dhis.android.core.organisationunit.internal.OrganisationUnitStoreImpl;
-import org.hisp.dhis.android.core.utils.integration.mock.BaseMockIntegrationTestFullDispatcher;
-import org.hisp.dhis.android.core.utils.runner.D2JunitRunner;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.util.Date;
-
-import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.fail;
-
-@RunWith(D2JunitRunner.class)
-public class EnrollmentObjectRepositoryMockIntegrationShould extends BaseMockIntegrationTestFullDispatcher {
-
+@RunWith(D2JunitRunner::class)
+class EnrollmentObjectRepositoryMockIntegrationShould : BaseMockIntegrationTestFullDispatcher() {
     @Test
-    public void update_organisation_unit() throws D2Error {
-        String orgUnitUid = "new_org_unit";
-        new OrganisationUnitStoreImpl(databaseAdapter).insert(OrganisationUnit.builder().uid(orgUnitUid).build());
+    @Throws(D2Error::class)
+    fun update_organisation_unit() {
+        val orgUnitUid = "new_org_unit"
+        OrganisationUnitStoreImpl(databaseAdapter).insert(OrganisationUnit.builder().uid(orgUnitUid).build())
 
-        EnrollmentObjectRepository repository = objectRepository();
+        val repository = objectRepository()
 
-        repository.setOrganisationUnitUid(orgUnitUid);
-        assertThat(repository.blockingGet().organisationUnit()).isEqualTo(orgUnitUid);
+        repository.setOrganisationUnitUid(orgUnitUid)
+        assertThat(repository.blockingGet()!!.organisationUnit()).isEqualTo(orgUnitUid)
 
-        repository.blockingDelete();
-        new OrganisationUnitStoreImpl(databaseAdapter).delete(orgUnitUid);
+        repository.blockingDelete()
+        OrganisationUnitStoreImpl(databaseAdapter).delete(orgUnitUid)
     }
 
-    @Test(expected = D2Error.class)
-    public void not_update_organisation_unit_if_not_exists() throws D2Error {
-        String orgUnitUid = "new_org_unit";
+    @Test(expected = D2Error::class)
+    @Throws(D2Error::class)
+    fun not_update_organisation_unit_if_not_exists() {
+        val orgUnitUid = "new_org_unit"
 
-        EnrollmentObjectRepository repository = objectRepository();
+        val repository = objectRepository()
 
         try {
-            repository.setOrganisationUnitUid(orgUnitUid);
+            repository.setOrganisationUnitUid(orgUnitUid)
         } finally {
-            repository.blockingDelete();
+            repository.blockingDelete()
         }
     }
 
     @Test
-    public void update_enrollment_date() throws D2Error {
-        Date enrollmentDate = new Date();
+    @Throws(D2Error::class)
+    fun update_enrollment_date() {
+        val enrollmentDate = Date()
 
-        EnrollmentObjectRepository repository = objectRepository();
+        val repository = objectRepository()
 
-        repository.setEnrollmentDate(enrollmentDate);
-        assertThat(repository.blockingGet().enrollmentDate()).isEqualTo(enrollmentDate);
+        repository.setEnrollmentDate(enrollmentDate)
+        assertThat(repository.blockingGet()!!.enrollmentDate()).isEqualTo(enrollmentDate)
 
-        repository.blockingDelete();
+        repository.blockingDelete()
     }
 
     @Test
-    public void update_incident_date() throws D2Error {
-        Date incidentDate = new Date();
+    @Throws(D2Error::class)
+    fun not_update_status_when_passing_same_value() {
+        val enrollmentUid = "enrollment_uid"
 
-        EnrollmentObjectRepository repository = objectRepository();
+        d2.databaseAdapter().insert(
+            EnrollmentTableInfo.TABLE_INFO.name(),
+            null,
+            EnrollmentSamples.get(
+                enrollmentUid,
+                "DiszpKrYNg8",
+                "lxAQ7Zs9VYR",
+                "nWrB0TfWlvh",
+                Date(),
+            ).toBuilder()
+                .id(null)
+                .aggregatedSyncState(State.SYNCED)
+                .syncState(State.SYNCED)
+                .status(EnrollmentStatus.ACTIVE)
+                .build()
+                .toContentValues(),
+        )
 
-        repository.setIncidentDate(incidentDate);
-        assertThat(repository.blockingGet().incidentDate()).isEqualTo(incidentDate);
+        val repository = d2.enrollmentModule().enrollments().uid(enrollmentUid)
 
-        repository.blockingDelete();
+        repository.setStatus(EnrollmentStatus.ACTIVE)
+        assertThat(repository.blockingGet()!!.aggregatedSyncState()).isEqualTo(State.SYNCED)
+
+        repository.setStatus(EnrollmentStatus.COMPLETED)
+        assertThat(repository.blockingGet()!!.aggregatedSyncState()).isEqualTo(State.TO_UPDATE)
+
+        d2.databaseAdapter().delete(
+            EnrollmentTableInfo.TABLE_INFO.name(),
+            "uid = ?",
+            arrayOf(enrollmentUid),
+        )
+
+        assertThat(repository.blockingExists()).isFalse()
     }
 
     @Test
-    public void update_completed_date() throws D2Error {
-        Date completedDate = new Date();
+    @Throws(D2Error::class)
+    fun update_incident_date() {
+        val incidentDate = Date()
 
-        EnrollmentObjectRepository repository = objectRepository();
+        val repository = objectRepository()
 
-        repository.setCompletedDate(completedDate);
-        assertThat(repository.blockingGet().completedDate()).isEqualTo(completedDate);
+        repository.setIncidentDate(incidentDate)
+        assertThat(repository.blockingGet()!!.incidentDate()).isEqualTo(incidentDate)
 
-        repository.blockingDelete();
+        repository.blockingDelete()
     }
 
     @Test
-    public void update_follow_up() throws D2Error {
-        EnrollmentObjectRepository repository = objectRepository();
+    @Throws(D2Error::class)
+    fun update_completed_date() {
+        val completedDate = Date()
 
-        repository.setFollowUp(true);
-        assertThat(repository.blockingGet().followUp()).isTrue();
+        val repository = objectRepository()
 
-        repository.blockingDelete();
+        repository.setCompletedDate(completedDate)
+        assertThat(repository.blockingGet()!!.completedDate()).isEqualTo(completedDate)
+
+        repository.blockingDelete()
     }
 
     @Test
-    public void update_enrollment_status_completed() throws D2Error {
-        EnrollmentStatus enrollmentStatus = EnrollmentStatus.COMPLETED;
+    @Throws(D2Error::class)
+    fun update_follow_up() {
+        val repository = objectRepository()
 
-        EnrollmentObjectRepository repository = objectRepository();
+        repository.setFollowUp(true)
+        assertThat(repository.blockingGet()!!.followUp()).isTrue()
 
-        repository.setStatus(enrollmentStatus);
-        assertThat(repository.blockingGet().status()).isEqualTo(enrollmentStatus);
-        assertThat(repository.blockingGet().completedDate()).isNotNull();
-
-        repository.blockingDelete();
+        repository.blockingDelete()
     }
 
     @Test
-    public void update_enrollment_status_active() throws D2Error {
-        EnrollmentStatus enrollmentStatus = EnrollmentStatus.ACTIVE;
+    @Throws(D2Error::class)
+    fun update_enrollment_status_completed() {
+        val enrollmentStatus = EnrollmentStatus.COMPLETED
 
-        EnrollmentObjectRepository repository = objectRepository();
+        val repository = objectRepository()
 
-        repository.setStatus(enrollmentStatus);
-        assertThat(repository.blockingGet().status()).isEqualTo(enrollmentStatus);
-        assertThat(repository.blockingGet().completedDate()).isNull();
+        repository.setStatus(enrollmentStatus)
+        assertThat(repository.blockingGet()!!.status()).isEqualTo(enrollmentStatus)
+        assertThat(repository.blockingGet()!!.completedDate()).isNotNull()
 
-        repository.blockingDelete();
+        repository.blockingDelete()
     }
 
     @Test
-    public void update_geometry() throws D2Error {
-        Geometry geometry = Geometry.builder()
-                .type(FeatureType.POINT)
-                .coordinates("[10.00, 11.00]")
-                .build();
+    @Throws(D2Error::class)
+    fun update_enrollment_status_active() {
+        val enrollmentStatus = EnrollmentStatus.ACTIVE
 
-        EnrollmentObjectRepository repository = objectRepository();
+        val repository = objectRepository()
 
-        repository.setGeometry(geometry);
-        assertThat(repository.blockingGet().geometry()).isEqualTo(geometry);
+        repository.setStatus(enrollmentStatus)
+        assertThat(repository.blockingGet()!!.status()).isEqualTo(enrollmentStatus)
+        assertThat(repository.blockingGet()!!.completedDate()).isNull()
 
-        repository.blockingDelete();
+        repository.blockingDelete()
     }
 
     @Test
-    public void update_invalid_geometry() throws D2Error {
-        Geometry geometry = Geometry.builder()
-                .type(FeatureType.POINT)
-                .build();
+    @Throws(D2Error::class)
+    fun update_geometry() {
+        val geometry = Geometry.builder()
+            .type(FeatureType.POINT)
+            .coordinates("[10.00, 11.00]")
+            .build()
 
-        EnrollmentObjectRepository repository = objectRepository();
+        val repository = objectRepository()
+
+        repository.setGeometry(geometry)
+        assertThat(repository.blockingGet()!!.geometry()).isEqualTo(geometry)
+
+        repository.blockingDelete()
+    }
+
+    @Test
+    @Throws(D2Error::class)
+    fun update_invalid_geometry() {
+        val geometry = Geometry.builder()
+            .type(FeatureType.POINT)
+            .build()
+
+        val repository = objectRepository()
 
         try {
-            repository.setGeometry(geometry);
-            fail("Invalid geometry should fail");
-        } catch (D2Error d2Error) {
-            assertThat(d2Error.errorCode()).isEquivalentAccordingToCompareTo(D2ErrorCode.INVALID_GEOMETRY_VALUE);
+            repository.setGeometry(geometry)
+            Assert.fail("Invalid geometry should fail")
+        } catch (d2Error: D2Error) {
+            assertThat(d2Error.errorCode()).isEquivalentAccordingToCompareTo(D2ErrorCode.INVALID_GEOMETRY_VALUE)
         } finally {
-            repository.blockingDelete();
+            repository.blockingDelete()
         }
     }
 
-    private EnrollmentObjectRepository objectRepository() throws D2Error {
-        String enrollmentUid = d2.enrollmentModule().enrollments().blockingAdd(
-                EnrollmentCreateProjection.create(
-                        "DiszpKrYNg8", "lxAQ7Zs9VYR", "nWrB0TfWlvh"));
-        return d2.enrollmentModule().enrollments().uid(enrollmentUid);
+    @Throws(D2Error::class)
+    private fun objectRepository(): EnrollmentObjectRepository {
+        val enrollmentUid = d2.enrollmentModule().enrollments().blockingAdd(
+            EnrollmentCreateProjection.create(
+                "DiszpKrYNg8",
+                "lxAQ7Zs9VYR",
+                "nWrB0TfWlvh",
+            ),
+        )
+        return d2.enrollmentModule().enrollments().uid(enrollmentUid)
     }
 }

14e7149cc45505c01bca6d82f73279e3f1575ae3
Marcos Campos <marcos.campos@dhis2.org>
2024-08-21 11:59:09 +0200

    [ANDROSDK-1900] Adapt tests

--- core/src/test/java/org/hisp/dhis/android/core/trackedentity/internal/TrackedEntityAttributeReservedValueValidatorHelperShould.kt
+++ core/src/test/java/org/hisp/dhis/android/core/trackedentity/internal/TrackedEntityAttributeReservedValueValidatorHelperShould.kt
@@ -25,67 +25,142 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.android.core.trackedentity.internal;
+package org.hisp.dhis.android.core.trackedentity.internal
 
-import org.hisp.dhis.android.core.period.internal.CalendarUtils;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import com.google.common.truth.Truth.assertThat
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import org.hisp.dhis.android.core.period.clock.internal.ClockProviderFactory
+import org.hisp.dhis.android.core.period.clock.internal.setFixed
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
-import static com.google.common.truth.Truth.assertThat;
-
-@RunWith(JUnit4.class)
-public class TrackedEntityAttributeReservedValueValidatorHelperShould {
-
-    private TrackedEntityAttributeReservedValueValidatorHelper helper;
+@RunWith(JUnit4::class)
+class TrackedEntityAttributeReservedValueValidatorHelperShould {
+    private lateinit var helper: TrackedEntityAttributeReservedValueValidatorHelper
 
     @Before
-    public void setUp() throws IOException {
-        helper = new TrackedEntityAttributeReservedValueValidatorHelper();
+    fun setUp() {
+        ClockProviderFactory.setFixed()
+        helper = TrackedEntityAttributeReservedValueValidatorHelper()
     }
 
     @Test
-    public void get_next_expiry_date() throws Exception {
-        Date date = helper.nextExpiryDate(true, false, false);
-        Calendar cal = setCalendarToDayInit();
-        cal.add(Calendar.YEAR, 1);
-        cal.set(Calendar.DAY_OF_YEAR, 1);
+    fun test_get_expiry_date_code_with_yearly_pattern() {
+        val pattern = "CURRENT_DATE(Y)"
+        val expiryInstant = helper.getExpiryDateCode(pattern)
+        val expectedInstant = LocalDate(2020, 1, 1)
+            .atStartOfDayIn(TimeZone.currentSystemDefault())
 
-        assertThat(date).isEqualTo(cal.getTime());
+        assertThat(expiryInstant).isEqualTo(expectedInstant)
     }
 
     @Test
-    public void get_current_date_list_from_pattern() {
-        List<String> result = helper.getCurrentDatePatternStrList("CURRENT_DATE(YYYYMM) + RANDOM(###) + CURRENT_DATE(MMww)");
+    fun test_get_expiry_date_code_with_monthly_pattern() {
+        val pattern = "CURRENT_DATE(M)"
+        val expiryInstant = helper.getExpiryDateCode(pattern)
+        val expectedInstant = LocalDate(2020, 1, 1)
+            .atStartOfDayIn(TimeZone.currentSystemDefault())
 
-        assertThat(result).isEqualTo(new ArrayList<>(Arrays.asList("YYYYMM", "MMww")));
+        assertThat(expiryInstant).isEqualTo(expectedInstant)
     }
 
     @Test
-    public void get_expiry_date_from_pattern() {
-        Date date = helper.getExpiryDateCode("CURRENT_DATE(YYYYMM) + RANDOM(###) + CURRENT_DATE(MMww)");
-        Calendar cal = setCalendarToDayInit();
-        cal.setFirstDayOfWeek(Calendar.MONDAY);
-        cal.add(Calendar.WEEK_OF_YEAR, 1);
-        CalendarUtils.setDayOfWeek(cal, Calendar.MONDAY);
+    fun test_get_expiry_date_code_with_weekly_pattern() {
+        val pattern = "CURRENT_DATE(w)"
+        val expiryInstant = helper.getExpiryDateCode(pattern)
+        val expectedInstant = LocalDate(2019, 12, 16)
+            .atStartOfDayIn(TimeZone.currentSystemDefault())
 
-        assertThat(date).isEqualTo(cal.getTime());
+        assertThat(expiryInstant).isEqualTo(expectedInstant)
     }
 
-    private Calendar setCalendarToDayInit() {
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.clear(Calendar.MINUTE);
-        cal.clear(Calendar.SECOND);
-        cal.clear(Calendar.MILLISECOND);
-        return cal;
+    @Test
+    fun test_get_expiry_date_code_with_combo_pattern() {
+        val pattern = "CURRENT_DATE(YYYYMM) + RANDOM(###) + CURRENT_DATE(MMww)"
+        val expiryInstant = helper.getExpiryDateCode(pattern)
+        val expectedInstant = LocalDate(2019, 12, 16)
+            .atStartOfDayIn(TimeZone.currentSystemDefault())
+
+        assertThat(expiryInstant).isEqualTo(expectedInstant)
+    }
+
+    @Test
+    fun get_next_expiry_date() {
+        val instant = helper.nextExpiryDate(yearly = true, monthly = false, weekly = false)
+        val expectedInstant = LocalDate(2020, 1, 1)
+            .atStartOfDayIn(TimeZone.currentSystemDefault())
+
+        assertThat(instant).isEqualTo(expectedInstant)
+    }
+
+    @Test
+    fun get_expiry_date_from_pattern() {
+        val expiryInstant = helper.getExpiryDateCode("CURRENT_DATE(YYYYMM) + RANDOM(###) + CURRENT_DATE(MMww)")
+        val expectedInstant = LocalDate(2019, 12, 16)
+            .atStartOfDayIn(TimeZone.currentSystemDefault())
+
+        assertThat(expiryInstant).isEqualTo(expectedInstant)
+    }
+
+    @Test
+    fun test_get_current_date_pattern_str_list_with_valid_pattern() {
+        val pattern = "CURRENT_DATE(Y) CURRENT_DATE(M) CURRENT_DATE(w)"
+        val result = helper.getCurrentDatePatternStrList(pattern)
+
+        assertThat(result).containsExactly("Y", "M", "w")
+    }
+
+    @Test
+    fun get_current_date_list_from_pattern() {
+        val pattern = "CURRENT_DATE(YYYYMM) + RANDOM(###) + CURRENT_DATE(MMww)"
+        val result = helper.getCurrentDatePatternStrList(pattern)
+
+        assertThat(listOf("YYYYMM", "MMww")).isEqualTo(result)
+    }
+
+    @Test
+    fun test_get_current_date_pattern_str_list_with_empty_pattern() {
+        val pattern = ""
+        val result = helper.getCurrentDatePatternStrList(pattern)
+        assertThat(result).isEmpty()
+    }
+
+    @Test
+    fun test_get_current_date_pattern_str_list_with_null_pattern() {
+        val result = helper.getCurrentDatePatternStrList(null)
+        assertThat(result).isEmpty()
+    }
+
+    @Test
+    fun test_get_current_date_pattern_str_list_with_invalid_pattern() {
+        val pattern = "SOME_TEXT CURRENT_DATE() MORE_TEXT"
+        val result = helper.getCurrentDatePatternStrList(pattern)
+
+        assertThat(result).isEmpty()
+    }
+
+    @Test
+    fun test_get_expiry_date_code_with_no_valid_flags() {
+        val pattern = "CURRENT_DATE()"
+        try {
+            helper.getExpiryDateCode(pattern)
+            assertThat("Exception should be thrown").isEmpty()
+        } catch (e: IllegalStateException) {
+            assertThat(e).hasMessageThat().isEqualTo("No expiry date available for this pattern.")
+        }
+    }
+
+    @Test
+    fun test_next_expiry_date_with_no_flags_should_throw_exception() {
+        try {
+            helper.nextExpiryDate(yearly = false, monthly = false, weekly = false)
+            assertThat("Exception should be thrown").isEmpty()
+        } catch (e: IllegalStateException) {
+            assertThat(e).hasMessageThat().isEqualTo("No expiry date available for this pattern.")
+        }
     }
 }

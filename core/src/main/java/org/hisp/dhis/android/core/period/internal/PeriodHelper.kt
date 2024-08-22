6224915511f57219d5263e020b5b34e34f917f19
Marcos Campos <marcos.campos@dhis2.org>
2024-08-22 11:03:53 +0200

    [ANDROSDK-1902] Remove calendar usages from PeriodHelper

--- core/src/main/java/org/hisp/dhis/android/core/period/internal/PeriodHelper.kt
+++ core/src/main/java/org/hisp/dhis/android/core/period/internal/PeriodHelper.kt
@@ -25,58 +25,23 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.hisp.dhis.android.core.period.internal
 
-package org.hisp.dhis.android.core.period.internal;
+import io.reactivex.Single
+import org.hisp.dhis.android.core.period.Period
+import org.hisp.dhis.android.core.period.PeriodType
+import org.hisp.dhis.android.core.period.PeriodType.Companion.periodTypeFromPeriodId
+import org.koin.core.annotation.Singleton
+import java.util.Date
+import kotlin.math.ceil
 
-import org.hisp.dhis.android.core.period.Period;
-import org.hisp.dhis.android.core.period.PeriodType;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
-import io.reactivex.Single;
-import io.reactivex.annotations.NonNull;
-
-public class PeriodHelper {
-
-    private final PeriodStore periodStore;
-    private final PeriodForDataSetManager periodForDataSetManager;
-    private final ParentPeriodGenerator parentPeriodGenerator;
-    private final PeriodParser periodParser;
-    private final CalendarProvider calendarProvider;
-
-    public PeriodHelper(PeriodStore periodStore,
-                        PeriodForDataSetManager periodForDataSetManager,
-                        ParentPeriodGenerator parentPeriodGenerator,
-                        PeriodParser periodParser,
-                        CalendarProvider calendarProvider) {
-        this.periodStore = periodStore;
-        this.periodForDataSetManager = periodForDataSetManager;
-        this.parentPeriodGenerator = parentPeriodGenerator;
-        this.periodParser = periodParser;
-        this.calendarProvider = calendarProvider;
-    }
-
-    public static Integer getDays(Period period) {
-        long diff = period.endDate().getTime() - period.startDate().getTime();
-        return (int) Math.ceil(diff / (1000.0 * 60.0 * 60.0 * 24.0));
-    }
-
-    /**
-     * Get a period object specifying a periodType and a date in the period.
-     * If the period does not exist in the database, it is inserted.
-     *
-     * @param periodType Period type
-     * @param date       Date contained in the period
-     * @return Period
-     * @deprecated Use {@link #getPeriodForPeriodTypeAndDate(PeriodType, Date)} instead.
-     */
-    @Deprecated
-    public Period getPeriod(@NonNull PeriodType periodType, @NonNull Date date) {
-        return blockingGetPeriodForPeriodTypeAndDate(periodType, date, 0);
-    }
-
+@Singleton
+class PeriodHelper internal constructor(
+    private val periodStore: PeriodStore,
+    private val periodForDataSetManager: PeriodForDataSetManager,
+    private val parentPeriodGenerator: ParentPeriodGenerator,
+    private val periodParser: PeriodParser
+) {
     /**
      * Get a period object specifying a periodType and a date in the period.
      * If the period does not exist in the database, it is inserted.
@@ -85,8 +50,12 @@ public class PeriodHelper {
      * @param date       Date contained in the period
      * @return Period
      */
-    public Period blockingGetPeriodForPeriodTypeAndDate(@NonNull PeriodType periodType, @NonNull Date date) {
-        return blockingGetPeriodForPeriodTypeAndDate(periodType, date, 0);
+    @Deprecated(
+        "Use {@link #getPeriodForPeriodTypeAndDate(PeriodType, Date)} instead.",
+        ReplaceWith("getPeriodForPeriodTypeAndDate(periodType, date)")
+    )
+    fun getPeriod(periodType: PeriodType, date: Date): Period {
+        return blockingGetPeriodForPeriodTypeAndDate(periodType, date, 0)
     }
 
     /**
@@ -95,10 +64,23 @@ public class PeriodHelper {
      *
      * @param periodType Period type
      * @param date       Date contained in the period
-     * @return {@code Single} with the generated period.
+     * @return `Single` with the generated period.
      */
-    public Single<Period> getPeriodForPeriodTypeAndDate(@NonNull PeriodType periodType, @NonNull Date date) {
-        return Single.just(blockingGetPeriodForPeriodTypeAndDate(periodType, date));
+    fun getPeriodForPeriodTypeAndDate(periodType: PeriodType, date: Date): Single<Period> {
+        return Single.just(blockingGetPeriodForPeriodTypeAndDate(periodType, date))
+    }
+
+    /**
+     * Get a period object specifying a periodType and a date in the period.
+     * If the period does not exist in the database, it is inserted.
+     *
+     * @param periodType   Period type
+     * @param date         Date contained in the period
+     * @param periodOffset Number of periods backwards or forwards relative to 'date'
+     * @return `Single` with the generated period.
+     */
+    fun getPeriodForPeriodTypeAndDate(periodType: PeriodType, date: Date, periodOffset: Int): Single<Period> {
+        return Single.just(blockingGetPeriodForPeriodTypeAndDate(periodType, date, periodOffset))
     }
 
     /**
@@ -110,72 +92,55 @@ public class PeriodHelper {
      * @param periodOffset Number of periods backwards or forwards relative to 'date'
      * @return Period
      */
-    public Period blockingGetPeriodForPeriodTypeAndDate(@NonNull PeriodType periodType, @NonNull Date date,
-                                                        @NonNull int periodOffset) {
-        Period period = parentPeriodGenerator.generatePeriod(periodType, date, periodOffset);
+    @JvmOverloads
+    @Throws(IllegalStateException::class)
+    fun blockingGetPeriodForPeriodTypeAndDate(periodType: PeriodType, date: Date, periodOffset: Int = 0): Period {
+        return parentPeriodGenerator.generatePeriod(periodType, date, periodOffset)?.also { period ->
+            periodStore.selectByPeriodId(period.periodId()) ?: periodStore.updateOrInsertWhere(period)
+        } ?: error("Generated period is null")
+    }
 
-        if (periodStore.selectByPeriodId(period.periodId()) == null) {
-            periodStore.updateOrInsertWhere(period);
+    /**
+     * Get a period object specifying a periodId.
+     * If the periodId does not exist in the database, it is inserted.
+     *
+     * @param periodId Period id.
+     * @return Period
+     * @throws IllegalArgumentException if the periodId does not match any period
+     */
+    @Throws(IllegalArgumentException::class)
+    fun blockingGetPeriodForPeriodId(periodId: String): Period {
+        val periodType = periodTypeFromPeriodId(periodId)
+        val date = Date(periodParser.parse(periodId, periodType).toEpochMilliseconds())
+
+        return blockingGetPeriodForPeriodTypeAndDate(periodType, date)
+    }
+
+    /**
+     * Get a period object specifying a periodId.
+     * If the periodId does not exist in the database, it is inserted.
+     *
+     * @param periodId Period id.
+     * @return `Single` with the generated period.
+     * @throws IllegalArgumentException if the periodId does not match any period
+     */
+    @Throws(IllegalArgumentException::class)
+    fun getPeriodForPeriodId(periodId: String): Single<Period> {
+        return Single.just(blockingGetPeriodForPeriodId(periodId))
+    }
+
+    fun getPeriodsForDataSet(dataSetUid: String): Single<List<Period>> {
+        return periodForDataSetManager.getPeriodsForDataSet(dataSetUid)
+    }
+
+    fun blockingGetPeriodsForDataSet(dataSetUid: String): List<Period> {
+        return getPeriodsForDataSet(dataSetUid).blockingGet()
+    }
+
+    companion object {
+        fun getDays(period: Period): Int {
+            val diffInMillis = period.endDate()!!.time - period.startDate()!!.time
+            return ceil(diffInMillis / (1000 * 60 * 60 * 24).toDouble()).toInt()
         }
-
-        return period;
-    }
-
-    /**
-     * Get a period object specifying a periodType and a date in the period.
-     * If the period does not exist in the database, it is inserted.
-     *
-     * @param periodType   Period type
-     * @param date         Date contained in the period
-     * @param periodOffset Number of periods backwards or forwards relative to 'date'
-     * @return {@code Single} with the generated period.
-     */
-    public Single<Period> getPeriodForPeriodTypeAndDate(@NonNull PeriodType periodType, @NonNull Date date,
-                                                        @NonNull int periodOffset) {
-        return Single.just(blockingGetPeriodForPeriodTypeAndDate(periodType, date, periodOffset));
-    }
-
-    /**
-     * Get a period object specifying a periodId.
-     * If the periodId does not exist in the database, it is inserted.
-     *
-     * @param periodId Period id.
-     * @return Period
-     * @throws IllegalArgumentException if the periodId does not match any period
-     */
-    public Period blockingGetPeriodForPeriodId(@NonNull String periodId) throws IllegalArgumentException {
-        PeriodType periodType = PeriodType.periodTypeFromPeriodId(periodId);
-        Date date = new Date(periodParser.parse(periodId, periodType).toEpochMilliseconds());
-
-        return blockingGetPeriodForPeriodTypeAndDate(periodType, date);
-    }
-
-    /**
-     * Get a period object specifying a periodId.
-     * If the periodId does not exist in the database, it is inserted.
-     *
-     * @param periodId Period id.
-     * @return {@code Single} with the generated period.
-     * @throws IllegalArgumentException if the periodId does not match any period
-     */
-    public Single<Period> getPeriodForPeriodId(@NonNull String periodId) throws IllegalArgumentException {
-        return Single.just(blockingGetPeriodForPeriodId(periodId));
-    }
-
-    public Single<List<Period>> getPeriodsForDataSet(String dataSetUid) {
-        return periodForDataSetManager.getPeriodsForDataSet(dataSetUid);
-    }
-
-    /**
-     * Get a calendar instance
-     *
-     * @return a calendar instance
-     */
-    public Calendar getCalendar() {
-        return calendarProvider.getCalendar();
-    }
-
-    public List<Period> blockingGetPeriodsForDataSet(String dataSetUid) {
-        return getPeriodsForDataSet(dataSetUid).blockingGet();
     }
 }

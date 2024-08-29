e1f1f1d571a97949c91e370bb5adb23480da6327
danielTari <daniel@dhis2.org>
2024-08-29 17:07:35 +0200

    [ANDROSDK-1912] migrate to kotlin

--- core/src/main/java/org/hisp/dhis/android/core/arch/api/payload/internal/Pager.kt
+++ core/src/main/java/org/hisp/dhis/android/core/arch/api/payload/internal/Pager.kt
@@ -25,42 +25,14 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.hisp.dhis.android.core.arch.api.payload.internal
 
-package org.hisp.dhis.android.core.arch.api.payload.internal;
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class Pager {
-    private final int page;
-    private final int pageCount;
-    private final int pageSize;
-    private final int total;
-
-    @JsonCreator
-    public Pager(@JsonProperty("page") int page,
-                 @JsonProperty("pageCount") int pageCount,
-                 @JsonProperty("pageSize") int pageSize,
-                 @JsonProperty("total") int total) {
-        this.page = page;
-        this.pageCount = pageCount;
-        this.pageSize = pageSize;
-        this.total = total;
-    }
-
-    public int page() {
-        return page;
-    }
-
-    public int pageCount() {
-        return pageCount;
-    }
-
-    public int pageSize() {
-        return pageSize;
-    }
-
-    public int total() {
-        return total;
-    }
-}
+data class Pager @JsonCreator constructor(
+    @JsonProperty("page") val page: Int,
+    @JsonProperty("pageCount") val pageCount: Int,
+    @JsonProperty("pageSize") val pageSize: Int,
+    @JsonProperty("total") val total: Int,
+)

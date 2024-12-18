abd0b571ac224474ef8ea15c87593e6b495fff1f
Marcos Campos <marcos.campos@dhis2.org>
2024-12-18 07:47:26 +0100

    [ANDROSDK-1964] Move ConstantFields to network package

--- core/src/main/java/org/hisp/dhis/android/network/constant/ConstantFields.kt
+++ core/src/main/java/org/hisp/dhis/android/network/constant/ConstantFields.kt
@@ -25,7 +25,7 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.android.core.constant.internal
+package org.hisp.dhis.android.network.constant
 
 import org.hisp.dhis.android.core.arch.api.fields.internal.BaseFields
 import org.hisp.dhis.android.core.arch.api.fields.internal.Fields

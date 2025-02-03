2764a71c6cadd9ce4e558337bbe0133319e96f9e
danielTari <daniel@dhis2.org>
2025-02-03 14:11:56 +0100

    [ANDROSDK-2001] create network handler and dtos

---
+++ core/src/main/java/org/hisp/dhis/android/network/relationshiptype/RelationshipConstraintDTO.kt
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (c) 2004-2025, University of Oslo
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *  Neither the name of the HISP project nor the names of its contributors may
+ *  be used to endorse or promote products derived from this software without
+ *  specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.android.network.relationshiptype
+
+import kotlinx.serialization.Serializable
+import org.hisp.dhis.android.core.relationship.RelationshipConstraint
+import org.hisp.dhis.android.core.relationship.RelationshipConstraintType
+import org.hisp.dhis.android.core.relationship.RelationshipEntityType
+import org.hisp.dhis.android.network.common.dto.ObjectWithUidDTO
+
+@Serializable
+internal data class RelationshipConstraintDTO(
+    val relationshipType: ObjectWithUidDTO?,
+    val constraintType: String?,
+    val relationshipEntity: String?,
+    val trackedEntityType: ObjectWithUidDTO?,
+    val program: ObjectWithUidDTO?,
+    val programStage: ObjectWithUidDTO?,
+    val trackerDataView: TrackerDataViewDTO?,
+) {
+    fun toDomain(): RelationshipConstraint {
+        return RelationshipConstraint.builder()
+            .relationshipType(relationshipType?.toDomain())
+            .constraintType(constraintType?.let { RelationshipConstraintType.valueOf(it) })
+            .relationshipEntity(relationshipEntity?.let { RelationshipEntityType.valueOf(it) })
+            .trackedEntityType(trackedEntityType?.toDomain())
+            .program(program?.toDomain())
+            .programStage(programStage?.toDomain())
+            .trackerDataView(trackerDataView?.toDomain())
+            .build()
+    }
+}

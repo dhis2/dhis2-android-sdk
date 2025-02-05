eeebe104368e56a85b38d9efc8a208acae0e7fbf
Marcos Campos <marcos.campos@dhis2.org>
2025-02-05 10:03:40 +0100

    [ANDROSDK-2003] Remove @singleton for SystemInfoService

--- core/src/main/java/org/hisp/dhis/android/network/systeminfo/SystemInfoService.kt
+++ core/src/main/java/org/hisp/dhis/android/network/systeminfo/SystemInfoService.kt
@@ -30,9 +30,7 @@ package org.hisp.dhis.android.network.systeminfo
 import org.hisp.dhis.android.core.systeminfo.SystemInfo
 import org.hisp.dhis.android.network.common.HttpServiceClientKotlinx
 import org.hisp.dhis.android.network.common.fields.Fields
-import org.koin.core.annotation.Singleton
 
-@Singleton
 internal class SystemInfoService(private val client: HttpServiceClientKotlinx) {
     suspend fun getSystemInfo(fields: Fields<SystemInfo>): SystemInfoDTO {
         return client.get {

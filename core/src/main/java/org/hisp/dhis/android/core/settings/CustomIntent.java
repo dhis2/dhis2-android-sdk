d8c6a8e1b260d82b6e6576b24c422c12aead6208
danielTari <daniel@dhis2.org>
2025-02-28 12:53:24 +0100

    [ANDROSDK-1934] add domain models

---
+++ core/src/main/java/org/hisp/dhis/android/core/settings/CustomIntent.java
@@ -0,0 +1,109 @@
+/*
+ *  Copyright (c) 2004-2025, University of Oslo
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *  Neither the name of the HISP project nor the names of its contributors may
+ *  be used to endorse or promote products derived from this software without
+ *  specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ *  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.android.core.settings;
+
+import android.database.Cursor;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.gabrielittner.auto.value.cursor.ColumnAdapter;
+import com.google.auto.value.AutoValue;
+
+import org.hisp.dhis.android.core.common.CoreObject;
+import org.hisp.dhis.android.core.common.ObjectWithUidInterface;
+import org.hisp.dhis.android.core.settings.internal.CustomIntentActionColumnAdapter;
+import org.hisp.dhis.android.core.settings.internal.CustomIntentRequestColumnAdapter;
+import org.hisp.dhis.android.core.settings.internal.CustomIntentResponseColumnAdapter;
+import org.hisp.dhis.android.core.settings.internal.IgnoreCustomIntentTriggerColumnAdapter;
+
+import java.util.List;
+
+@AutoValue
+public abstract class CustomIntent implements ObjectWithUidInterface, CoreObject {
+
+    @Override
+    @NonNull
+    public abstract String uid();
+
+    @Nullable
+    public abstract String name();
+
+    @Nullable
+    @ColumnAdapter(IgnoreCustomIntentTriggerColumnAdapter.class)
+    public abstract CustomIntentTrigger trigger();
+
+    @Nullable
+    @ColumnAdapter(CustomIntentActionColumnAdapter.class)
+    public abstract List<CustomIntentActionType> action();
+
+    @Nullable
+    public abstract String packageName();
+
+    @Nullable
+    @ColumnAdapter(CustomIntentRequestColumnAdapter.class)
+    public abstract CustomIntentRequest request();
+
+    @Nullable
+    @ColumnAdapter(CustomIntentResponseColumnAdapter.class)
+    public abstract CustomIntentResponse response();
+
+    public static CustomIntent create(Cursor cursor) {
+        return AutoValue_CustomIntent.createFromCursor(cursor);
+    }
+
+    public abstract Builder toBuilder();
+
+
+    public static Builder builder() {
+        return new AutoValue_CustomIntent.Builder();
+    }
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+        public abstract Builder id(Long id);
+
+        public abstract Builder uid(String intentUid);
+
+        public abstract Builder name(String name);
+
+        public abstract Builder trigger(CustomIntentTrigger trigger);
+
+        public abstract Builder action(List<CustomIntentActionType> action);
+
+        public abstract Builder packageName(String packageName);
+
+        public abstract Builder request(CustomIntentRequest request);
+
+        public abstract Builder response(CustomIntentResponse response);
+
+        public abstract CustomIntent build();
+    }
+
+}

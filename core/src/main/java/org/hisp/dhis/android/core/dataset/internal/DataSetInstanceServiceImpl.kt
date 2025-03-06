283563cdeb16f96ffa4e32b277b7e5967418bfe8
danielTari <daniel@dhis2.org>
2025-03-06 16:37:23 +0100

    [ANDROSDK-2042] suppress magic number

--- core/src/main/java/org/hisp/dhis/android/core/dataset/internal/DataSetInstanceServiceImpl.kt
+++ core/src/main/java/org/hisp/dhis/android/core/dataset/internal/DataSetInstanceServiceImpl.kt
@@ -174,6 +174,7 @@ internal class DataSetInstanceServiceImpl(
         } ?: false
     }
 
+    @Suppress("MagicNumber")
     override fun getMissingMandatoryFieldsCombination(
         dataSetUid: String,
         periodId: String,

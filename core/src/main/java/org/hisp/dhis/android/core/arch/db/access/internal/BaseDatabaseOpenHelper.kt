4927f71c1c1142ae3016ae55756cdb09c7febab0
Marcos Campos <marcos.campos@dhis2.org>
2025-04-09 11:23:18 +0200

    [ANDROSDK-2067] Migration fix for Aggregated Sync State Bug

--- core/src/main/java/org/hisp/dhis/android/core/arch/db/access/internal/BaseDatabaseOpenHelper.kt
+++ core/src/main/java/org/hisp/dhis/android/core/arch/db/access/internal/BaseDatabaseOpenHelper.kt
@@ -59,6 +59,6 @@ internal class BaseDatabaseOpenHelper(context: Context, targetVersion: Int) {
     }
 
     companion object {
-        const val VERSION = 170
+        const val VERSION = 171
     }
 }

515af8176c32d41bc46168e49a67dae22591000e
danielTari <daniel@dhis2.org>
2025-05-09 14:27:29 +0200

    [ANDROSDK-2079] app mappers to UserGroupDB

--- core/src/main/java/org/hisp/dhis/android/persistence/user/UserGroupDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/user/UserGroupDB.kt
@@ -4,6 +4,10 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.user.Authority
+import org.hisp.dhis.android.core.user.UserGroup
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.core.util.toJavaDate
 
 @Entity(
     tableName = "UserGroup",
@@ -21,4 +25,28 @@ internal data class UserGroupDB(
     val displayName: String?,
     val created: String?,
     val lastUpdated: String?,
-)
+) {
+    fun toDomain(): UserGroup {
+        return UserGroup.builder()
+            .uid(uid)
+            .code(code)
+            .name(name)
+            .displayName(displayName)
+            .created(created.toJavaDate())
+            .lastUpdated(lastUpdated.toJavaDate())
+            .build()
+    }
+}
+
+internal fun UserGroup.toDB(): UserGroupDB {
+    return UserGroupDB(
+        uid = uid(),
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created().dateFormat(),
+        lastUpdated = lastUpdated().dateFormat(),
+    )
+}
+
+

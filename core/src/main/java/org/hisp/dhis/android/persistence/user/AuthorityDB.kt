312f8608c7a582cc6c54e31530e9a306c763c6dc
danielTari <daniel@dhis2.org>
2025-05-09 14:27:53 +0200

    [ANDROSDK-2079] app mappers to AuthorityDB

--- core/src/main/java/org/hisp/dhis/android/persistence/user/AuthorityDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/user/AuthorityDB.kt
@@ -3,6 +3,8 @@ package org.hisp.dhis.android.persistence.user
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.user.AuthenticatedUser
+import org.hisp.dhis.android.core.user.Authority
 
 @Entity(tableName = "Authority")
 internal data class AuthorityDB(
@@ -10,4 +12,17 @@ internal data class AuthorityDB(
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
     val name: String?,
-)
+) {
+    fun toDomain(): Authority {
+        return Authority.builder()
+            .name(name)
+            .build()
+    }
+}
+
+internal fun Authority.toDB(): AuthorityDB {
+    return AuthorityDB(
+        name = name(),
+    )
+}
+

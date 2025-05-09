95286fc4915ae220177fdc448ba7a14b791d9502
danielTari <daniel@dhis2.org>
2025-05-09 11:58:14 +0200

    [ANDROSDK-2079] app mappers to TrackerVisualizationDB

--- core/src/main/java/org/hisp/dhis/android/persistence/visualization/TrackerVisualizationDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/visualization/TrackerVisualizationDB.kt
@@ -5,6 +5,12 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.common.ObjectWithUid
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.core.util.toJavaDate
+import org.hisp.dhis.android.core.visualization.TrackerVisualization
+import org.hisp.dhis.android.core.visualization.TrackerVisualizationOutputType
+import org.hisp.dhis.android.core.visualization.TrackerVisualizationType
 import org.hisp.dhis.android.persistence.program.ProgramDB
 import org.hisp.dhis.android.persistence.program.ProgramStageDB
 import org.hisp.dhis.android.persistence.trackedentity.TrackedEntityTypeDB
@@ -58,4 +64,41 @@ internal data class TrackerVisualizationDB(
     val program: String?,
     val programStage: String?,
     val trackedEntityType: String?,
-)
+) {
+    fun toDomain(): TrackerVisualization {
+        return TrackerVisualization.builder()
+            .uid(uid)
+            .code(code)
+            .name(name)
+            .displayName(displayName)
+            .created(created.toJavaDate())
+            .lastUpdated(lastUpdated.toJavaDate())
+            .description(description)
+            .displayDescription(displayDescription)
+            .type(type?.let { TrackerVisualizationType.valueOf(it) })
+            .outputType(outputType?.let { TrackerVisualizationOutputType.valueOf(it) })
+            .program(ObjectWithUid.create(program))
+            .programStage(ObjectWithUid.create(programStage))
+            .trackedEntityType(ObjectWithUid.create(trackedEntityType))
+            .build()
+    }
+}
+
+internal fun TrackerVisualization.toDB(): TrackerVisualizationDB {
+    return TrackerVisualizationDB(
+        uid = this.uid(),
+        code = this.code(),
+        name = this.name(),
+        displayName = this.displayName(),
+        created = this.created().dateFormat(),
+        lastUpdated = this.lastUpdated().dateFormat(),
+        description = this.description(),
+        displayDescription = this.displayDescription(),
+        type = this.type()?.name,
+        outputType = this.outputType()?.name,
+        program = this.program()?.uid(),
+        programStage = this.programStage()?.uid(),
+        trackedEntityType = this.trackedEntityType()?.uid(),
+    )
+}
+

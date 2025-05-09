d8bc9f79631058f9a037eed1ed0894f4421f5e3e
danielTari <daniel@dhis2.org>
2025-05-09 11:58:43 +0200

    [ANDROSDK-2079] app mappers to VisualizationDB

--- core/src/main/java/org/hisp/dhis/android/persistence/visualization/VisualizationDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/visualization/VisualizationDB.kt
@@ -4,6 +4,18 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.common.AggregationType
+import org.hisp.dhis.android.core.common.ObjectWithUid
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.core.util.toJavaDate
+import org.hisp.dhis.android.core.visualization.DigitGroupSeparator
+import org.hisp.dhis.android.core.visualization.DisplayDensity
+import org.hisp.dhis.android.core.visualization.HideEmptyItemStrategy
+import org.hisp.dhis.android.core.visualization.LegendStrategy
+import org.hisp.dhis.android.core.visualization.LegendStyle
+import org.hisp.dhis.android.core.visualization.Visualization
+import org.hisp.dhis.android.core.visualization.VisualizationLegend
+import org.hisp.dhis.android.core.visualization.VisualizationType
 
 @Entity(
     tableName = "Visualization",
@@ -29,21 +41,21 @@ internal data class VisualizationDB(
     val subtitle: String?,
     val displaySubtitle: String?,
     val type: String?,
-    val hideTitle: Int?,
-    val hideSubtitle: Int?,
-    val hideEmptyColumns: Int?,
-    val hideEmptyRows: Int?,
+    val hideTitle: Boolean?,
+    val hideSubtitle: Boolean?,
+    val hideEmptyColumns: Boolean?,
+    val hideEmptyRows: Boolean?,
     val hideEmptyRowItems: String?,
-    val hideLegend: Int?,
-    val showHierarchy: Int?,
-    val rowTotals: Int?,
-    val rowSubTotals: Int?,
-    val colTotals: Int?,
-    val colSubTotals: Int?,
-    val showDimensionLabels: Int?,
-    val percentStackedValues: Int?,
-    val noSpaceBetweenColumns: Int?,
-    val skipRounding: Int?,
+    val hideLegend: Boolean?,
+    val showHierarchy: Boolean?,
+    val rowTotals: Boolean?,
+    val rowSubTotals: Boolean?,
+    val colTotals: Boolean?,
+    val colSubTotals: Boolean?,
+    val showDimensionLabels: Boolean?,
+    val percentStackedValues: Boolean?,
+    val noSpaceBetweenColumns: Boolean?,
+    val skipRounding: Boolean?,
     val displayDensity: String?,
     val digitGroupSeparator: String?,
     val legendShowKey: String?,
@@ -51,4 +63,92 @@ internal data class VisualizationDB(
     val legendSetId: String?,
     val legendStrategy: String?,
     val aggregationType: String?,
-)
+) {
+    fun toDomain(): Visualization {
+        return Visualization.builder()
+            .uid(uid)
+            .code(code)
+            .name(name)
+            .displayName(displayName)
+            .created(created?.toJavaDate())
+            .lastUpdated(lastUpdated?.toJavaDate())
+            .description(description)
+            .displayDescription(displayDescription)
+            .displayFormName(displayFormName)
+            .title(title)
+            .displayTitle(displayTitle)
+            .subtitle(subtitle)
+            .displaySubtitle(displaySubtitle)
+            .type(type?.let { VisualizationType.valueOf(it) })
+            .hideTitle(hideTitle)
+            .hideSubtitle(hideSubtitle)
+            .hideEmptyColumns(hideEmptyColumns)
+            .hideEmptyRows(hideEmptyRows)
+            .hideEmptyRowItems(hideEmptyRowItems?.let { HideEmptyItemStrategy.valueOf(it) })
+            .hideLegend(hideLegend)
+            .showHierarchy(showHierarchy)
+            .rowTotals(rowTotals)
+            .rowSubTotals(rowSubTotals)
+            .colTotals(colTotals)
+            .colSubTotals(colSubTotals)
+            .showDimensionLabels(showDimensionLabels)
+            .percentStackedValues(percentStackedValues)
+            .noSpaceBetweenColumns(noSpaceBetweenColumns)
+            .skipRounding(skipRounding)
+            .legend(
+                if (sequenceOf(legendShowKey, legendStyle, legendSetId, legendStrategy).any { it != null }) {
+                    VisualizationLegend.builder()
+                        .showKey(legendShowKey == "1")
+                        .style(legendStyle?.let { LegendStyle.valueOf(it) })
+                        .set(legendSetId?.let { ObjectWithUid.create(it) })
+                        .strategy(legendStrategy?.let { LegendStrategy.valueOf(it) })
+                        .build()
+                } else null
+            )
+            .displayDensity(displayDensity?.let { DisplayDensity.valueOf(it) })
+            .digitGroupSeparator(digitGroupSeparator?.let { DigitGroupSeparator.valueOf(it) })
+            .aggregationType(aggregationType?.let { AggregationType.valueOf(it) })
+            .build()
+    }
+}
+
+internal fun Visualization.toDB(): VisualizationDB {
+    return VisualizationDB(
+        uid = uid()!!,
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created()?.dateFormat(),
+        lastUpdated = lastUpdated()?.dateFormat(),
+        description = description(),
+        displayDescription = displayDescription(),
+        displayFormName = displayFormName(),
+        title = title(),
+        displayTitle = displayTitle(),
+        subtitle = subtitle(),
+        displaySubtitle = displaySubtitle(),
+        type = type()?.name,
+        hideTitle = hideTitle(),
+        hideSubtitle = hideSubtitle(),
+        hideEmptyColumns = hideEmptyColumns(),
+        hideEmptyRows = hideEmptyRows(),
+        hideEmptyRowItems = hideEmptyRowItems()?.name,
+        hideLegend = hideLegend(),
+        showHierarchy = showHierarchy(),
+        rowTotals = rowTotals(),
+        rowSubTotals = rowSubTotals(),
+        colTotals = colTotals(),
+        colSubTotals = colSubTotals(),
+        showDimensionLabels = showDimensionLabels(),
+        percentStackedValues = percentStackedValues(),
+        noSpaceBetweenColumns = noSpaceBetweenColumns(),
+        skipRounding = skipRounding(),
+        displayDensity = displayDensity()?.name,
+        digitGroupSeparator = digitGroupSeparator()?.name,
+        legendShowKey = legend()?.showKey()?.let { if (it) "1" else "0" },
+        legendStyle = legend()?.style()?.name,
+        legendSetId = legend()?.set()?.uid(),
+        legendStrategy = legend()?.strategy()?.name,
+        aggregationType = aggregationType()?.name,
+    )
+}

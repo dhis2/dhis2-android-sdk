59d7d7ed00556a5c80ac9365007faa23ba1d343e
Marcos Campos <marcos.campos@dhis2.org>
2025-05-09 09:17:37 +0200

    [ANDROSDK-2078] Add mappers to CategoryDB

--- core/src/main/java/org/hisp/dhis/android/persistence/category/CategoryDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/category/CategoryDB.kt
@@ -4,6 +4,11 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.category.Category
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.persistence.common.BaseIdentifiableObjectDB
+import org.hisp.dhis.android.persistence.common.EntityDB
+import org.hisp.dhis.android.persistence.common.applyBaseIdentifiableFields
 
 @Entity(
     tableName = "Category",
@@ -15,11 +20,33 @@ internal data class CategoryDB(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
-    val uid: String,
-    val code: String?,
-    val name: String?,
-    val displayName: String?,
-    val created: String?,
-    val lastUpdated: String?,
+    override val uid: String,
+    override val code: String?,
+    override val name: String?,
+    override val displayName: String?,
+    override val created: String?,
+    override val lastUpdated: String?,
     val dataDimensionType: String?,
-)
+) : EntityDB<Category>, BaseIdentifiableObjectDB {
+
+    override fun toDomain(): Category {
+        return Category.builder()
+            .applyBaseIdentifiableFields(this)
+            .dataDimensionType(dataDimensionType)
+            .build()
+    }
+
+    companion object {
+        fun Category.toDB(): CategoryDB {
+            return CategoryDB(
+                uid = uid(),
+                code = code(),
+                name = name(),
+                displayName = displayName(),
+                created = created().dateFormat(),
+                lastUpdated = lastUpdated().dateFormat(),
+                dataDimensionType = dataDimensionType(),
+            )
+        }
+    }
+}

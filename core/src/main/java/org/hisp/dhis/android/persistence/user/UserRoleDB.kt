be40c58d12f770cc8b23f3322d99d9548d3e1086
danielTari <daniel@dhis2.org>
2025-05-09 14:27:16 +0200

    [ANDROSDK-2079] app mappers to UserRoleDB

--- core/src/main/java/org/hisp/dhis/android/persistence/user/UserRoleDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/user/UserRoleDB.kt
@@ -4,6 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.user.UserRole
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.core.util.toJavaDate
 
 @Entity(
     tableName = "UserRole",
@@ -21,4 +24,27 @@ internal data class UserRoleDB(
     val displayName: String?,
     val created: String?,
     val lastUpdated: String?,
-)
+) {
+    fun toDomain(): UserRole {
+        return UserRole.builder()
+            .uid(uid)
+            .code(code)
+            .name(name)
+            .displayName(displayName)
+            .created(created.toJavaDate())
+            .lastUpdated(lastUpdated.toJavaDate())
+            .build()
+    }
+}
+
+internal fun UserRole.toDB(): UserRoleDB {
+    return UserRoleDB(
+        uid = uid(),
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created().dateFormat(),
+        lastUpdated = lastUpdated().dateFormat(),
+
+        )
+}

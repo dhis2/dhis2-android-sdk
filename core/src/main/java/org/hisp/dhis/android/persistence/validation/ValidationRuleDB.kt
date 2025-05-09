b89b9b5e8f8508798d3748ebffd69e31fcf4c80f
danielTari <daniel@dhis2.org>
2025-05-09 11:58:35 +0200

    [ANDROSDK-2079] app mappers to ValidationRuleDB

--- core/src/main/java/org/hisp/dhis/android/persistence/validation/ValidationRuleDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/validation/ValidationRuleDB.kt
@@ -4,6 +4,17 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
+import org.hisp.dhis.android.core.arch.json.internal.KotlinxJsonParser
+import org.hisp.dhis.android.core.period.PeriodType
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.core.util.toJavaDate
+import org.hisp.dhis.android.core.validation.MissingValueStrategy
+import org.hisp.dhis.android.core.validation.ValidationRule
+import org.hisp.dhis.android.core.validation.ValidationRuleExpression
+import org.hisp.dhis.android.core.validation.ValidationRuleImportance
+import org.hisp.dhis.android.core.validation.ValidationRuleOperator
 
 @Entity(
     tableName = "ValidationRule",
@@ -29,7 +40,7 @@ internal data class ValidationRuleDB(
     val importance: String?,
     val operator: String?,
     val periodType: String?,
-    val skipFormValidation: Int?,
+    val skipFormValidation: Boolean?,
     val leftSideExpression: String?,
     val leftSideDescription: String?,
     val leftSideMissingValueStrategy: String?,
@@ -37,4 +48,73 @@ internal data class ValidationRuleDB(
     val rightSideDescription: String?,
     val rightSideMissingValueStrategy: String?,
     val organisationUnitLevels: String?,
-)
+) {
+    fun toDomain(): ValidationRule {
+        return ValidationRule.builder()
+            .uid(uid)
+            .code(code)
+            .name(name)
+            .displayName(displayName)
+            .created(created?.toJavaDate())
+            .lastUpdated(lastUpdated?.toJavaDate())
+            .shortName(shortName)
+            .displayShortName(displayShortName)
+            .description(description)
+            .displayDescription(displayDescription)
+            .instruction(instruction)
+            .importance(importance?.let { ValidationRuleImportance.valueOf(it) })
+            .operator(operator?.let { ValidationRuleOperator.valueOf(it) })
+            .periodType(periodType?.let { PeriodType.valueOf(it) })
+            .skipFormValidation(skipFormValidation)
+            .leftSide(
+                ValidationRuleExpression.builder()
+                    .expression(leftSideExpression)
+                    .description(leftSideDescription)
+                    .missingValueStrategy(leftSideMissingValueStrategy?.let { MissingValueStrategy.valueOf(it) })
+                    .build()
+            )
+            .rightSide(
+                ValidationRuleExpression.builder()
+                    .expression(rightSideExpression)
+                    .description(rightSideDescription)
+                    .missingValueStrategy(rightSideMissingValueStrategy?.let { MissingValueStrategy.valueOf(it) })
+                    .build()
+            )
+            .organisationUnitLevels(
+                organisationUnitLevels?.let {
+                    KotlinxJsonParser.instance.decodeFromString(ListSerializer(Int.serializer()), it)
+                } ?: emptyList()
+            )
+            .build()
+    }
+}
+
+internal fun ValidationRule.toDB(): ValidationRuleDB {
+    return ValidationRuleDB(
+        uid = uid()!!,
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created()?.dateFormat(),
+        lastUpdated = lastUpdated()?.dateFormat(),
+        shortName = shortName(),
+        displayShortName = displayShortName(),
+        description = description(),
+        displayDescription = displayDescription(),
+        instruction = instruction(),
+        importance = importance()?.name,
+        operator = operator()?.name,
+        periodType = periodType()?.name,
+        skipFormValidation = skipFormValidation(),
+        leftSideExpression = leftSide().expression(),
+        leftSideDescription = leftSide().description(),
+        leftSideMissingValueStrategy = leftSide().missingValueStrategy()?.name,
+        rightSideExpression = rightSide().expression(),
+        rightSideDescription = rightSide().description(),
+        rightSideMissingValueStrategy = rightSide().missingValueStrategy()?.name,
+        organisationUnitLevels = KotlinxJsonParser.instance.encodeToString(
+            ListSerializer(Int.serializer()),
+            organisationUnitLevels() ?: emptyList()
+        )
+    )
+}

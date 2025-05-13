231f422aa8ad88dd4e841c78aa0932142a55d23e
danielTari <daniel@dhis2.org>
2025-05-13 12:06:22 +0200

    [ANDROSDK-2089] add toDomain and toDB mappers to ProgramStageWorkingListDB

--- core/src/main/java/org/hisp/dhis/android/persistence/programstageworkinglist/ProgramStageWorkingListDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/programstageworkinglist/ProgramStageWorkingListDB.kt
@@ -5,6 +5,20 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.common.AssignedUserMode
+import org.hisp.dhis.android.core.enrollment.EnrollmentStatus
+import org.hisp.dhis.android.core.event.EventStatus
+import org.hisp.dhis.android.core.organisationunit.OrganisationUnitMode
+import org.hisp.dhis.android.core.programstageworkinglist.ProgramStageQueryCriteria
+import org.hisp.dhis.android.core.programstageworkinglist.ProgramStageWorkingList
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.persistence.common.BaseIdentifiableObjectDB
+import org.hisp.dhis.android.persistence.common.DateFilterPeriodDB
+import org.hisp.dhis.android.persistence.common.EntityDB
+import org.hisp.dhis.android.persistence.common.ObjectWithUidDB
+import org.hisp.dhis.android.persistence.common.StringListDB
+import org.hisp.dhis.android.persistence.common.applyBaseIdentifiableFields
+import org.hisp.dhis.android.persistence.common.toDB
 import org.hisp.dhis.android.persistence.organisationunit.OrganisationUnitDB
 import org.hisp.dhis.android.persistence.program.ProgramDB
 import org.hisp.dhis.android.persistence.program.ProgramStageDB
@@ -45,25 +59,77 @@ internal data class ProgramStageWorkingListDB(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
-    val uid: String,
-    val code: String?,
-    val name: String?,
-    val displayName: String?,
-    val created: String?,
-    val lastUpdated: String?,
+    override val uid: String,
+    override val code: String?,
+    override val name: String?,
+    override val displayName: String?,
+    override val created: String?,
+    override val lastUpdated: String?,
     val description: String?,
     val program: String,
     val programStage: String,
     val eventStatus: String?,
-    val eventCreatedAt: String?,
-    val eventOccurredAt: String?,
-    val eventScheduledAt: String?,
+    val eventCreatedAt: DateFilterPeriodDB?,
+    val eventOccurredAt: DateFilterPeriodDB?,
+    val eventScheduledAt: DateFilterPeriodDB?,
     val enrollmentStatus: String?,
-    val enrolledAt: String?,
-    val enrollmentOccurredAt: String?,
+    val enrolledAt: DateFilterPeriodDB?,
+    val enrollmentOccurredAt: DateFilterPeriodDB?,
     val orderProperty: String?,
-    val displayColumnOrder: String?,
+    val displayColumnOrder: StringListDB?,
     val orgUnit: String?,
     val ouMode: String?,
     val assignedUserMode: String?,
-)
+) : EntityDB<ProgramStageWorkingList>, BaseIdentifiableObjectDB {
+
+    override fun toDomain(): ProgramStageWorkingList {
+        return ProgramStageWorkingList.builder()
+            .applyBaseIdentifiableFields(this)
+            .description(description)
+            .program(ObjectWithUidDB(program).toDomain())
+            .programStage(ObjectWithUidDB(programStage).toDomain())
+            .programStageQueryCriteria(
+                ProgramStageQueryCriteria.builder()
+                    .eventStatus(eventStatus?.let { EventStatus.valueOf(it) })
+                    .eventCreatedAt(eventCreatedAt?.toDomain())
+                    .eventOccurredAt(eventOccurredAt?.toDomain())
+                    .eventScheduledAt(eventScheduledAt?.toDomain())
+                    .enrollmentStatus(enrollmentStatus?.let { EnrollmentStatus.valueOf(it) })
+                    .enrolledAt(enrolledAt?.toDomain())
+                    .enrollmentOccurredAt(enrollmentOccurredAt?.toDomain())
+                    .order(orderProperty)
+                    .displayColumnOrder(displayColumnOrder?.toDomain())
+                    .orgUnit(orgUnit)
+                    .ouMode(ouMode?.let { OrganisationUnitMode.valueOf(it) })
+                    .assignedUserMode(assignedUserMode?.let { AssignedUserMode.valueOf(it) })
+                    .build()
+            )
+            .build()
+    }
+}
+
+internal fun ProgramStageWorkingList.toDB(): ProgramStageWorkingListDB {
+    return ProgramStageWorkingListDB(
+        uid = uid()!!,
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created()?.dateFormat(),
+        lastUpdated = lastUpdated()?.dateFormat(),
+        description = description(),
+        program = program().uid(),
+        programStage = programStage().uid(),
+        eventStatus = programStageQueryCriteria()?.eventStatus()?.name,
+        eventCreatedAt = programStageQueryCriteria()?.eventCreatedAt()?.toDB(),
+        eventOccurredAt = programStageQueryCriteria()?.eventOccurredAt()?.toDB(),
+        eventScheduledAt = programStageQueryCriteria()?.eventScheduledAt()?.toDB(),
+        enrollmentStatus = programStageQueryCriteria()?.enrollmentStatus()?.name,
+        enrolledAt = programStageQueryCriteria()?.enrolledAt()?.toDB(),
+        enrollmentOccurredAt = programStageQueryCriteria()?.enrollmentOccurredAt()?.toDB(),
+        orderProperty = programStageQueryCriteria()?.order(),
+        displayColumnOrder = programStageQueryCriteria()?.displayColumnOrder()?.toDB(),
+        orgUnit = programStageQueryCriteria()?.orgUnit(),
+        ouMode = programStageQueryCriteria()?.ouMode()?.name,
+        assignedUserMode = programStageQueryCriteria()?.assignedUserMode()?.name,
+    )
+}

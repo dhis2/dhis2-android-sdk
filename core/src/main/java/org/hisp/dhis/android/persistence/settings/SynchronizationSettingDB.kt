0476c466440adf3b7c47aba9540f182ce35243c8
danielTari <daniel@dhis2.org>
2025-05-13 17:22:36 +0200

    [ANDROSDK-2091] add mapping to entity SynchronizationSettingDB

--- core/src/main/java/org/hisp/dhis/android/persistence/settings/SynchronizationSettingDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/settings/SynchronizationSettingDB.kt
@@ -3,6 +3,12 @@ package org.hisp.dhis.android.persistence.settings
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.settings.DataSyncPeriod
+import org.hisp.dhis.android.core.settings.MetadataSyncPeriod
+import org.hisp.dhis.android.core.settings.SynchronizationSettings
+import org.hisp.dhis.android.core.tracker.TrackerExporterVersion
+import org.hisp.dhis.android.core.tracker.TrackerImporterVersion
+import org.hisp.dhis.android.persistence.common.EntityDB
 
 @Entity(tableName = "SynchronizationSetting")
 internal data class SynchronizationSettingDB(
@@ -14,4 +20,26 @@ internal data class SynchronizationSettingDB(
     val trackerImporterVersion: String?,
     val trackerExporterVersion: String?,
     val fileMaxLengthBytes: Int?,
-)
+) : EntityDB<SynchronizationSettings> {
+
+    override fun toDomain(): SynchronizationSettings {
+        return SynchronizationSettings.builder()
+            .id(id?.toLong())
+            .dataSync(dataSync?.let { DataSyncPeriod.valueOf(it) })
+            .metadataSync(metadataSync?.let { MetadataSyncPeriod.valueOf(it) })
+            .trackerImporterVersion(trackerImporterVersion?.let { TrackerImporterVersion.valueOf(it) })
+            .trackerExporterVersion(trackerExporterVersion?.let { TrackerExporterVersion.valueOf(it) })
+            .fileMaxLengthBytes(fileMaxLengthBytes)
+            .build()
+    }
+}
+
+internal fun SynchronizationSettings.toDB(): SynchronizationSettingDB {
+    return SynchronizationSettingDB(
+        dataSync = dataSync()?.name,
+        metadataSync = metadataSync()?.name,
+        trackerImporterVersion = trackerImporterVersion()?.name,
+        trackerExporterVersion = trackerExporterVersion()?.name,
+        fileMaxLengthBytes = fileMaxLengthBytes(),
+    )
+}

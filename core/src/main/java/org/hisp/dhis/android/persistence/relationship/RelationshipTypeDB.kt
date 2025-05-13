03b5592d5e4a046448b4a9810b2da9e89fc15684
danielTari <daniel@dhis2.org>
2025-05-13 13:13:30 +0200

    [ANDROSDK-2089] fix access

--- core/src/main/java/org/hisp/dhis/android/persistence/relationship/RelationshipTypeDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/relationship/RelationshipTypeDB.kt
@@ -42,7 +42,7 @@ internal data class RelationshipTypeDB(
                 fromToName(fromToName)
                 toFromName(toFromName)
                 bidirectional(bidirectional)
-                accessDataWrite?.toDomain()
+                accessDataWrite?.let { access(it.toDomain()) }
             }.build()
     }
 }

6b3bfe80dad051669665e3fca1729bf7f1aecd71
danielTari <daniel@dhis2.org>
2025-05-13 17:23:01 +0200

    [ANDROSDK-2091] add mapping to entity UserSettingsDB and SystemSettingDB

--- core/src/main/java/org/hisp/dhis/android/persistence/settings/UserSettingsDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/settings/UserSettingsDB.kt
@@ -3,6 +3,8 @@ package org.hisp.dhis.android.persistence.settings
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.settings.UserSettings
+import org.hisp.dhis.android.persistence.common.EntityDB
 
 @Entity(tableName = "UserSettings")
 internal data class UserSettingsDB(
@@ -11,4 +13,20 @@ internal data class UserSettingsDB(
     val id: Int? = 0,
     val keyUiLocale: String?,
     val keyDbLocale: String?,
-)
+) : EntityDB<UserSettings> {
+
+    override fun toDomain(): UserSettings {
+        return UserSettings.builder()
+            .id(id?.toLong())
+            .keyUiLocale(keyUiLocale)
+            .keyDbLocale(keyDbLocale)
+            .build()
+    }
+}
+
+internal fun UserSettings.toDB(): UserSettingsDB {
+    return UserSettingsDB(
+        keyUiLocale = keyUiLocale(),
+        keyDbLocale = keyDbLocale(),
+    )
+}

689525eff9d5f2500ac89eaa7fcfcdc4324cd213
danielTari <daniel@dhis2.org>
2025-05-13 17:18:56 +0200

    [ANDROSDK-2091] add mappers to Entity ProgramConfigurationSettingDB

--- core/src/main/java/org/hisp/dhis/android/persistence/settings/ProgramConfigurationSettingDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/settings/ProgramConfigurationSettingDB.kt
@@ -3,6 +3,9 @@ package org.hisp.dhis.android.persistence.settings
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.settings.ProgramConfigurationSetting
+import org.hisp.dhis.android.core.settings.ProgramItemHeader
+import org.hisp.dhis.android.persistence.common.EntityDB
 
 @Entity(tableName = "ProgramConfigurationSetting")
 internal data class ProgramConfigurationSettingDB(
@@ -10,12 +13,48 @@ internal data class ProgramConfigurationSettingDB(
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
     val uid: String?,
-    val completionSpinner: Int?,
-    val optionalSearch: Int?,
-    val disableReferrals: Int?,
-    val disableCollapsibleSections: Int?,
+    val completionSpinner: Boolean?,
+    val optionalSearch: Boolean?,
+    val disableReferrals: Boolean?,
+    val disableCollapsibleSections: Boolean?,
     val itemHeaderProgramIndicator: String?,
     val minimumLocationAccuracy: Int?,
-    val disableManualLocation: Int?,
-    val quickActions: String?,
-)
+    val disableManualLocation: Boolean?,
+    val quickActions: QuickActionsDB?,
+) : EntityDB<ProgramConfigurationSetting> {
+
+    override fun toDomain(): ProgramConfigurationSetting {
+        return ProgramConfigurationSetting.builder()
+            .id(id?.toLong())
+            .uid(uid)
+            .completionSpinner(completionSpinner)
+            .optionalSearch(optionalSearch)
+            .disableReferrals(disableReferrals)
+            .disableCollapsibleSections(disableCollapsibleSections)
+            .itemHeader(
+                itemHeaderProgramIndicator?.let {
+                    ProgramItemHeader.builder()
+                        .programIndicator(it)
+                        .build()
+                }
+            )
+            .minimumLocationAccuracy(minimumLocationAccuracy)
+            .disableManualLocation(disableManualLocation)
+            .quickActions(quickActions?.toDomain())
+            .build()
+    }
+}
+
+internal fun ProgramConfigurationSetting.toDB(): ProgramConfigurationSettingDB {
+    return ProgramConfigurationSettingDB(
+        uid = uid(),
+        completionSpinner = completionSpinner(),
+        optionalSearch = optionalSearch(),
+        disableReferrals = disableReferrals(),
+        disableCollapsibleSections = disableCollapsibleSections(),
+        itemHeaderProgramIndicator = itemHeader()?.programIndicator(),
+        minimumLocationAccuracy = minimumLocationAccuracy(),
+        disableManualLocation = disableManualLocation(),
+        quickActions = quickActions()?.toDB(),
+    )
+}

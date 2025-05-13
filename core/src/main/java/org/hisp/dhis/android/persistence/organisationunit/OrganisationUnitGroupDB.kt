99984414fa53e8220a8a2a7f2f23e7097e349fe1
danielTari <daniel@dhis2.org>
2025-05-13 12:27:03 +0200

    [ANDROSDK-2089] add toDomain and toDB mappers to OrganisationUnitGroupDB

--- core/src/main/java/org/hisp/dhis/android/persistence/organisationunit/OrganisationUnitGroupDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/organisationunit/OrganisationUnitGroupDB.kt
@@ -4,6 +4,11 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.organisationunit.OrganisationUnitGroup
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.persistence.common.BaseIdentifiableObjectDB
+import org.hisp.dhis.android.persistence.common.EntityDB
+import org.hisp.dhis.android.persistence.common.applyBaseIdentifiableFields
 
 @Entity(
     tableName = "OrganisationUnitGroup",
@@ -15,12 +20,35 @@ internal data class OrganisationUnitGroupDB(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
-    val uid: String,
-    val code: String?,
-    val name: String?,
-    val displayName: String?,
-    val created: String?,
-    val lastUpdated: String?,
+    override val uid: String,
+    override val code: String?,
+    override val name: String?,
+    override val displayName: String?,
+    override val created: String?,
+    override val lastUpdated: String?,
     val shortName: String?,
     val displayShortName: String?,
-)
+) : EntityDB<OrganisationUnitGroup>, BaseIdentifiableObjectDB {
+
+    override fun toDomain(): OrganisationUnitGroup {
+        return OrganisationUnitGroup.builder()
+            .applyBaseIdentifiableFields(this)
+            .id(id?.toLong())
+            .shortName(shortName)
+            .displayShortName(displayShortName)
+            .build()
+    }
+}
+
+internal fun OrganisationUnitGroup.toDB(): OrganisationUnitGroupDB {
+    return OrganisationUnitGroupDB(
+        uid = uid()!!,
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created()?.dateFormat(),
+        lastUpdated = lastUpdated()?.dateFormat(),
+        shortName = shortName(),
+        displayShortName = displayShortName(),
+    )
+}

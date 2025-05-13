c3b4d8f2550b887e83f806594e7fac36c966b561
danielTari <daniel@dhis2.org>
2025-05-13 11:45:13 +0200

    [ANDROSDK-2089] add toDomain and toDB mappers to RelationshipTypeDB

--- core/src/main/java/org/hisp/dhis/android/persistence/relationship/RelationshipTypeDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/relationship/RelationshipTypeDB.kt
@@ -4,6 +4,13 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.relationship.RelationshipType
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.persistence.common.AccessDB
+import org.hisp.dhis.android.persistence.common.BaseIdentifiableObjectDB
+import org.hisp.dhis.android.persistence.common.EntityDB
+import org.hisp.dhis.android.persistence.common.applyBaseIdentifiableFields
+import org.hisp.dhis.android.persistence.common.toDB
 
 @Entity(
     tableName = "RelationshipType",
@@ -15,14 +22,42 @@ internal data class RelationshipTypeDB(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
-    val uid: String,
-    val code: String?,
-    val name: String?,
-    val displayName: String?,
-    val created: String?,
-    val lastUpdated: String?,
+    override val uid: String,
+    override val code: String?,
+    override val name: String?,
+    override val displayName: String?,
+    override val created: String?,
+    override val lastUpdated: String?,
     val fromToName: String?,
     val toFromName: String?,
-    val bidirectional: Int?,
-    val accessDataWrite: Int?,
-)
+    val bidirectional: Boolean?,
+    val accessDataWrite: Boolean?,
+) : EntityDB<RelationshipType>, BaseIdentifiableObjectDB {
+
+    override fun toDomain(): RelationshipType {
+        return RelationshipType.builder()
+            .apply {
+                applyBaseIdentifiableFields(this@RelationshipTypeDB)
+                id(id?.toLong())
+                fromToName(fromToName)
+                toFromName(toFromName)
+                bidirectional(bidirectional)
+                accessDataWrite?.let { AccessDB(accessDataWrite).toDomain() }
+            }.build()
+    }
+}
+
+internal fun RelationshipType.toDB(): RelationshipTypeDB {
+    return RelationshipTypeDB(
+        uid = uid()!!,
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created()?.dateFormat(),
+        lastUpdated = lastUpdated()?.dateFormat(),
+        fromToName = fromToName(),
+        toFromName = toFromName(),
+        bidirectional = bidirectional(),
+        accessDataWrite = access().toDB(),
+    )
+}

b701a8041920a1b1762aafb944cac798312c560a
danielTari <daniel@dhis2.org>
2025-05-14 13:20:05 +0200

    [ANDROSDK-2092] add mappers to ProgramStageSectionDB

--- core/src/main/java/org/hisp/dhis/android/persistence/program/ProgramStageSectionDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/program/ProgramStageSectionDB.kt
@@ -5,6 +5,15 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.common.ObjectWithUid
+import org.hisp.dhis.android.core.program.ProgramStageSection
+import org.hisp.dhis.android.core.program.SectionDeviceRendering
+import org.hisp.dhis.android.core.program.SectionRendering
+import org.hisp.dhis.android.core.program.SectionRenderingType
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.persistence.common.BaseIdentifiableObjectDB
+import org.hisp.dhis.android.persistence.common.EntityDB
+import org.hisp.dhis.android.persistence.common.applyBaseIdentifiableFields
 
 @Entity(
     tableName = "ProgramStageSection",
@@ -26,16 +35,51 @@ internal data class ProgramStageSectionDB(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
-    val uid: String,
-    val code: String?,
-    val name: String?,
-    val displayName: String?,
-    val created: String?,
-    val lastUpdated: String?,
+    override val uid: String,
+    override val code: String?,
+    override val name: String?,
+    override val displayName: String?,
+    override val created: String?,
+    override val lastUpdated: String?,
     val sortOrder: Int?,
     val programStage: String,
     val desktopRenderType: String?,
     val mobileRenderType: String?,
     val description: String?,
     val displayDescription: String?,
-)
+) : EntityDB<ProgramStageSection>, BaseIdentifiableObjectDB {
+
+    override fun toDomain(): ProgramStageSection {
+        return ProgramStageSection.builder()
+            .applyBaseIdentifiableFields(this)
+            .id(id?.toLong())
+            .sortOrder(sortOrder)
+            .programStage(ObjectWithUid.create(programStage))
+            .renderType(
+                SectionRendering.create(
+                    SectionDeviceRendering.create(desktopRenderType?.let { SectionRenderingType.valueOf(it) }),
+                    SectionDeviceRendering.create(mobileRenderType?.let { SectionRenderingType.valueOf(it) })
+                )
+            )
+            .description(description)
+            .displayDescription(displayDescription)
+            .build()
+    }
+}
+
+internal fun ProgramStageSection.toDB(): ProgramStageSectionDB {
+    return ProgramStageSectionDB(
+        uid = uid(),
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created().dateFormat(),
+        lastUpdated = lastUpdated().dateFormat(),
+        sortOrder = sortOrder(),
+        programStage = programStage()!!.uid(),
+        desktopRenderType = renderType()?.desktop()?.type()?.name,
+        mobileRenderType = renderType()?.mobile()?.type()?.name,
+        description = description(),
+        displayDescription = displayDescription(),
+    )
+}

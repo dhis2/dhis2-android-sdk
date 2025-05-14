c8208f00e1ef17c921a5e34e88c4e057651818ec
danielTari <daniel@dhis2.org>
2025-05-14 12:11:46 +0200

    [ANDROSDK-2092] add mappers to ProgramRuleDB

--- core/src/main/java/org/hisp/dhis/android/persistence/program/ProgramRuleDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/program/ProgramRuleDB.kt
@@ -5,6 +5,12 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.common.ObjectWithUid
+import org.hisp.dhis.android.core.program.ProgramRule
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.persistence.common.BaseIdentifiableObjectDB
+import org.hisp.dhis.android.persistence.common.EntityDB
+import org.hisp.dhis.android.persistence.common.applyBaseIdentifiableFields
 
 @Entity(
     tableName = "ProgramRule",
@@ -34,14 +40,41 @@ internal data class ProgramRuleDB(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
-    val uid: String,
-    val code: String?,
-    val name: String?,
-    val displayName: String?,
-    val created: String?,
-    val lastUpdated: String?,
+    override val uid: String,
+    override val code: String?,
+    override val name: String?,
+    override val displayName: String?,
+    override val created: String?,
+    override val lastUpdated: String?,
     val priority: Int?,
     val condition: String?,
     val program: String,
     val programStage: String?,
-)
+) : EntityDB<ProgramRule>, BaseIdentifiableObjectDB {
+
+    override fun toDomain(): ProgramRule {
+        return ProgramRule.builder().apply {
+            applyBaseIdentifiableFields(this@ProgramRuleDB)
+            id(id?.toLong())
+            priority(priority)
+            condition(condition)
+            program(ObjectWithUid.create(program))
+            programStage?.let { programStage(ObjectWithUid.create(it)) }
+        }.build()
+    }
+}
+
+internal fun ProgramRule.toDB(): ProgramRuleDB {
+    return ProgramRuleDB(
+        uid = uid()!!,
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created()?.dateFormat(),
+        lastUpdated = lastUpdated()?.dateFormat(),
+        priority = priority(),
+        condition = condition(),
+        program = program()!!.uid(),
+        programStage = programStage()?.uid(),
+    )
+}

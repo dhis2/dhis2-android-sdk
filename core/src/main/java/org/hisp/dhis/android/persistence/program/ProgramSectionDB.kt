c3d2d984cb89628d8507c8b0a4ade8eb8f6f2222
danielTari <daniel@dhis2.org>
2025-05-14 12:25:11 +0200

    [ANDROSDK-2092] add mappers to ProgramSectionDB

--- core/src/main/java/org/hisp/dhis/android/persistence/program/ProgramSectionDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/program/ProgramSectionDB.kt
@@ -5,6 +5,16 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.common.ObjectStyle
+import org.hisp.dhis.android.core.common.ObjectWithUid
+import org.hisp.dhis.android.core.program.ProgramSection
+import org.hisp.dhis.android.core.program.SectionDeviceRendering
+import org.hisp.dhis.android.core.program.SectionRendering
+import org.hisp.dhis.android.core.program.SectionRenderingType
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.persistence.common.BaseIdentifiableObjectDB
+import org.hisp.dhis.android.persistence.common.EntityDB
+import org.hisp.dhis.android.persistence.common.applyBaseIdentifiableFields
 
 @Entity(
     tableName = "ProgramSection",
@@ -26,12 +36,12 @@ internal data class ProgramSectionDB(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
-    val uid: String,
-    val code: String?,
-    val name: String?,
-    val displayName: String?,
-    val created: String?,
-    val lastUpdated: String?,
+    override val uid: String,
+    override val code: String?,
+    override val name: String?,
+    override val displayName: String?,
+    override val created: String?,
+    override val lastUpdated: String?,
     val description: String?,
     val program: String?,
     val sortOrder: Int?,
@@ -40,4 +50,42 @@ internal data class ProgramSectionDB(
     val icon: String?,
     val desktopRenderType: String?,
     val mobileRenderType: String?,
-)
+) : EntityDB<ProgramSection>, BaseIdentifiableObjectDB {
+
+    override fun toDomain(): ProgramSection {
+        return ProgramSection.builder().apply {
+            applyBaseIdentifiableFields(this@ProgramSectionDB)
+            id(id?.toLong())
+            description(description)
+            program?.let { program(ObjectWithUid.create(it)) }
+            sortOrder(sortOrder)
+            formName(formName)
+            renderType(
+                SectionRendering.create(
+                    SectionDeviceRendering.create(desktopRenderType?.let { SectionRenderingType.valueOf(it) }),
+                    SectionDeviceRendering.create(mobileRenderType?.let { SectionRenderingType.valueOf(it) })
+                )
+            )
+            style(ObjectStyle.builder().color(color).icon(icon).build())
+        }.build()
+    }
+}
+
+internal fun ProgramSection.toDB(): ProgramSectionDB {
+    return ProgramSectionDB(
+        uid = uid()!!,
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created()?.dateFormat(),
+        lastUpdated = lastUpdated()?.dateFormat(),
+        description = description(),
+        program = program()?.uid(),
+        sortOrder = sortOrder(),
+        formName = formName(),
+        color = style()?.color(),
+        icon = style()?.icon(),
+        desktopRenderType = renderType()?.desktop()?.type()?.name,
+        mobileRenderType = renderType()?.mobile()?.type()?.name,
+    )
+}

6bde9ac019df75b042361c0cd1fde7c20fb1f28a
danielTari <daniel@dhis2.org>
2025-05-14 16:31:00 +0200

    [ANDROSDK-2093] add mappers to entity OptionSetDB

--- core/src/main/java/org/hisp/dhis/android/persistence/option/OptionSetDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/option/OptionSetDB.kt
@@ -4,6 +4,12 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.common.ValueType
+import org.hisp.dhis.android.core.option.OptionSet
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.persistence.common.BaseIdentifiableObjectDB
+import org.hisp.dhis.android.persistence.common.EntityDB
+import org.hisp.dhis.android.persistence.common.applyBaseIdentifiableFields
 
 @Entity(
     tableName = "OptionSet",
@@ -15,12 +21,35 @@ internal data class OptionSetDB(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
-    val uid: String,
-    val code: String?,
-    val name: String?,
-    val displayName: String?,
-    val created: String?,
-    val lastUpdated: String?,
+    override val uid: String,
+    override val code: String?,
+    override val name: String?,
+    override val displayName: String?,
+    override val created: String?,
+    override val lastUpdated: String?,
     val version: Int?,
     val valueType: String?,
-)
+) : EntityDB<OptionSet>, BaseIdentifiableObjectDB {
+
+    override fun toDomain(): OptionSet {
+        return OptionSet.builder().apply {
+            applyBaseIdentifiableFields(this@OptionSetDB)
+            id(id?.toLong())
+            version(version)
+            valueType?.let { valueType(ValueType.valueOf(it)) }
+        }.build()
+    }
+}
+
+internal fun OptionSet.toDB(): OptionSetDB {
+    return OptionSetDB(
+        uid = uid(),
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created().dateFormat(),
+        lastUpdated = lastUpdated().dateFormat(),
+        version = version(),
+        valueType = valueType()?.name,
+    )
+}

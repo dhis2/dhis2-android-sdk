b214f3d9ef899822f8178abca19d38ab806c892d
Marcos Campos <marcos.campos@dhis2.org>
2025-05-15 11:03:39 +0200

    [ANDROSDK-2090] add mappers to CustomIconDB

--- core/src/main/java/org/hisp/dhis/android/persistence/icon/CustomIconDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/icon/CustomIconDB.kt
@@ -3,6 +3,9 @@ package org.hisp.dhis.android.persistence.icon
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.common.ObjectWithUid
+import org.hisp.dhis.android.core.icon.CustomIcon
+import org.hisp.dhis.android.persistence.common.EntityDB
 
 @Entity(tableName = "CustomIcon")
 internal data class CustomIconDB(
@@ -12,4 +15,22 @@ internal data class CustomIconDB(
     val key: String,
     val fileResource: String,
     val href: String,
-)
+) : EntityDB<CustomIcon> {
+
+    override fun toDomain(): CustomIcon {
+        return CustomIcon.builder()
+            .id(id?.toLong())
+            .key(key)
+            .fileResource(ObjectWithUid.create(fileResource))
+            .href(href)
+            .build()
+    }
+}
+
+internal fun CustomIcon.toDB(): CustomIconDB {
+    return CustomIconDB(
+        key = key(),
+        fileResource = fileResource().uid(),
+        href = href(),
+    )
+}

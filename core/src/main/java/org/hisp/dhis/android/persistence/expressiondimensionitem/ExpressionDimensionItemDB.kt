5c7eaeee445a5991fa87f688520f10fef95410ab
Marcos Campos <marcos.campos@dhis2.org>
2025-05-15 09:17:11 +0200

    [ANDROSDK-2088] Implement toDomain and toDB functions for ExpressionDimensionItem package

--- core/src/main/java/org/hisp/dhis/android/persistence/expressiondimensionitem/ExpressionDimensionItemDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/expressiondimensionitem/ExpressionDimensionItemDB.kt
@@ -4,6 +4,11 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.expressiondimensionitem.ExpressionDimensionItem
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.persistence.common.BaseIdentifiableObjectDB
+import org.hisp.dhis.android.persistence.common.EntityDB
+import org.hisp.dhis.android.persistence.common.applyBaseIdentifiableFields
 
 @Entity(
     tableName = "ExpressionDimensionItem",
@@ -15,11 +20,32 @@ internal data class ExpressionDimensionItemDB(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     val id: Int? = 0,
-    val uid: String,
-    val code: String?,
-    val name: String?,
-    val displayName: String?,
-    val created: String?,
-    val lastUpdated: String?,
+    override val uid: String,
+    override val code: String?,
+    override val name: String?,
+    override val displayName: String?,
+    override val created: String?,
+    override val lastUpdated: String?,
     val expression: String?,
-)
+) : EntityDB<ExpressionDimensionItem>, BaseIdentifiableObjectDB {
+
+    override fun toDomain(): ExpressionDimensionItem {
+        return ExpressionDimensionItem.builder()
+            .applyBaseIdentifiableFields(this@ExpressionDimensionItemDB)
+            .id(id?.toLong())
+            .expression(expression)
+            .build()
+    }
+}
+
+internal fun ExpressionDimensionItem.toDB(): ExpressionDimensionItemDB {
+    return ExpressionDimensionItemDB(
+        uid = uid(),
+        code = code(),
+        name = name(),
+        displayName = displayName(),
+        created = created().dateFormat(),
+        lastUpdated = lastUpdated().dateFormat(),
+        expression = expression(),
+    )
+}

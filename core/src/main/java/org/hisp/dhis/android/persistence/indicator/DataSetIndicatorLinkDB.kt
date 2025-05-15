dea1bc04715bb19f961eb4d8133b577157c4a631
Marcos Campos <marcos.campos@dhis2.org>
2025-05-15 11:04:10 +0200

    [ANDROSDK-2090] add mappers to indicator package

--- core/src/main/java/org/hisp/dhis/android/persistence/indicator/DataSetIndicatorLinkDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/indicator/DataSetIndicatorLinkDB.kt
@@ -5,6 +5,8 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.indicator.DataSetIndicatorLink
+import org.hisp.dhis.android.persistence.common.EntityDB
 import org.hisp.dhis.android.persistence.dataset.DataSetDB
 
 @Entity(
@@ -37,4 +39,20 @@ internal data class DataSetIndicatorLinkDB(
     val id: Int? = 0,
     val dataSet: String,
     val indicator: String,
-)
+) : EntityDB<DataSetIndicatorLink> {
+
+    override fun toDomain(): DataSetIndicatorLink {
+        return DataSetIndicatorLink.builder()
+            .id(id?.toLong())
+            .dataSet(dataSet)
+            .indicator(indicator)
+            .build()
+    }
+}
+
+internal fun DataSetIndicatorLink.toDB(): DataSetIndicatorLinkDB {
+    return DataSetIndicatorLinkDB(
+        dataSet = dataSet()!!,
+        indicator = indicator()!!,
+    )
+}

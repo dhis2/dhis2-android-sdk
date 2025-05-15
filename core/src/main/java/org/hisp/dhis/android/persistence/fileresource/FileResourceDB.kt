332551d28c34c98a7b8e3b2f0d3d2a6c80508f88
Marcos Campos <marcos.campos@dhis2.org>
2025-05-15 09:17:26 +0200

    [ANDROSDK-2088] Implement toDomain and toDB functions for FileResource package

--- core/src/main/java/org/hisp/dhis/android/persistence/fileresource/FileResourceDB.kt
+++ core/src/main/java/org/hisp/dhis/android/persistence/fileresource/FileResourceDB.kt
@@ -4,6 +4,14 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import org.hisp.dhis.android.core.fileresource.FileResource
+import org.hisp.dhis.android.core.fileresource.FileResourceDomain
+import org.hisp.dhis.android.core.util.dateFormat
+import org.hisp.dhis.android.core.util.toJavaDate
+import org.hisp.dhis.android.persistence.common.DataObjectDB
+import org.hisp.dhis.android.persistence.common.EntityDB
+import org.hisp.dhis.android.persistence.common.SyncStateDB
+import org.hisp.dhis.android.persistence.common.toDB
 
 @Entity(
     tableName = "FileResource",
@@ -20,8 +28,38 @@ internal data class FileResourceDB(
     val created: String?,
     val lastUpdated: String?,
     val contentType: String?,
-    val contentLength: Int?,
+    val contentLength: Long?,
     val path: String?,
-    val syncState: String?,
+    override val syncState: SyncStateDB?,
     val domain: String?,
-)
+) : EntityDB<FileResource>, DataObjectDB {
+
+    override fun toDomain(): FileResource {
+        return FileResource.builder().apply {
+            id(id?.toLong())
+            uid(uid)
+            name?.let { name(it) }
+            created?.let { created(it.toJavaDate()!!) }
+            lastUpdated?.let { lastUpdated(it.toJavaDate()!!) }
+            contentType?.let { contentType(it) }
+            contentLength?.let { contentLength(it) }
+            path?.let { path(it) }
+            syncState?.let { syncState(it.toDomain()) }
+            domain?.let { domain(FileResourceDomain.valueOf(it)) }
+        }.build()
+    }
+}
+
+internal fun FileResource.toDB(): FileResourceDB {
+    return FileResourceDB(
+        uid = uid()!!,
+        name = name(),
+        created = created().dateFormat(),
+        lastUpdated = lastUpdated().dateFormat(),
+        contentType = contentType(),
+        contentLength = contentLength(),
+        path = path(),
+        syncState = syncState()?.toDB(),
+        domain = domain()?.name,
+    )
+}

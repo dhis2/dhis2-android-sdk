bba4244c91e27a80f72502e4f8e0db75f06f667d
Victor Garcia <victor.garcia@dhis2.org>
2025-06-10 23:01:25 +0200

    [ANDROSDK-2116] Refactor to reuse property names

--- processor/src/main/java/org/hisp/dhis/android/processor/EntityProcessor.kt
+++ processor/src/main/java/org/hisp/dhis/android/processor/EntityProcessor.kt
@@ -64,7 +64,7 @@ class EntityProcessor(
             val tableName = getTableName(symbol)
             val tableInfoClass = tableName + "TableInfo"
 
-            val columnNames = getColumns(symbol)
+            val columns = getColumns(symbol)
 
             val file = codeGenerator.createNewFile(
                 // Make sure to associate the generated file with sources to keep/maintain it across incremental builds.
@@ -97,19 +97,18 @@ class EntityProcessor(
                 class Columns : CoreColumns() {
                     override fun all(): Array<String> {
                         return arrayOf(
-                            ${columnNames.joinToString(
+                            ${columns.joinToString(
                                 separator = "\n                            ",
-                            ) { name -> 
-                                "${camelToUpperSnakeCase(name)}," 
+                            ) { c ->
+                                "${c.propertyName}," 
                             }}
                         )
                     }
                     companion object {
-                        ${columnNames.joinToString(
+                        ${columns.joinToString(
                             separator = "\n                        ",
-                        ) { name -> 
-                            val upperName = camelToUpperSnakeCase(name)
-                            "const val $upperName = \"$name\"" 
+                        ) { c ->
+                            "const val ${c.propertyName} = \"${c.columnName}\"" 
                         }}
                     }
                 }
@@ -136,8 +135,12 @@ class EntityProcessor(
         return string.replace(pattern, "_$0").uppercase()
     }
 
-    private fun getColumns(symbol: KSClassDeclaration): List<String> {
-        return symbol.getDeclaredProperties().map { getColumnName(it) }.toList()
+    private fun getColumns(symbol: KSClassDeclaration): List<TableColumn> {
+        return symbol.getDeclaredProperties().map {
+            val columnName = getColumnName(it)
+            val propertyName = camelToUpperSnakeCase(columnName)
+            TableColumn(propertyName = propertyName, columnName = columnName)
+        }.toList()
     }
 
     private fun getColumnName(property: KSPropertyDeclaration): String {
@@ -149,3 +152,5 @@ class EntityProcessor(
         return serialNameValue ?: property.simpleName.getShortName()
     }
 }
+
+private data class TableColumn(val propertyName: String, val columnName: String)

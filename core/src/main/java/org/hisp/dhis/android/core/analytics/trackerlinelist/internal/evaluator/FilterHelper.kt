97e18c4d8c07764bb80b2571171f207d3d854404
danielTari <daniel@dhis2.org>
2025-06-10 10:13:45 +0200

    [ANDROSDK-2114] add parenthesis

--- core/src/main/java/org/hisp/dhis/android/core/analytics/trackerlinelist/internal/evaluator/FilterHelper.kt
+++ core/src/main/java/org/hisp/dhis/android/core/analytics/trackerlinelist/internal/evaluator/FilterHelper.kt
@@ -39,7 +39,7 @@ internal class FilterHelper(private val itemId: String) {
     fun like(value: String, ignoreCase: Boolean): String = itemTo("LIKE '%$value%'${case(ignoreCase)}")
     fun notLike(value: String, ignoreCase: Boolean): String = itemTo("NOT LIKE '%$value%'${case(ignoreCase)}")
     fun inValues(values: List<String>): String = itemTo("IN (${values.joinToString(", ") { "'$it'" }})")
-    fun isNull(isNull: Boolean): String = itemTo(if (isNull) "IS NULL OR = ''" else "IS NOT NULL AND <> ''")
+    fun isNull(isNull: Boolean): String = itemTo(if (isNull) "(IS NULL OR = '')" else "(IS NOT NULL AND <> '')")
 
     private fun itemTo(comparison: String) = "\"$itemId\" $comparison"
 

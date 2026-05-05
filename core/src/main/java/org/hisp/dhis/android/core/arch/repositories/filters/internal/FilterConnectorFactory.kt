c6e42d0624ccc079380111b49f81d6b0bfba7656
danielTari <daniel@dhis2.org>
2026-05-05 08:35:48 +0200

    [ANDROSDK-2288] remove unused extractor

--- core/src/main/java/org/hisp/dhis/android/core/arch/repositories/filters/internal/FilterConnectorFactory.kt
+++ core/src/main/java/org/hisp/dhis/android/core/arch/repositories/filters/internal/FilterConnectorFactory.kt
@@ -27,13 +27,11 @@
  */
 package org.hisp.dhis.android.core.arch.repositories.filters.internal
 
-import android.content.ContentValues
 import org.hisp.dhis.android.core.arch.repositories.collection.BaseRepository
 import org.hisp.dhis.android.core.arch.repositories.collection.internal.BaseRepositoryFactory
 import org.hisp.dhis.android.core.arch.repositories.scope.RepositoryScope
 import org.hisp.dhis.android.core.arch.repositories.scope.RepositoryScope.OrderByDirection
 import org.hisp.dhis.android.core.arch.repositories.scope.internal.RepositoryScopeHelper
-import org.hisp.dhis.android.core.arch.repositories.scope.internal.RepositoryScopeKeyOrderExtractor
 import org.hisp.dhis.android.core.arch.repositories.scope.internal.RepositoryScopeOrderByItem
 
 @SuppressWarnings("TooManyFunctions")
@@ -139,17 +137,6 @@ class FilterConnectorFactory<R : BaseRepository> internal constructor(
             ownLink,
             connectedAdditionalWhere,
         )
-        val extractor = RepositoryScopeKeyOrderExtractor { contentValues: ContentValues, _: String ->
-            val ownLinkKey = contentValues.getAsString(ownLink)
-            String.format(
-                "(SELECT %s FROM %s WHERE %s = '%s' %s)",
-                externalColumn,
-                externalTable,
-                externalLink,
-                ownLinkKey,
-                connectedAdditionalWhere,
-            )
-        }
         val item =
             RepositoryScopeOrderByItem.builder().column(column).direction(direction).build()
         return repositoryFactory.updated(RepositoryScopeHelper.withOrderBy(scope, item))

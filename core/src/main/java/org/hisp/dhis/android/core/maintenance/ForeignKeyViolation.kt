be4223ce03b3a6c296343acd966eaf918df3505e
danielTari <daniel@dhis2.org>
2026-06-25 16:20:38 +0200

    [ANDROSDK-2332] migrate ForeignKeyViolation to data class

--- core/src/main/java/org/hisp/dhis/android/core/maintenance/ForeignKeyViolation.kt
+++ core/src/main/java/org/hisp/dhis/android/core/maintenance/ForeignKeyViolation.kt
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2004-2023, University of Oslo
+ *  Copyright (c) 2004-2026, University of Oslo
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -26,68 +26,39 @@
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.hisp.dhis.android.core.maintenance;
+package org.hisp.dhis.android.core.maintenance
 
-import androidx.annotation.Nullable;
+import org.hisp.dhis.android.annotations.ModelBuilder
+import org.hisp.dhis.android.core.common.CoreObject
+import java.util.Date
 
-import com.google.auto.value.AutoValue;
+@ModelBuilder
+data class ForeignKeyViolation(
+    val fromTable: String?,
+    val fromColumn: String?,
+    val toTable: String?,
+    val toColumn: String?,
+    val notFoundValue: String?,
+    val fromObjectUid: String?,
+    val fromObjectRow: String?,
+    val created: Date?,
+) : CoreObject {
 
-import org.hisp.dhis.android.core.common.CoreObject;
+    fun fromTable(): String? = fromTable
+    fun fromColumn(): String? = fromColumn
+    fun toTable(): String? = toTable
+    fun toColumn(): String? = toColumn
+    fun notFoundValue(): String? = notFoundValue
+    fun fromObjectUid(): String? = fromObjectUid
+    fun fromObjectRow(): String? = fromObjectRow
+    fun created(): Date? = created
 
-import java.util.Date;
+    fun toBuilder(): Builder = ForeignKeyViolationBuilder.from(this)
 
-@AutoValue
-public abstract class ForeignKeyViolation implements CoreObject {
+    class Builder : ForeignKeyViolationBuilder()
 
-    @Nullable
-    public abstract String fromTable();
-
-    @Nullable
-    public abstract String fromColumn();
-
-    @Nullable
-    public abstract String toTable();
-
-    @Nullable
-    public abstract String toColumn();
-
-    @Nullable
-    public abstract String notFoundValue();
-
-    @Nullable
-    public abstract String fromObjectUid();
-
-    @Nullable
-    public abstract String fromObjectRow();
-
-    @Nullable
-    public abstract Date created();
-
-    public static Builder builder() {
-        return new AutoValue_ForeignKeyViolation.Builder();
-    }
-
-    public abstract Builder toBuilder();
-
-    @AutoValue.Builder
-    public abstract static class Builder {
-
-        public abstract Builder fromTable(String fromTable);
-
-        public abstract Builder fromColumn(String fromColumn);
-
-        public abstract Builder toTable(String toTable);
-
-        public abstract Builder toColumn(String toColumn);
-
-        public abstract Builder notFoundValue(String notFoundValue);
-
-        public abstract Builder fromObjectUid(String fromObjectUid);
-
-        public abstract Builder fromObjectRow(String fromObjectRow);
-
-        public abstract Builder created(Date created);
-
-        public abstract ForeignKeyViolation build();
+    companion object {
+        @JvmStatic
+        fun builder(): Builder = Builder()
     }
 }

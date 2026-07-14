f6775f41de93b25017dae215d133a640c60300ee
danielTari <daniel@dhis2.org>
2026-07-14 15:55:50 +0200

    [ANDROSDK-2353] remove unused classes migrate Unit to kotlin

--- core/src/main/java/org/hisp/dhis/android/core/common/Unit.kt
+++ core/src/main/java/org/hisp/dhis/android/core/common/Unit.kt
@@ -25,12 +25,6 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.hisp.dhis.android.core.common
 
-package org.hisp.dhis.android.core.common;
-
-public final class Unit {
-
-    public Unit() {
-        // This constructor is intentionally empty. Nothing special is needed here.
-    }
-}
+class Unit 
